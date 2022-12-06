198a5bf6013e2b4bbbee5333affc728fc581addf
cberg-aot <93226309+cberg-aot@users.noreply.github.com>
2022-12-06 14:17:00 -0800

    Hello Demo! (#31)
    Co-authored-by: Chris Berg <christopherpberg@gmail.com>

--- frontend/src/app.controller.spec.ts
+++ frontend/src/app.controller.spec.ts
@@ -13,9 +13,9 @@ describe('AppController', () => {
   })
 
   describe('getHello', () => {
-    it('should return "Hello World! - test"', () => {
+    it('should return "Hello Demo!"', () => {
       const appController = app.get<AppController>(AppController)
-      expect(appController.getHello()).toBe('Hello World! - test')
+      expect(appController.getHello()).toBe('Hello Demo!')
     })
   })
 })
