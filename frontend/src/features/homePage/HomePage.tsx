11b9c96ae703d6d2698c618efc54cfa985cdb078
Devin Bushey <devin.bushey@aot-technologies.com>
2022-12-12 18:46:43 -0800

    Changed dotenv devdependency

--- frontend/src/features/homePage/HomePage.tsx
+++ frontend/src/features/homePage/HomePage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 export const HomePage = React.memo(() => {
+
+  const DEPLOY_ENV: string | undefined =
+    process.env.REACT_APP_DEPLOY_ENVIRONMENT;
+
   return (
     <div>
-      <p>OnRouteBc Home</p>
+      <p>OnRouteBc Home -{DEPLOY_ENV}-</p>
     </div>
   );
 });
