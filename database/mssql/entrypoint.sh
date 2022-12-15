d3dfb42445165b26591ed20de5b9b5f260463943
Devin Bushey <devin.bushey@aot-technologies.com>
2022-12-15 10:53:46 -0800

    Added environment variables for MSSQL and added sample.env's

--- database/mssql/entrypoint.sh
+++ database/mssql/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Run init-script with long timeout - and make it run in the background
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P $SA_PASSWORD -i $DDL_FILENAME &
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U $MSSQL_SA_USER -P $MSSQL_SA_PASSWORD -i $MSSQL_DDL_FILENAME &
 # Start SQL server
 /opt/mssql/bin/sqlservr
