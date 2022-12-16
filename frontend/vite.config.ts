11b2018e22dc581c9b107a6e6a84846863385718
Chris Berg <christopherpberg@gmail.com>
2022-12-16 10:48:13 -0800

    iterating

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+import { defineConfig } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
