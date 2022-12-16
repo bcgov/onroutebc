f6c9d479a73eb04e7a968d798a32927fdd99fb17
Devin Bushey <devin.bushey@aot-technologies.com>
2022-12-15 16:33:54 -0800

    Added vite deployment env variable to homepage for testing

--- frontend/src/features/homePage/HomePage.tsx
+++ frontend/src/features/homePage/HomePage.tsx
@@ -5,9 +5,11 @@ export const HomePage = React.memo(() => {
 
   const DEPLOY_ENV = Config.VITE_DEPLOY_ENVIRONMENT;
 
+  const DEPLOY_ENV2: string | undefined = import.meta.env.VITE_DEPLOY_ENVIRONMENT;
+
   return (
     <div>
-      <p>OnRouteBc Home -{DEPLOY_ENV}-</p>
+      <p>OnRouteBc Home -{DEPLOY_ENV}-  x{DEPLOY_ENV2}x</p>
     </div>
   );
 });
