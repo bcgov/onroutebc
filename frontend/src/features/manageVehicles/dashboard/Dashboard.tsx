8670e343f40767a1974dbf1b09f27aa06181c555
Devin Bushey <devin.bushey@aot-technologies.com>
2022-12-16 09:59:31 -0800

    Added tabs to dashboard

--- frontend/src/features/manageVehicles/dashboard/Dashboard.tsx
+++ frontend/src/features/manageVehicles/dashboard/Dashboard.tsx
@@ -5,17 +5,80 @@ import { List } from "../list/List";
 
 import SlidingPane from "react-sliding-pane";
 import "react-sliding-pane/dist/react-sliding-pane.css";
+import { Box, Typography, Tabs, Tab } from "@mui/material";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{}}>
+          <Typography component={"span"}>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 export const Dashboard = React.memo(() => {
   const [showForm, setShowForm] = useState<boolean>(false);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
     <>
-      <p>Manage Vehicles Dashboard</p>
-      <Button color={"BC-Gov-PrimaryButton"} onClick={() => setShowForm(true)}>
-        Create New
-      </Button>
-      <List />
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <h2>Vehicle Inventory</h2>
+          <Button
+            color={"BC-Gov-PrimaryButton"}
+            onClick={() => setShowForm(true)}
+          >
+            Create New
+          </Button>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Item One" {...a11yProps(0)} />
+            <Tab label="Item Two" {...a11yProps(1)} />
+            <Tab label="Item Three" {...a11yProps(2)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <List />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </Box>
 
       <SlidingPane
         isOpen={showForm}
@@ -30,4 +93,4 @@ export const Dashboard = React.memo(() => {
   );
 });
 
-Dashboard.displayName = 'Dashboard';
+Dashboard.displayName = "Dashboard";
