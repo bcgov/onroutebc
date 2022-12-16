291a0ab8cda8b72d82469399853792932f3b5b80
Chris Berg <christopherpberg@gmail.com>
2022-12-16 11:09:03 -0800

    iterating

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,9 +8,6 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 
-export default ({ mode }) => {
-	  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }; // <-
-}
 export default defineConfig({
   server: {
     port: 3000,
@@ -32,4 +29,7 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-});
+}
+
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }; // <-
+);
