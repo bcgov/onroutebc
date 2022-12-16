bd1230e7a7c7bf6d5b8bb458cd5bd6c488bb7bf5
praju-aot <praveen.raju@aot-technologies.com>
2022-12-16 12:55:54 -0500

    Connecting to MSSQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehiclesModule } from './vehicles/vehicles.module';
 
+
+console.log('Var check - DB_TYPE', process.env.DB_TYPE);
 console.log('Var check - POSTGRESQL_HOST', process.env.POSTGRESQL_HOST);
 console.log('Var check - POSTGRESQL_DATABASE', process.env.POSTGRESQL_DATABASE);
 console.log('Var check - POSTGRESQL_USER', process.env.POSTGRESQL_USER);
@@ -14,21 +16,34 @@ if (process.env.POSTGRESQL_PASSWORD != null) {
 } else {
   console.log('Var check - POSTGRESQL_PASSWORD not present');
 }
+console.log('Var check - MSSQL_HOST', process.env.MSSQL_HOST);
+console.log('Var check - MSSQL_PORT', process.env.MSSQL_PORT);
+console.log('Var check - MSSQL_DB', process.env.MSSQL_DB);
+console.log('Var check - MSSQL_SA_USER', process.env.MSSQL_SA_USER);
+console.log('Var check - MSSQL_SA_PASSWORD', process.env.MSSQL_SA_PASSWORD);
+
+
+console.log('Var check - DB_TYPE',process.env.DB_TYPE==='mssql'?'mssql':'postgres');
+console.log('Var check - DB_HOST', process.env.DB_TYPE === 'mssql'? process.env.MSSQL_HOST: process.env.POSTGRESQL_HOST);
+console.log('Var check - DB_PORT', process.env.DB_TYPE === 'mssql'? parseInt(process.env.MSSQL_PORT):5432);
+console.log('Var check - DB_DATABASE', process.env.DB_TYPE === 'mssql'? process.env.MSSQL_DB : process.env.POSTGRESQL_DATABASE);
+console.log('Var check - DB_USERNAME', process.env.DB_TYPE === 'mssql'? process.env.MSSQL_SA_USER : process.env.POSTGRESQL_USER);
+console.log('Var check - DB_PASSWORD', process.env.DB_TYPE === 'mssql'? process.env.MSSQL_SA_PASSWORD : process.env.POSTGRESQL_PASSWORD);
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({ 
-      type: process.env.DB_TYPE==='mssql'?'mssql':'postgres',
-      host: process.env.DB_TYPE === 'mssql'? process.env.MSSQL_HOST: process.env.POSTGRESQL_HOST,
-      port: process.env.DB_TYPE === 'mssql'? parseInt(process.env.MSSQL_PORT):5432,
-      database: process.env.DB_TYPE === 'mssql'? process.env.MSSQL_DB : process.env.POSTGRESQL_DATABASE,
-      username: process.env.DB_TYPE === 'mssql'? process.env.MSSQL_SA_USER : process.env.POSTGRESQL_USER,
-      password: process.env.DB_TYPE === 'mssql'? process.env.MSSQL_SA_PASSWORD : process.env.POSTGRESQL_PASSWORD,
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      database: 'onroutebc',
+      username: 'SA',
+      password: 'YourStrong@Passw0rd',
+      options: { encrypt: false },
       // entities: [User],
       autoLoadEntities: true, // Auto load all entities regiestered by typeorm forFeature method.
-      synchronize: process.env.DB_TYPE === 'mssql'? true: true, // This changes the DB schema to match changes to entities, which we might not want.
-      options: { encrypt: process.env.DB_TYPE === 'mssql'? false: null },
+      synchronize: true, // This changes the DB schema to match changes to entities, which we might not want.
     }),
     VehiclesModule,
   ],
