d995e0ca9da7e33928b24c1f1d9ee91673598809
Chris Berg <christopherpberg@gmail.com>
2022-12-16 14:15:01 -0800

    iterating

--- frontend/src/features/homePage/HomePage.tsx
+++ frontend/src/features/homePage/HomePage.tsx
@@ -8,10 +8,11 @@ export const HomePage = React.memo(() => {
   const DEPLOY_ENV3: string | undefined = import.meta.env.MODE;
   const DEPLOY_ENV4: boolean | undefined = import.meta.env.PROD;
   const DEPLOY_ENV5: boolean | undefined = import.meta.env.DEV;
+  const DEPLOY_ENV6: boolean | undefined = process.env.VITE_DEPLOY_ENVIRONMENT;
 
   return (
     <div>
-      <p>OnRouteBc Home - {DEPLOY_ENV} - {DEPLOY_ENV2} - {DEPLOY_ENV3} - {DEPLOY_ENV4} - {DEPLOY_ENV5}</p>
+      <p>OnRouteBc Home - {DEPLOY_ENV} - {DEPLOY_ENV2} - {DEPLOY_ENV3} - {DEPLOY_ENV4} - {DEPLOY_ENV5} - {DEPLOY_ENV6}</p>
     </div>
   );
 });
