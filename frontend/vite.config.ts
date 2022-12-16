452994ffa175b0c952c4d5c6eed11ef17133cae5
Chris Berg <christopherpberg@gmail.com>
2022-12-16 13:19:35 -0800

    iterating

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-	loadEnv('FOO', process.cwd());
+	loadEnv(process.cwd());
 	// import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
 	// import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
   server: {
