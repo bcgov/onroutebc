b1acf11c650c2ddeb4c84d09e657e806717fb225
Chris Berg <christopherpberg@gmail.com>
2022-12-18 09:59:08 -0800

    Solution Attempt #4

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,1 +1,6 @@
-window.VITE_DEPLOY_ENVIRONMENT = "local";
+// vite.config.js / vite.config.ts
+	 import { envConfig } from '@geprog/vite-plugin-env-config';
+
+export default {
+	  plugins: [envConfig({ variables: ['VITE_DEPLOY_ENVIRONMENT'] })],
+};
--- frontend/src/features/homePage/HomePage.tsx
+++ frontend/src/features/homePage/HomePage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Config } from '../../config';
+import { getEnvConfig } from '@geprog/vite-plugin-env-config';
+
+const DEPLOY_ENVIRONMENT = getEnvConfig('VITE_DEPLOY_ENVIRONMENT');
 
 export const HomePage = React.memo(() => {
-
-  const DEPLOY_ENV = Config.VITE_DEPLOY_ENVIRONMENT;
-
   return (
     <div>
       <p>OnRouteBc Home -{DEPLOY_ENV}-</p>
