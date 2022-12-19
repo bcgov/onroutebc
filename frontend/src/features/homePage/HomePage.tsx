4b5032be6274cd24d581c19040a9fc58064f530f
Chris Berg <christopherpberg@gmail.com>
2022-12-19 15:23:37 -0800

    Interating...

--- frontend/src/features/homePage/HomePage.tsx
+++ frontend/src/features/homePage/HomePage.tsx
@@ -3,11 +3,12 @@ import { Config } from '../../config';
 
 export const HomePage = React.memo(() => {
 
-  const DEPLOY_ENV = Config.VITE_DEPLOY_ENVIRONMENT;
+  const DEPLOY_ENV_1 = Config.VITE_DEPLOY_ENVIRONMENT;
+  const DEPLOY_ENV_2 = import.meta.env.VITE_DEPLOY_ENVIRONMENT;
 
   return (
     <div>
-      <p>OnRouteBc Home -{DEPLOY_ENV}-</p>
+      <p>OnRouteBc Home-{DEPLOY_ENV_1}-{DEPLOY_ENV_2}-</p>
     </div>
   );
 });
