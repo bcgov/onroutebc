ae8c3ebf4ded039f8025045296a2ee28aad4be98
Chris Berg <christopherpberg@gmail.com>
2022-12-19 15:48:10 -0800

    Interating...

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-	envDir: src/config
+	envDir: src/config,
   plugins: [eslint(), react(), viteTsconfigPaths(), svgrPlugin()],
   test: {
     globals: true,
