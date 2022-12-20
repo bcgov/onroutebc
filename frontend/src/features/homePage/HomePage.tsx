25727a357291a2747e71db965c45224821883f13
Chris Berg <christopherpberg@gmail.com>
2022-12-20 11:03:31 -0800

    Interating...

--- frontend/src/features/homePage/HomePage.tsx
+++ frontend/src/features/homePage/HomePage.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 export const HomePage = React.memo(() => {
 
-  const DEPLOY_ENV = import.meta.env.VITE_DEPLOY_ENVIRONMENT;
+  const DEPLOY_ENV1 = import.meta.env.VITE_DEPLOY_ENVIRONMENT;
+  const DEPLOY_ENV2 = process.env.VITE_DEPLOY_ENVIRONMENT;
+	const DEPLOY_ENV3 = loadEnv('production', '/usr/share/nginx/html/config', '')
 
   return (
     <div>
-      <p>OnRouteBc Home -{DEPLOY_ENV}-</p>
+      <p>OnRouteBc Home-{DEPLOY_ENV1}-{DEPLOY_ENV2}-{DEPLOY_ENV3}-</p>
     </div>
   );
 });
