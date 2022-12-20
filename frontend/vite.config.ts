3919422ab371c7a1820beae920dd504b953e06ff
Chris Berg <christopherpberg@gmail.com>
2022-12-20 11:06:31 -0800

    Interating...

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
