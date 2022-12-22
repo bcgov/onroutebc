edba791a30b717735009e3209ce674cfaf54077a
John Fletcher <john.fletcher@aot-technologies.com>
2022-12-22 15:28:23 -0800

    Updated comments

--- database/mssql/entrypoint.sh
+++ database/mssql/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Taken from https://github.com/microsoft/mssql-docker/blob/master/linux/preview/examples/mssql-customize/entrypoint.sh
 
-# Start the script to create the DB and user
+# Start the script to create and initialize the DB
 /usr/config/configure-db.sh &
 
 # Start SQL Server
