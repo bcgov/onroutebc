8f6fb70a561225d7ed16f4c259479561dd0a8acc
Devin Bushey <devin.bushey@aot-technologies.com>
2022-12-30 11:58:35 -0800

    Changed logic to get openshift environment

--- frontend/src/features/manageVehicles/hooks/endpoints/endpoints.tsx
+++ frontend/src/features/manageVehicles/hooks/endpoints/endpoints.tsx
@@ -2,15 +2,15 @@ import { getOpenshiftEnv } from "../../../../common/helpers/helpers";
 
 const ENV = getOpenshiftEnv();
 
-export const MV_BACKEND_URL =
-  ENV === "localhost"
-    ? "http://localhost:5000"
-    // If the Env is a number, then it is in dev and 
-    // the number corresponds to the PR number in github
-    : ENV === ("test" || "uat" || "prod" || !isNaN(Number(ENV)))
-    ? `https://onroutebc-${ENV}-backend.apps.silver.devops.gov.bc.ca`
-    // If the env is unknown, it may be in prod, try the following url
-    : `https://onroutebc-backend.apps.silver.devops.gov.bc.ca`;
+/**
+ * If the Env is a number, then it is in dev and 
+ * the number corresponds to the PR number in github
+ * 
+ * If the env is unknown, it may be in prod, try the following url:
+ * https://onroutebc-backend.apps.silver.devops.gov.bc.ca
+ * 
+ */
+export const MV_BACKEND_URL = import.meta.env.VITE_BACKEND_API|| ((window as any)._env_ && (window as any)._env_.VITE_BACKEND_API) 
 
 export const VEHICLES_API = {
   GET_ALL_POWER_UNITS: `${MV_BACKEND_URL}/vehicles/powerUnit`,
