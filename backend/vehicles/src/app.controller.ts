1ae950b311c3f1b0373d277a28e7439816750443
Devin Bushey <devin.bushey@aot-technologies.com>
2023-01-06 13:26:33 -0800

    Removed healthcheck endpoint

--- backend/vehicles/src/app.controller.ts
+++ backend/vehicles/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('vehicles/healthcheck')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
