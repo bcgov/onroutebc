000e853834741425106672842a2ae239cef83d20
gchauhan-aot <garima.chauhan@aot-technologies.com>
2023-01-23 15:06:11 -0700

    Formatting and Lint changes

--- backend/vehicles/src/common/filters/http.filter.ts
+++ backend/vehicles/src/common/filters/http.filter.ts
@@ -1,22 +1,22 @@
 import {
-    ArgumentsHost,
-    Catch,
-    ExceptionFilter,
-    HttpException,
-  } from '@nestjs/common';
-  import { Response } from 'express';
-  import { ExceptionDto } from '../dto/exception.dto';
-  /*Catch all http exceptions */
-  @Catch(HttpException)
-  export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
-      console.log('HTTP exception handler triggered', JSON.stringify(exception));
-  
-      const ctx = host.switchToHttp();
-  
-      const response = ctx.getResponse<Response>(),
-        statusCode = exception.getStatus();
-      const exceptionDto = new ExceptionDto(statusCode, exception.message);
-      return response.status(statusCode).json(exceptionDto);
-    }
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { ExceptionDto } from '../dto/exception.dto';
+/*Catch all http exceptions */
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    console.log('HTTP exception handler triggered', JSON.stringify(exception));
+
+    const ctx = host.switchToHttp();
+
+    const response = ctx.getResponse<Response>(),
+      statusCode = exception.getStatus();
+    const exceptionDto = new ExceptionDto(statusCode, exception.message);
+    return response.status(statusCode).json(exceptionDto);
   }
+}
