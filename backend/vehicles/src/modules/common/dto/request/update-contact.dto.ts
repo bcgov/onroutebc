5654d3cab08da07a45adbf8f3c5f7777e79b9c1b
praju-aot <praveen.raju@aot-technologies.com>
2023-02-23 10:33:05 -0500

    formatting changes

--- backend/vehicles/src/modules/common/dto/request/update-contact.dto.ts
+++ backend/vehicles/src/modules/common/dto/request/update-contact.dto.ts
@@ -1,6 +1,3 @@
-import { AutoMap } from '@automapper/classes';
-import { ApiProperty } from '@nestjs/swagger';
 import { CreateContactDto } from './create-contact.dto';
 
-export class UpdateContactDto extends CreateContactDto {
-}
+export class UpdateContactDto extends CreateContactDto {}
