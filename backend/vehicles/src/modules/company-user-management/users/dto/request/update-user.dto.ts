e0f92c718a0d8c2b39729ca965302d202a49adef
praju-aot <praveen.raju@aot-technologies.com>
2023-03-17 13:06:30 -0400

    ORV2-579 Backend Changes

--- backend/vehicles/src/modules/company-user-management/users/dto/request/update-user.dto.ts
+++ backend/vehicles/src/modules/company-user-management/users/dto/request/update-user.dto.ts
@@ -11,7 +11,7 @@ export class UpdateUserDto extends UpdateContactDto {
   @ApiProperty({
     enum: UserAuthGroup,
     description: 'The user auth group.',
-    example: UserAuthGroup.ADMIN,
+    example: UserAuthGroup.COMPANY_ADMINISTRATOR,
   })
   userAuthGroup: UserAuthGroup;
 }
