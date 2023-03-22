4c191569ff859aa60226a488aa452802bc9b93f1
praju-aot <praveen.raju@aot-technologies.com>
2023-03-22 11:23:53 -0400

    Rename helper function to isCAorUS

--- backend/vehicles/src/common/helper/province-country.helper.ts
+++ backend/vehicles/src/common/helper/province-country.helper.ts
@@ -1,4 +1,4 @@
-export const isCAOrUA = (countryCode: string) => {
+export const isCAOrUS = (countryCode: string) => {
   return countryCode === 'CA' || countryCode === 'US' ? true : false;
 };
 
@@ -9,9 +9,9 @@ export const getCountryCode = (provinceId: string) => {
 
 export const getProvinceCode = (provinceId: string) => {
   const province = provinceId.split('-');
-  return isCAOrUA(province[0]) ? province[1] : undefined;
+  return isCAOrUS(province[0]) ? province[1] : undefined;
 };
 
 export const getProvinceId = (countryCode: string, provinceCode: string) => {
-  return countryCode + '-' + (isCAOrUA(countryCode) ? provinceCode : 'XX');
+  return countryCode + '-' + (isCAOrUS(countryCode) ? provinceCode : 'XX');
 };
