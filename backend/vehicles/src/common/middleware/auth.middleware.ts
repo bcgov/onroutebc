e9ea7f2f39033b7f0b1988ad7d1e9c7c5ba42cc3
gchauhan-aot <garima.chauhan@aot-technologies.com>
2023-03-28 12:22:15 -0600

    To resolve Sonar cloud warning

--- backend/vehicles/src/common/middleware/auth.middleware.ts
+++ backend/vehicles/src/common/middleware/auth.middleware.ts
@@ -15,7 +15,6 @@ export class AuthMiddleware implements NestMiddleware {
       req.headers.authorization.split(' ')[0] === 'Bearer'
     ) {
       token = req.headers.authorization.split(' ')[1];
-      req.headers.authorization.split(' ')[1];
     } else {
       token =
         'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJxUWlFWDB2T2Z1SlBuWUw4MWo0Q2tDOHVPdEJ1aFZvM0xBd2ppczZWbHRzIn0.eyJleHAiOjE2Nzk1MTM3NzMsImlhdCI6MTY3OTUxMzQ3MywiYXV0aF90aW1lIjoxNjc5NTEzNDczLCJqdGkiOiI1Mzc1NWRkYy1hZjVmLTQ1MDEtYjlkOC1lOTg3ZDU2MjYxMTIiLCJpc3MiOiJodHRwczovL2Rldi5sb2dpbnByb3h5Lmdvdi5iYy5jYS9hdXRoL3JlYWxtcy9zdGFuZGFyZCIsImF1ZCI6Im9uLXJvdXRlLWJjLWRpcmVjdC00NTk4Iiwic3ViIjoiMDYyNjc5NDVmMmViNGUzMWI1ODU5MzJmNzhiNzYyNjlAYmNlaWRib3RoIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoib24tcm91dGUtYmMtZGlyZWN0LTQ1OTgiLCJzZXNzaW9uX3N0YXRlIjoiNmI1MDhlMWMtYmM2Ni00OGE5LTg5N2MtNjdhOTYwZTExN2I3Iiwic2NvcGUiOiJvcGVuaWQgaWRpciBiY2VpZGJvdGggZW1haWwgcHJvZmlsZSIsInNpZCI6IjZiNTA4ZTFjLWJjNjYtNDhhOS04OTdjLTY3YTk2MGUxMTdiNyIsImlkZW50aXR5X3Byb3ZpZGVyIjoiYmNlaWRib3RoIiwiYmNlaWRfdXNlcl9ndWlkIjoiMDYyNjc5NDVGMkVCNEUzMUI1ODU5MzJGNzhCNzYyNjkiLCJiY2VpZF91c2VybmFtZSI6InJlZHRydWNrIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiSGFycnkgRXdpbmciLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiIwNjI2Nzk0NWYyZWI0ZTMxYjU4NTkzMmY3OGI3NjI2OUBiY2VpZGJvdGgiLCJnaXZlbl9uYW1lIjoiSGFycnkgRXdpbmciLCJkaXNwbGF5X25hbWUiOiJIYXJyeSBFd2luZyIsImZhbWlseV9uYW1lIjoiIiwiZW1haWwiOiJjaHJpcy5yb2JpbnNvbkBnb3YuYmMuY2EifQ.N8IJ-IGgEi-H0ZYhJaOPwaoQzFjtyGMNIEl11Cm9A_klWYAcP1jNepvuHXavig6k4bUlhAnHZdtVtz-N9zA6NDBKbay6tdQrt3pZ9_1_OrtTfMUuMwNIdJKlIxYW99t1kaJChv1hAJwcTUIVp9T1iQ6igH4l99bP5t7hYq2qWERaYBSAebH3gksM8TO6-vLrz0wM0_N9uZCSnceMA17Yhf-d2mJHHtdOuMyY5DO21Pht080KULFrVtYDBjsJC8kPb5VpvGlCh69K_2dNJzqdsMO_XDJ9we8aZM92n3cPdaTNZpwXdtE3ove_3rzT6DPtKYKc81HOst-lLZ4cCnjtCQ';
@@ -23,7 +22,6 @@ export class AuthMiddleware implements NestMiddleware {
     try {
       const loginUserDto: LoginUserDto = this.decodeToken(token);
       const userDetails: UserDetailsDto = new UserDetailsDto();
-      // req.userDetails = loginUserDto;
 
       this.getRoles(req, loginUserDto)
         .then((value) => {
@@ -40,10 +38,6 @@ export class AuthMiddleware implements NestMiddleware {
         });
 
       console.log('Request loginuser ', req.userDetails);
-
-      // if (this.checkTokenExpired(loginUserDto.exp)) {
-      //  return res.status(401).json('Token expired');
-      //   }
     } catch (err) {
       return res.status(403).json(err);
     }
@@ -72,8 +66,6 @@ export class AuthMiddleware implements NestMiddleware {
     req: Request,
     loginUser: LoginUserDto,
   ): Promise<UserDetailsDto> {
-    //loginuser.userAuthGroup = [];
-    //loginuser.action = [];
     const userDetailsdto: UserDetailsDto = new UserDetailsDto();
     userDetailsdto.userCompany = new UserCompanyRoleDto();
     userDetailsdto.userCompany.userAuthGroup = [];
@@ -91,8 +83,6 @@ export class AuthMiddleware implements NestMiddleware {
       console.log('After calling findUserDetailsWithCompanyId');
       //  loginUser.userCompany = [] as UsercompanyRoleDto[];
       for (const companyUserRole of companyUserRoles) {
-        //userDetailsdto.userCompany.userRoles = [];
-
         userDetailsdto.statusCode = companyUserRole.user.statusCode;
         console.log('Status Code ', userDetailsdto.statusCode);
         userDetailsdto.userGUID = companyUserRole.user.userGUID;
