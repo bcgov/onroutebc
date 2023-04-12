9758a2a1872503105eee732fbf2e92971808bb31
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2023-04-12 10:47:37 -0700

    Revert "Merging Email Changes from ORV2-477"
    This reverts commit a39eeb10c0e5f0cf1d2adec6116d935439eee9c4.

--- backend/vehicles/src/app.controller.ts
+++ backend/vehicles/src/app.controller.ts
@@ -1,15 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
-import { EmailService } from './modules/common/email.service';
 
 @ApiTags('Health Check')
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly emailService: EmailService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
--- backend/vehicles/src/modules/company-user-management/company/company.module.ts
+++ backend/vehicles/src/modules/company-user-management/company/company.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CommonModule } from '../../common/common.module';
 import { UsersModule } from '../users/users.module';
 import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
@@ -8,11 +7,7 @@ import { Company } from './entities/company.entity';
 import { CompanyProfile } from './profiles/company.profile';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Company]),
-    forwardRef(() => UsersModule),
-    CommonModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Company]), forwardRef(() => UsersModule)],
   controllers: [CompanyController],
   providers: [CompanyService, CompanyProfile],
   exports: [CompanyService],
