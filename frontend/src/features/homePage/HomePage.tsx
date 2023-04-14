136e2b0d1273e802d6bf73e773685a8cc507f862
Devin Bushey <devin.bushey@aot-technologies.com>
2023-04-14 14:31:48 -0700

    Use user data in welcome page

--- frontend/src/features/homePage/HomePage.tsx
+++ frontend/src/features/homePage/HomePage.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import { useAuth } from "react-oidc-context";
 
 export const HomePage = React.memo(() => {
-  const { isAuthenticated, user } = useAuth();
+  const { isAuthenticated } = useAuth();
+
+  // TODO Clean this up
+  const userInfo: any = sessionStorage.getItem("onRoutebc.user.context");
+  const userJson = JSON.parse(userInfo);
+
   const DEPLOY_ENV =
     import.meta.env.VITE_DEPLOY_ENVIRONMENT ||
     envConfig.VITE_DEPLOY_ENVIRONMENT;
 
   return (
     <div style={{ padding: "0px 60px", height: "100vh" }}>
-      {isAuthenticated && <div>{`Hello ${user?.profile?.display_name}`}</div>}
+      {isAuthenticated && (
+        <div>{`Hello ${userJson.firstName} ${userJson.lastName}`}</div>
+      )}
       <p>OnRouteBC Home -{DEPLOY_ENV}- Environment</p>
     </div>
   );
