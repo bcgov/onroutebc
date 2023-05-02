703c3675cfe248265d7ac632a618b0dae4a8e388
gchauhan-aot <113390759+gchauhan-aot@users.noreply.github.com>
2023-05-02 16:07:40 -0600

    ORV2 711 AND ORV2 713: Changes For Creating And Viewing an application  (#367)

---
+++ backend/vehicles/src/modules/permit/profile/application.profile.ts
@@ -0,0 +1,42 @@
+import { AutomapperProfile, InjectMapper } from '@automapper/nestjs';
+import { createMap, forMember, mapFrom, Mapper } from '@automapper/core';
+import { Injectable } from '@nestjs/common';
+import { Permit } from '../entities/permit.entity';
+import { CreateApplicationDto } from '../dto/request/create-application.dto';
+import { ReadApplicationDto } from '../dto/response/read-application.dto';
+
+@Injectable()
+export class ApplicationProfile extends AutomapperProfile {
+  constructor(@InjectMapper() mapper: Mapper) {
+    super(mapper);
+  }
+  override get profile() {
+    return (mapper: Mapper) => {
+      createMap(
+        mapper,
+        CreateApplicationDto,
+        Permit,
+        forMember(
+          (d) => d.permitData?.permitData,
+          mapFrom((s) => {
+            return s.permitData ? JSON.stringify(s.permitData) : undefined;
+          }),
+        ),
+      );
+
+      createMap(
+        mapper,
+        Permit,
+        ReadApplicationDto,
+        forMember(
+          (d) => d.permitData,
+          mapFrom((s) => {
+            return s.permitData?.permitData
+              ? (JSON.parse(s.permitData?.permitData) as JSON)
+              : undefined;
+          }),
+        ),
+      );
+    };
+  }
+}
