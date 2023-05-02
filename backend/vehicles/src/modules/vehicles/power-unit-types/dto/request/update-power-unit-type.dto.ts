c5df8343e13c9a3970b39cd4c8373bfad007c672
praju-aot <praveen.raju@aot-technologies.com>
2023-05-02 10:42:47 -0400

    ORv2-696 Vehicle Validations

--- backend/vehicles/src/modules/vehicles/power-unit-types/dto/request/update-power-unit-type.dto.ts
+++ backend/vehicles/src/modules/vehicles/power-unit-types/dto/request/update-power-unit-type.dto.ts
@@ -1,5 +1,11 @@
 import { AutoMap } from '@automapper/classes';
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNumberString,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class UpdatePowerUnitTypeDto {
   @AutoMap()
@@ -7,6 +13,8 @@ export class UpdatePowerUnitTypeDto {
     example: 'Concrete Pumper Trucks',
     description: 'Short description of the power unit type.',
   })
+  @IsString()
+  @MaxLength(150)
   type: string;
 
   @AutoMap()
@@ -14,13 +22,21 @@ export class UpdatePowerUnitTypeDto {
     example:
       'Concrete Pumper Trucks are used to pump concrete from a cement mixer truck to where the concrete is actually needed. They travel on the highway at their equipped weight with no load.',
     description: 'Long description of the power unit type.',
+    required: false,
   })
-  description: string;
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  description?: string;
 
   @AutoMap()
   @ApiProperty({
     example: '1',
     description: 'Order that the type should be presented in user interfaces.',
+    required: false,
   })
-  sortOrder: string;
+  @IsOptional()
+  @IsString()
+  @IsNumberString()
+  sortOrder?: string;
 }
