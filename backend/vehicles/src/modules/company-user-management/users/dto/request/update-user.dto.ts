e7a1e04568ceee3ba15bcffeaf07e30c22712267
praju-aot <praveen.raju@aot-technologies.com>
2023-05-03 13:06:07 -0400

    Validation on Enum

--- backend/vehicles/src/modules/company-user-management/users/dto/request/update-user.dto.ts
+++ backend/vehicles/src/modules/company-user-management/users/dto/request/update-user.dto.ts
@@ -2,7 +2,7 @@ import { AutoMap } from '@automapper/classes';
 import { ApiProperty } from '@nestjs/swagger';
 import { UserAuthGroup } from '../../../../../common/enum/user-auth-group.enum';
 import { UpdateContactDto } from '../../../../common/dto/request/update-contact.dto';
-import { Allow } from 'class-validator';
+import { IsEnum } from 'class-validator';
 
 /**
  * JSON representation of request object for updating user information.
@@ -14,6 +14,6 @@ export class UpdateUserDto extends UpdateContactDto {
     description: 'The user auth group.',
     example: UserAuthGroup.COMPANY_ADMINISTRATOR,
   })
-  @Allow()
+  @IsEnum(UserAuthGroup)
   userAuthGroup: UserAuthGroup;
 }
