4f3cea2fec78ef2584765e49d2cbb2ac54aff0ab
Devin Bushey <devin.bushey@aot-technologies.com>
2023-05-05 08:32:34 -0700

    Replace undefined and empty fields with null in request body

--- frontend/src/common/helpers/util.ts
+++ frontend/src/common/helpers/util.ts
@@ -25,3 +25,32 @@ export const removeEmptyValues = (obj: object): object => {
       })
   );
 };
+
+/**
+ * Replace undefined and empty fields with null (including arrays).
+ * 
+ * The function takes an obj parameter, which can be any JavaScript object.
+ * If the object is an array, it calls the replaceEmptyWithNull function recursively on each item in the array and returns the resulting array.
+ * If the object is an object (i.e., not an array and not null), it uses Object.entries to get an array of [key, value] pairs for each property in the object. It then reduces that array into a new object with the same keys, but with each value replaced by the result of calling replaceEmptyWithNull on it.
+ * If the object is not an array and not an object, it checks whether the value is undefined or an empty string (''). If it is, it returns null. Otherwise, it returns the original value.
+
+ * @param obj The object to replace empty values from.
+ * @returns An Object with only valid values.
+ *
+ * @see https://dev.to/typescripttv/what-is-the-difference-between-null-and-undefined-5h76
+ */
+export const replaceEmptyValuesWithNull = (obj: object): any => {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => replaceEmptyValuesWithNull(item));
+  } 
+  else if (typeof obj === 'object' && obj !== null) {
+    // acc is a shorthand name for the accumulator object that's being built up by the reduce function
+    return Object.entries(obj).reduce((acc : any, [key, value]) => {
+      acc[key] = replaceEmptyValuesWithNull(value);
+      return acc;
+    }, {});
+  } 
+  else {
+    return obj === undefined || obj === '' ? null : obj;
+  }
+}
