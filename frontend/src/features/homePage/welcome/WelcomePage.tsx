4651a9a46616603e714a65da1690e1883faa04b1
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2023-05-08 15:33:35 -0700

    Fix layout styling issue on wizard feature

--- frontend/src/features/homePage/welcome/WelcomePage.tsx
+++ frontend/src/features/homePage/welcome/WelcomePage.tsx
@@ -1,71 +1,61 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import { useNavigate } from "react-router-dom";
 import "./welcome.scss";
-import { CardActionArea, CardContent, Typography } from "@mui/material";
+import { CardActionArea, CardContent, Chip, Typography } from "@mui/material";
 
 export const WelcomePage = React.memo(() => {
   const navigate = useNavigate();
   return (
-    <div>
-      <Grid container spacing={3} sx={{ flexGrow: 1 }} alignItems="center">
-        <Grid xs={4} item={false}></Grid>
-        <Grid xs={6} justifyContent="center" item={true}>
-          <br></br>
+    <div className="welcome-page">
+      <div className="welcome-page__main">
+        <div className="welcome-page__header">
           <div className="welcome-graphic"></div>
           <h2>Welcome to onRouteBC!</h2>
-          <Grid container>
-            <Grid xs={4} justifyContent="center" item={true}>
-              <Card className="welcome-cards" elevation={12}>
-                <CardActionArea>
-                  <Grid container>
-                    <Grid xs={4}></Grid>
-                    <Grid xs={1}>
-                      <img
-                        height="80"
-                        width="80"
-                        className="welcome-account-graphics"
-                        src="./Existing_Account_Graphic.svg"
-                      ></img>
-                    </Grid>
-                    <Grid xs={3}></Grid>
-                  </Grid>
-                  <CardContent>
-                    <Typography variant="body2">
-                      Claim an existing TPS Profile
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-            <Grid xs={4} justifyContent="center" item={true}>
-              <Card className="welcome-cards" elevation={12}>
-                <CardActionArea onClick={() => navigate("/create-profile")}>
-                  <Grid container>
-                    <Grid xs={4}></Grid>
-                    <Grid xs={1}>
-                      <img
-                        height="80"
-                        width="80"
-                        className="welcome-account-graphics"
-                        src="./Create_New_Profile_Graphic.svg"
-                      ></img>
-                    </Grid>
-                    <Grid xs={3}></Grid>
-                  </Grid>
-                  <CardContent>
-                    <Typography variant="body2">
-                      Create a new onRouteBC Profile
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-      <br></br>
+        </div>
+        <div className="separator-line">
+          <Chip className="separator-line__label" label="Choose An Option Below" />
+        </div>
+        <div className="welcome-page__profile-actions">
+          <Card className="welcome-cards welcome-cards--existing" elevation={12}>
+            <CardActionArea>
+              <div className="welcome-cards__img">
+                <img
+                  height="80"
+                  width="80"
+                  className="welcome-account-graphics"
+                  src="./Existing_Account_Graphic.svg"
+                  alt="Existing TPS Profile"
+                />
+              </div>
+              <CardContent>
+                <Typography variant="body2">
+                  Claim an existing TPS Profile
+                </Typography>
+              </CardContent>
+              <Chip className="welcome-cards__recommend" label="Recommend" />
+            </CardActionArea>
+          </Card>
+          <Card className="welcome-cards welcome-cards--new" elevation={12}>
+            <CardActionArea onClick={() => navigate("/create-profile")}>
+              <div className="welcome-cards__img">
+                <img
+                  height="80"
+                  width="80"
+                  className="welcome-account-graphics"
+                  src="./Create_New_Profile_Graphic.svg"
+                  alt="New onRouteBC Profile"
+                />
+              </div>
+              <CardContent>
+                <Typography variant="body2">
+                  Create a new onRouteBC Profile
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 });
