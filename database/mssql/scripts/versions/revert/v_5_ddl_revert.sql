e32776de24128ca22b5f4f9cfc5db94a899f4c1c
Devin Bushey <devin.bushey@aot-technologies.com>
2023-05-19 10:04:34 -0700

    Added revert script

---
+++ database/mssql/scripts/versions/revert/v_5_ddl_revert.sql
@@ -0,0 +1,16 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET NOCOUNT ON
+GO
+BEGIN TRANSACTION
+
+DROP TABLE [dbo].[ORBC_PDF_TEMPLATE]
+
+DECLARE @VersionDescription VARCHAR(255)
+SET @VersionDescription = 'Reverting initial creation of entities for pdf generation feature'
+
+INSERT [dbo].[ORBC_SYS_VERSION] ([VERSION_ID], [DESCRIPTION], [DDL_FILE_SHA1], [RELEASE_DATE]) VALUES (4, @VersionDescription, '$(FILE_HASH)', getdate())
+
+COMMIT
