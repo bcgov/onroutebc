8a51c149cb120fc65f273112c38d15335df7ad6d
praju-aot <praveen.raju@aot-technologies.com>
2023-05-26 16:06:45 -0400

    ORV2-878 Sample Data for ORBC_DOCUMENT

--- database/mssql/scripts/utility/refresh-sample-data.sh
+++ database/mssql/scripts/utility/refresh-sample-data.sh
@@ -6,6 +6,7 @@ USAGE="-u USER -p PASS -s SERVER -d DATABASE"
 parse_options "${USAGE}" ${@}
 
 echo "Deleting existing sample data"
+sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -Q "DELETE FROM dbo.ORBC_DOCUMENT"
 sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -Q "DELETE FROM dbo.ORBC_PDF_TEMPLATE"
 sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -Q "DELETE FROM dbo.ORBC_TRAILER"
 sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -Q "DELETE FROM dbo.ORBC_POWER_UNIT"
@@ -25,4 +26,5 @@ sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -i ${SCRIPT_DIR}/samp
 sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -i ${SCRIPT_DIR}/sampledata/dbo.ORBC_POWER_UNIT.Table.sql
 sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -i ${SCRIPT_DIR}/sampledata/dbo.ORBC_TRAILER.Table.sql
 sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -i ${SCRIPT_DIR}/sampledata/dbo.ORBC_PDF_TEMPLATE.Table.sql
+sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -i ${SCRIPT_DIR}/sampledata/dbo.ORBC_DOCUMENT.Table.sql
 echo "Finished loading sample data"
