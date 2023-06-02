6b55a4f8f438f370deb4bfbc7c9dba1fb01d51b2
Devin Bushey <devin.bushey@aot-technologies.com>
2023-06-01 17:33:53 -0700

    Fixed unit and e2e tests

--- backend/vehicles/test/e2e/app.e2e-spec.ts
+++ backend/vehicles/test/e2e/app.e2e-spec.ts
@@ -3,14 +3,51 @@ import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppController } from '../../src/app.controller';
 import { AppService } from '../../src/app.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { PermitType } from 'src/modules/permit/entities/permit-type.entity';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PowerUnitTypesService } from 'src/modules/vehicles/power-unit-types/power-unit-types.service';
+import { TrailerTypesService } from 'src/modules/vehicles/trailer-types/trailer-types.service';
+import { CommonService } from 'src/modules/common/common.service';
+
+let repo: DeepMocked<Repository<PermitType>>;
+let powerUnitTypeServiceMock: DeepMocked<PowerUnitTypesService>;
+let trailerTypeServiceMock: DeepMocked<TrailerTypesService>;
+let commonServiceMock: DeepMocked<CommonService>;
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
+    repo = createMock<Repository<PermitType>>();
+    powerUnitTypeServiceMock = createMock<PowerUnitTypesService>();
+    trailerTypeServiceMock = createMock<TrailerTypesService>();
+    commonServiceMock = createMock<CommonService>();
+
     const moduleFixture = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        { provide: CACHE_MANAGER, useFactory: jest.fn() },
+        {
+          provide: getRepositoryToken(PermitType),
+          useValue: repo,
+        },
+        {
+          provide: PowerUnitTypesService,
+          useValue: powerUnitTypeServiceMock,
+        },
+        {
+          provide: TrailerTypesService,
+          useValue: trailerTypeServiceMock,
+        },
+        {
+          provide: CommonService,
+          useValue: commonServiceMock,
+        },
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
