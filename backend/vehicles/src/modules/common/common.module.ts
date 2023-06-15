93befee9e341644a470ab0076bf9e8d0c3dfc847
Praveen Raju <80779423+praju-aot@users.noreply.github.com>
2023-06-15 18:53:30 -0400

    ORV2-573: CHES integration - Iteration 1 (#511)

--- backend/vehicles/src/modules/common/common.module.ts
+++ backend/vehicles/src/modules/common/common.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Province } from './entities/province.entity';
 import { Country } from './entities/country.entity';
@@ -7,10 +7,15 @@ import { Address } from './entities/address.entity';
 import { AddressProfile } from './profiles/address.profile';
 import { ContactProfile } from './profiles/contact.profile';
 import { CommonService } from './common.service';
+import { HttpModule } from '@nestjs/axios';
 
+@Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([Country, Province, Address, Contact])],
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([Country, Province, Address, Contact]),
+  ],
   providers: [AddressProfile, ContactProfile, CommonService],
-  exports: [TypeOrmModule, AddressProfile, ContactProfile, CommonService],
+  exports: [HttpModule, AddressProfile, ContactProfile, CommonService],
 })
 export class CommonModule {}
