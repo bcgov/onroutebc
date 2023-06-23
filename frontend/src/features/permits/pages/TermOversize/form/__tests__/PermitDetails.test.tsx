1e340b99d2b1feb55457a1062e91f5854caa2d8f
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2023-06-22 18:06:24 -0700

    Add remaining tests for start date on permit details

--- frontend/src/features/permits/pages/TermOversize/form/__tests__/PermitDetails.test.tsx
+++ frontend/src/features/permits/pages/TermOversize/form/__tests__/PermitDetails.test.tsx
@@ -1,7 +1,7 @@
 import { FormProvider, useForm } from "react-hook-form";
 import { PermitDetails } from "../PermitDetails";
 import { DATE_FORMATS, now } from "../../../../../../common/helpers/formatDate";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent, { PointerEventsCheckLevel } from "@testing-library/user-event";
 import dayjs from "dayjs";
 import { TROS_COMMODITIES } from "../../../../constants/termOversizeConstants";
@@ -9,7 +9,13 @@ import { TROS_COMMODITIES } from "../../../../constants/termOversizeConstants";
 const currentDt = now();
 const tomorrow = dayjs(currentDt).add(1, "day");
 const day = currentDt.date();
+const thisMonth = currentDt.month();
+const daysInCurrMonth = currentDt.daysInMonth();
 const tomorrowDay = tomorrow.date();
+const maxFutureDate = dayjs(currentDt).add(14, "day");
+const maxFutureMonth = maxFutureDate.month();
+const maxFutureDay = maxFutureDate.date();
+const daysInFutureMonth = maxFutureDate.daysInMonth();
 
 const commodities = [...TROS_COMMODITIES];
 
@@ -115,10 +121,186 @@ describe("Permit Details start date", () => {
 
     // Assert
     const dateOptions = await screen.findAllByRole("gridcell");
-    const todayIndex = dateOptions.findIndex(option => option.textContent === String(day));
-    if (todayIndex > 0 && day > 1) {
-      expect(dateOptions[todayIndex - 1]).toBeDisabled();
+    const thisMonthBeforeTodayIndices = dateOptions
+      .map((option, i) => {
+        const isBeforeToday = 
+          option.textContent && Number(option.textContent) >= 1 && Number(option.textContent) < day;
+        return isBeforeToday ? i : -1;
+      })
+      .filter(i => i >= 0);
+
+    const beforeTodayDisabledIndices = thisMonthBeforeTodayIndices
+      .filter(i => {
+        return dateOptions[i].hasAttribute("disabled");
+      });
+    
+    expect(await screen.findByTitle(/Previous month/i)).toBeDisabled();
+    expect(beforeTodayDisabledIndices.length).toBe(day - 1);
+  });
+
+  it("should have disabled date options after 14 days from today", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    render(
+      <TestFormWrapper>
+        <PermitDetails
+          feature="testfeature"
+          defaultStartDate={currentDt}
+          defaultDuration={30}
+          commodities={[]}
+        />
+      </TestFormWrapper>
+    );
+
+    // Act
+    const startDateSelect = await screen.findByTestId("CalendarIcon");
+    await user.click(startDateSelect);
+    const nextMonthButton = await screen.findByTitle(/Next month/i);
+    
+    if (maxFutureMonth > thisMonth) {
+      await user.click(nextMonthButton);
+
+      await waitFor(async () => {
+        // There's a period of time where all cells for both last month and this month are shown,
+        // so we need to wait for last month cells to disappear
+        const allCells = await screen.findAllByRole("gridcell");
+        const dateCells = allCells.filter(
+          cell => cell.textContent && Number(cell.textContent) > 0 && Number(cell.textContent) <= 31
+        );
+        expect(dateCells.length).toBeLessThanOrEqual(31);
+      });
     }
+
+    // Assert
+    const dateOptions = await screen.findAllByRole("gridcell");
+    const afterMaxFutureDayIndices = dateOptions
+      .map((option, i) => {
+        const isAfterMaxFutureDay = 
+          option.textContent && Number(option.textContent) > maxFutureDay && Number(option.textContent) <= daysInFutureMonth;
+        return isAfterMaxFutureDay ? i : -1;
+      })
+      .filter(i => i >= 0);
+    
+    const afterMaxFutureDayDisabledIndices = afterMaxFutureDayIndices
+      .filter(i => {
+        return dateOptions[i].hasAttribute("disabled");
+      });
+    expect(afterMaxFutureDayDisabledIndices.length).toBe(daysInFutureMonth - maxFutureDay);
+  });
+
+  it("should accept start dates with 14 days from today", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    render(
+      <TestFormWrapper>
+        <PermitDetails
+          feature="testfeature"
+          defaultStartDate={currentDt}
+          defaultDuration={30}
+          commodities={[]}
+        />
+      </TestFormWrapper>
+    );
+
+    // Act
+    const startDateSelect = await screen.findByTestId("CalendarIcon");
+    await user.click(startDateSelect);
+    const nextMonthButton = await screen.findByTitle(/Next month/i);
+    
+    // Assert
+    if (maxFutureMonth > thisMonth) {
+      // First find this month's active days
+      const dateOptions = await screen.findAllByRole("gridcell");
+      const indicesBetweenTodayToEndMonth = dateOptions
+        .map((option, i) => {
+          const isBetween = 
+            option.textContent && Number(option.textContent) >= day && Number(option.textContent) <= daysInCurrMonth;
+          return isBetween ? i : -1;
+        })
+        .filter(i => i >= 0);
+      
+      const activeIndices = indicesBetweenTodayToEndMonth
+        .filter(i => {
+          return !dateOptions[i].hasAttribute("disabled");
+        });
+      expect(activeIndices.length).toBe(daysInCurrMonth - day + 1);
+
+      // Next, check active dates for next month
+      await user.click(nextMonthButton);
+      await waitFor(async () => {
+        // There's a period of time where all cells for both last month and this month are shown,
+        // so we need to wait for last month cells to disappear
+        const allCells = await screen.findAllByRole("gridcell");
+        const dateCells = allCells.filter(
+          cell => cell.textContent && Number(cell.textContent) > 0 && Number(cell.textContent) <= 31
+        );
+        expect(dateCells.length).toBeLessThanOrEqual(31);
+      });
+
+      const nextMonthDateOptions = await screen.findAllByRole("gridcell");
+      const indicesBetweenStartNextMonthToMaxFutureDay = nextMonthDateOptions
+        .map((option, i) => {
+          const isBetween = 
+            option.textContent && Number(option.textContent) >= 1 && Number(option.textContent) <= maxFutureDay;
+          return isBetween ? i : -1;
+        })
+        .filter(i => i >= 0);
+      
+      const activeIndicesNextMonth = indicesBetweenStartNextMonthToMaxFutureDay
+        .filter(i => {
+          return !nextMonthDateOptions[i].hasAttribute("disabled");
+        });
+      expect(activeIndicesNextMonth.length).toBe(maxFutureDay);
+    } else {
+      const dateOptions = await screen.findAllByRole("gridcell");
+      const indicesBetweenTodayAndMaxFutureDay = dateOptions
+        .map((option, i) => {
+          const isBetween = 
+            option.textContent && Number(option.textContent) >= day && Number(option.textContent) <= maxFutureDay;
+          return isBetween ? i : -1;
+        })
+        .filter(i => i >= 0);
+      
+      const activeIndices = indicesBetweenTodayAndMaxFutureDay
+        .filter(i => {
+          return !dateOptions[i].hasAttribute("disabled");
+        });
+      expect(activeIndices.length).toBe(15); // today and next 14 days
+    }
+  });
+
+  it("should display error message for invalid past start dates", async () => {
+    // Arrange and Act
+    render(
+      <TestFormWrapper>
+        <PermitDetails
+          feature="testfeature"
+          defaultStartDate={dayjs(currentDt).subtract(1, "day")}
+          defaultDuration={30}
+          commodities={[]}
+        />
+      </TestFormWrapper>
+    );
+
+    // Assert
+    expect(await screen.findByText(/^Start date cannot be in the past/i)).toBeVisible();
+  });
+
+  it("should display error message for invalid future start dates (more than 14 days from today)", async () => {
+    // Arrange and Act
+    render(
+      <TestFormWrapper>
+        <PermitDetails
+          feature="testfeature"
+          defaultStartDate={dayjs(currentDt).add(15, "day")}
+          defaultDuration={30}
+          commodities={[]}
+        />
+      </TestFormWrapper>
+    );
+
+    // Assert
+    expect(await screen.findByText(/^Start date must be within 14 days/i)).toBeVisible();
   });
 
   it("should display correct expiry date after selecting start date", async () => {
