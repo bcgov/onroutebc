ba1cf43d33f75d6c4b2a267605cd158a59254649
praju-aot <praveen.raju@aot-technologies.com>
2023-07-10 13:35:31 -0400

    DB bugfix and changes

--- database/mssql/scripts/versions/revert/v_5_ddl_revert.sql
+++ database/mssql/scripts/versions/revert/v_5_ddl_revert.sql
@@ -6,7 +6,9 @@ SET NOCOUNT ON
 GO
 BEGIN TRANSACTION
 
+ALTER TABLE [dops].ORBC_DOCUMENT_TEMPLATE DROP CONSTRAINT [UK_ORBC_DOCUMENT_TEMPLATE]
 DROP TABLE [dops].[ORBC_DOCUMENT_TEMPLATE]
+DROP SCHEMA [dops]
 
 DECLARE @VersionDescription VARCHAR(255)
 SET @VersionDescription = 'Reverting initial creation of entities for document generation feature'
