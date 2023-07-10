3b5344aff26701704ccf9fde3a49d4bcd409d71f
praju-aot <praveen.raju@aot-technologies.com>
2023-07-10 11:18:12 -0400

    Fix test specs

--- backend/dops/test/unit/app/app.controller.spec.ts
+++ backend/dops/test/unit/app/app.controller.spec.ts
@@ -1,14 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from '../../../src/app.controller';
 import { AppService } from '../../../src/app.service';
+import { DgenService } from '../../../src/modules/dgen/dgen.service';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { ComsService } from '../../../src/modules/common/coms.service';
+import { DmsService } from '../../../src/modules/dms/dms.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 describe('AppController', () => {
   let appController: AppController;
 
+  let dgenServiceMock: DeepMocked<DgenService>;
+  let comsServiceMock: DeepMocked<ComsService>;
+  let dmsServiceMock: DeepMocked<DmsService>;
+
   beforeEach(async () => {
+    dgenServiceMock = createMock<DgenService>();
+    comsServiceMock = createMock<ComsService>();
+    dmsServiceMock = createMock<DmsService>();
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        { provide: CACHE_MANAGER, useFactory: jest.fn() },
+        {
+          provide: DgenService,
+          useValue: dgenServiceMock,
+        },
+        {
+          provide: ComsService,
+          useValue: comsServiceMock,
+        },
+        {
+          provide: DmsService,
+          useValue: dmsServiceMock,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
