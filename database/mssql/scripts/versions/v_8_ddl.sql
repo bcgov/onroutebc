0d96704a4a3a427fa6bf654170aba63cc380aa4d
gchauhan-aot <113390759+gchauhan-aot@users.noreply.github.com>
2023-08-10 17:19:42 -0600

    Fix for 1311 (#594)

--- database/mssql/scripts/versions/v_8_ddl.sql
+++ database/mssql/scripts/versions/v_8_ddl.sql
@@ -10,9 +10,6 @@ CREATE FULLTEXT INDEX ON [permit].[ORBC_PERMIT_DATA](PERMIT_DATA) KEY INDEX PK_O
 GO
 BEGIN TRANSACTION
 
-DECLARE @VersionDescription VARCHAR(255)
-SET @VersionDescription = 'Creation of full text search index on ORBC_PERMIT_DATA table.'
-
 CREATE SEQUENCE [permit].[ORBC_TRANSACTION_NUMBER_SEQ] 
  AS [bigint]
  START WITH 1
@@ -21,6 +18,9 @@ CREATE SEQUENCE [permit].[ORBC_TRANSACTION_NUMBER_SEQ]
  MAXVALUE 9999999999
  CACHE 
 GO 
+
+DECLARE @VersionDescription VARCHAR(255)
+SET @VersionDescription = 'Creation of full text search index on ORBC_PERMIT_DATA table.'
 
 INSERT [dbo].[ORBC_SYS_VERSION] ([VERSION_ID], [DESCRIPTION], [DDL_FILE_SHA1], [RELEASE_DATE]) VALUES (8, @VersionDescription, '$(FILE_HASH)', getutcdate())
 
