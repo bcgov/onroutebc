3e469a429d1da95cdff115fd0684793a38fc6cbe
Krishnan Subramanian <84348052+krishnan-aot@users.noreply.github.com>
2023-08-18 12:54:07 -0700

    ORV2-1127 IDIR Search, Login Context, Role setting (#604)

--- frontend/src/features/manageProfile/apiManager/hooks.ts
+++ frontend/src/features/manageProfile/apiManager/hooks.ts
@@ -1,15 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import {
   getCompanyInfo,
+  getIDIRUserRoles,
   getUserContext,
   getUserRolesByCompanyId,
 } from "./manageProfileAPI";
 import { FIVE_MINUTES } from "../../../common/constants/constants";
 import { useContext } from "react";
 import OnRouteBCContext, {
-  UserDetailContext,
+  BCeIDUserDetailContext,
+  IDIRUserDetailContext,
 } from "../../../common/authentication/OnRouteBCContext";
-import { UserContextType } from "../../../common/authentication/types";
+import {
+  BCeIDUserContextType,
+  IDIRUserContextType,
+} from "../../../common/authentication/types";
+import { BCeIDAuthGroup } from "../types/userManagement";
+import { useAuth } from "react-oidc-context";
 
 /**
  * Fetches company info of current user.
@@ -30,35 +37,65 @@ export const useCompanyInfoQuery = () => {
  * @returns UseQueryResult containing the query results.
  */
 export const useUserContext = () => {
-  const { setCompanyId, setUserDetails, setCompanyLegalName } = useContext(OnRouteBCContext);
+  const {
+    setCompanyId,
+    setUserDetails,
+    setCompanyLegalName,
+    setIDIRUserDetails,
+  } = useContext(OnRouteBCContext);
+  const { isAuthenticated, user: userFromToken } = useAuth();
   return useQuery({
     queryKey: ["userContext"],
     queryFn: getUserContext,
     cacheTime: 500,
     refetchOnMount: "always",
-    onSuccess: (userContextResponseBody: UserContextType) => {
-      const { user, associatedCompanies } = userContextResponseBody;
-      if (user?.userGUID) {
-        const companyId = associatedCompanies[0].companyId;
-        const legalName = associatedCompanies[0].legalName;
-        setCompanyId?.(() => companyId);
-        setCompanyLegalName?.(() => legalName);
-        const userDetails = {
-          firstName: user.firstName,
-          lastName: user.lastName,
-          userName: user.userName,
-          phone1: user.phone1,
-          phone1Extension: user.phone1Extension,
-          phone2: user.phone2,
-          phone2Extension: user.phone2Extension,
-          email: user.email,
-          fax: user.fax,
-        } as UserDetailContext;
-        setUserDetails?.(() => userDetails);
+    onSuccess: (
+      userContextResponseBody: BCeIDUserContextType | IDIRUserContextType
+    ) => {
+      if (
+        isAuthenticated &&
+        userFromToken?.profile?.identity_provider === "idir"
+      ) {
+        const { user } = userContextResponseBody as IDIRUserContextType;
+        if (user?.userGUID) {
+          const userDetails = {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            userName: user.userName,
+            email: user.email,
+            userAuthGroup: user.userAuthGroup,
+          } as IDIRUserDetailContext;
+          setIDIRUserDetails?.(() => userDetails);
+        }
+      } else {
+        const { user, associatedCompanies } =
+          userContextResponseBody as BCeIDUserContextType;
+        if (user?.userGUID) {
+          const companyId = associatedCompanies[0].companyId;
+          const legalName = associatedCompanies[0].legalName;
+          setCompanyId?.(() => companyId);
+          setCompanyLegalName?.(() => legalName);
+          const userDetails = {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            userName: user.userName,
+            phone1: user.phone1,
+            phone1Extension: user.phone1Extension,
+            phone2: user.phone2,
+            phone2Extension: user.phone2Extension,
+            email: user.email,
+            fax: user.fax,
+            userAuthGroup: user.userAuthGroup as BCeIDAuthGroup,
+          } as BCeIDUserDetailContext;
+          setUserDetails?.(() => userDetails);
 
-        // Setting the companyId to sessionStorage so that it can be
-        // used outside of react components.
-        sessionStorage.setItem("onRouteBC.user.companyId", companyId.toString());
+          // Setting the companyId to sessionStorage so that it can be
+          // used outside of react components.
+          sessionStorage.setItem(
+            "onRouteBC.user.companyId",
+            companyId.toString()
+          );
+        }
       }
     },
     retry: false,
@@ -75,6 +112,23 @@ export const useUserRolesByCompanyId = () => {
     queryKey: ["userRoles"],
     refetchInterval: FIVE_MINUTES,
     queryFn: getUserRolesByCompanyId,
+    onSuccess: (userRolesResponseBody: string[]) => {
+      setUserRoles?.(() => userRolesResponseBody);
+    },
+    retry: true,
+  });
+};
+
+/**
+ * Hook to set up the user roles after fetching the data from user-context api.
+ * @returns UseQueryResult containing the query results.
+ */
+export const useIDIRUserRoles = () => {
+  const { setUserRoles } = useContext(OnRouteBCContext);
+  return useQuery({
+    queryKey: ["userIDIRRoles"],
+    refetchInterval: FIVE_MINUTES,
+    queryFn: getIDIRUserRoles,
     onSuccess: (userRolesResponseBody: string[]) => {
       setUserRoles?.(() => userRolesResponseBody);
     },
