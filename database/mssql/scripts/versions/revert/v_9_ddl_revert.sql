8af893a85d39476c27934ca431df0953d7fc34a3
john-fletcher-aot <113134542+john-fletcher-aot@users.noreply.github.com>
2023-09-11 14:10:19 -0700

    Create data schema elements to support TPS migration (#638)

---
+++ database/mssql/scripts/versions/revert/v_9_ddl_revert.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET NOCOUNT ON
+GO
+BEGIN TRANSACTION
+
+DROP TABLE [tps].[ORBC_TPS_MIGRATED_PERMITS]
+DROP TABLE [tps].[LEGACY_TPS_CLIENT_DATA]
+DROP SCHEMA [tps]
+
+DECLARE @VersionDescription VARCHAR(255)
+SET @VersionDescription = 'Reverting creation of tables to store TPS migration staging data.'
+
+INSERT [dbo].[ORBC_SYS_VERSION] ([VERSION_ID], [DESCRIPTION], [DDL_FILE_SHA1], [RELEASE_DATE]) VALUES (8, @VersionDescription, '$(FILE_HASH)', getutcdate())
+
+COMMIT
