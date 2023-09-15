e12b841f2a3dd1be91047d4ab774b3a586533784
John Fletcher <john.fletcher@aot-technologies.com>
2023-09-15 13:43:48 -0700

    Add pending users to the PENDING_IDIR_USERS table when sample data is refreshed (from env variable)

---
+++ database/mssql/scripts/utility/refresh-sample-idir-users.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Retrieve arguments
+source ${SCRIPT_DIR}/utility/getopt.sh
+USAGE="-u USER -p PASS -s SERVER -d DATABASE"
+parse_options "${USAGE}" ${@}
+
+# Clear out all records from the pending IDIR users table
+sqlcmd -C -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -Q "DELETE FROM dbo.ORBC_PENDING_IDIR_USER"
+
+(
+# SAMPLE_PENDING_IDIR_USERS environment variable must be a string representing an array
+# of tuples in the format IDIR_USERNAME,AUTH_GROUP separated by single space characters.
+# For example "USER1,SYSADMIN USER2,EOFFICER"
+for i in ${SAMPLE_PENDING_IDIR_USERS}
+do 
+	IFS=","; set -- $i
+	echo "Adding pending IDIR user ${1} to auth group ${2}"
+  sqlcmd -C -U ${USER} -P "${PASS}" -S ${SERVER} -d ${DATABASE} -v IDIR_USERNAME=${1} AUTH_GROUP=${2} -i ${SCRIPT_DIR}/sampledata/dbo.ORBC_PENDING_IDIR_USER.Table.sql
+done
+)
