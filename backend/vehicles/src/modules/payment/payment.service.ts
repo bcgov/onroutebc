557344a61dc784e6ba83faff6f1121dd2ff1c3a7
gchauhan-aot <garima.chauhan@aot-technologies.com>
2023-10-19 12:09:00 -0600

    getting code from env file

--- backend/vehicles/src/modules/payment/payment.service.ts
+++ backend/vehicles/src/modules/payment/payment.service.ts
@@ -86,7 +86,9 @@ export class PaymentService {
       `&glDate=${date}` +
       `&paymentMethod=CC` +
       `&currency=CAD` +
-      `&revenue=1:039.18ACE.14691.8928.1800000.000000.0000:${transaction.totalTransactionAmount}`;
+      `&revenue=1:${process.env.GL_CODE}:${transaction.totalTransactionAmount}`;
+
+      console.log('Query string: ', queryString+process.env.PAYBC_API_KEY)
 
     // Generate the hash using the query string and the MD5 algorithm
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
