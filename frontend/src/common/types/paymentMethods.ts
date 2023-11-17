5bccd34a727bdf34a052b2563602fdba70270a73
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2023-11-17 10:13:35 -0800

    Trying out json and object name change

--- frontend/src/common/types/paymentMethods.ts
+++ frontend/src/common/types/paymentMethods.ts
@@ -29,7 +29,7 @@ export type PaymentTypeAsEnum =
 /**
  * The set of payment method types
  */
-export type PaymentMethodTypeId =
+export type PaymentMethodTypeCode =
   | "ACCOUNT"
   | "GA"
   | "CASH"
@@ -48,103 +48,17 @@ export type PaymentMethodTypeId =
  * MD - Mastercard Debit
  * DB - Debit
  */
-export type PaymentTypeId = "AM" | "PV" | "MC" | "MD" | "VI" | "DB";
+export type PaymentTypeCode = "AM" | "PV" | "MC" | "MD" | "VI" | "DB";
 
 /**
  * An object containing the PaymentMethodTypeId and PaymentTypeId values.
  *
  */
 export type PaymentMethodAndPaymentType = {
-  paymentMethodTypeId: PaymentMethodTypeId;
+  paymentMethodTypeCode: PaymentMethodTypeCode;
   /**
    * This field is named as paymentType instead of paymentTypeId to
    * remain consistent with backend.
    */
-  paymentType?: PaymentTypeId;
-};
-
-/**
- * The following record contains key value pairs for Payment Methods
- * and their associated paymentMethodType and paymentType values.
- */
-export const PaymentMethodAndTypeRecord: Record<
-  string,
-  PaymentMethodAndPaymentType
-> = {
-  Cash: {
-    paymentMethodTypeId: "CASH",
-  },
-  Cheque: {
-    paymentMethodTypeId: "CHEQUE",
-  },
-  "Credit Account": {
-    paymentMethodTypeId: "ACCOUNT",
-  },
-  "GA Payment": {
-    paymentMethodTypeId: "GA",
-  },
-  "IcePay - AMEX": {
-    paymentMethodTypeId: "ICEPAY",
-    paymentType: "AM",
-  },
-  "IcePay - Mastercard": {
-    paymentMethodTypeId: "ICEPAY",
-    paymentType: "MC",
-  },
-  "IcePay - Mastercard (Debit)": {
-    paymentMethodTypeId: "ICEPAY",
-    paymentType: "MD",
-  },
-  "IcePay - Visa": {
-    paymentMethodTypeId: "ICEPAY",
-    paymentType: "VI",
-  },
-  "IcePay - Visa (Debit)": {
-    paymentMethodTypeId: "ICEPAY",
-    paymentType: "PV",
-  },
-  "PoS - AMEX": {
-    paymentMethodTypeId: "POS",
-    paymentType: "AM",
-  },
-  "PoS - Debit": {
-    paymentMethodTypeId: "POS",
-    paymentType: "DB",
-  },
-  "PoS - Mastercard": {
-    paymentMethodTypeId: "POS",
-    paymentType: "MC",
-  },
-  "PoS - Mastercard (Debit)": {
-    paymentMethodTypeId: "POS",
-    paymentType: "MD",
-  },
-  "PoS - Visa": {
-    paymentMethodTypeId: "POS",
-    paymentType: "VI",
-  },
-  "PoS - Visa (Debit)": {
-    paymentMethodTypeId: "POS",
-    paymentType: "PV",
-  },
-  "Web - AMEX": {
-    paymentMethodTypeId: "WEB",
-    paymentType: "AM",
-  },
-  "Web - Mastercard": {
-    paymentMethodTypeId: "WEB",
-    paymentType: "MC",
-  },
-  "Web - Mastercard (Debit)": {
-    paymentMethodTypeId: "WEB",
-    paymentType: "MD",
-  },
-  "Web - Visa": {
-    paymentMethodTypeId: "WEB",
-    paymentType: "VI",
-  },
-  "Web - Visa (Debit)": {
-    paymentMethodTypeId: "WEB",
-    paymentType: "PV",
-  },
+  paymentTypeCode?: PaymentTypeCode;
 };
