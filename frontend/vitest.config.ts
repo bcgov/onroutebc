7eb4cbf2a0c34aa37cd09667fb56d2e927940964
Derek Roberts <derek.roberts@gmail.com>
2023-11-29 12:54:32 -0800

    Add frontend/vitest.config.ts

---
+++ frontend/vitest.config.ts
@@ -0,0 +1,11 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov']
+    },
+  },
+})
