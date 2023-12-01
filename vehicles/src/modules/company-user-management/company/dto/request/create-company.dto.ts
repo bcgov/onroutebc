fd5c5edd09b2db7816fe82a3397a6656cf0a46b6
praju-aot <praveen.raju@aot-technologies.com>
2023-12-01 14:47:42 -0500

    ORV2-1599: Add Issued by and DBA fields to payment template

--- vehicles/src/modules/company-user-management/company/dto/request/create-company.dto.ts
+++ vehicles/src/modules/company-user-management/company/dto/request/create-company.dto.ts
@@ -26,6 +26,15 @@ export class CreateCompanyDto {
 
   @AutoMap()
   @ApiProperty({
+    description: 'The Alternate name of the company/Doing Business As (DBA).',
+    example: 'ABC Carriers Inc.',
+  })
+  @IsOptional()
+  @IsString()
+  alternateName?: string;
+
+  @AutoMap()
+  @ApiProperty({
     description:
       'The mailing address of the company. ' +
       'The object must adhere to the individual field rules',
