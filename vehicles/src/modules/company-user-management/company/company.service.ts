34ab3b6f9fc16c618c3f694170573ca4fbfd4de1
praju-aot <praveen.raju@aot-technologies.com>
2023-12-08 15:55:13 -0500

    code review fixes

--- vehicles/src/modules/company-user-management/company/company.service.ts
+++ vehicles/src/modules/company-user-management/company/company.service.ts
@@ -126,7 +126,7 @@ export class CompanyService {
         User,
         {
           extraArgs: () => ({
-            UserAuthGroup: UserAuthGroup.COMPANY_ADMINISTRATOR,
+            userAuthGroup: UserAuthGroup.COMPANY_ADMINISTRATOR,
             userName: currentUser.userName,
             directory: currentUser.orbcUserDirectory,
             userGUID: currentUser.userGUID,
