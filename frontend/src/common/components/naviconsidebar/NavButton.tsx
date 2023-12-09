65bd7e301aafb8d77bd72fa232937b639fb14c1c
zgong-gov <123983557+zgong-gov@users.noreply.github.com>
2023-12-08 19:00:22 -0800

    fix: ORV2-1397 - Fix styling and behaviour of sticky sidebar (#872)

--- frontend/src/common/components/naviconsidebar/NavButton.tsx
+++ frontend/src/common/components/naviconsidebar/NavButton.tsx
@@ -12,26 +12,32 @@ export const NavButton = ({
   type,
   additionalClassNames,
   onClick,
+  isActive,
 }: {
   type: NavButtonType;
   additionalClassNames?: string;
   onClick: () => void;
+  isActive?: boolean;
 }) => {
   const additionalClasses = getDefaultRequiredVal("", additionalClassNames);
   const buttonClassName = `nav-button--${type}`;
+  const activeClassName = isActive ? "nav-button--active" : "";
 
   const icon = getIcon(type);
   const title = getNavButtonTitle(type);
-
   return (
     <button
       type="button"
-      title={title}
-      className={`nav-button ${buttonClassName} ${additionalClasses}`}
+      title=""
+      className={`nav-button ${buttonClassName} ${additionalClasses} ${activeClassName}`}
       onClick={onClick}
     >
       <div className="nav-button__hover-container">
         {<FontAwesomeIcon icon={icon} />}
+
+        <div className={`nav-button__hover-tooltip`}>
+          {title}
+        </div>
       </div>
     </button>
   );
