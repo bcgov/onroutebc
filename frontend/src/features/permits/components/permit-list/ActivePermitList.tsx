0ac9d56e2ea067f62a4c051326ec3f06ef9343e7
Krishnan Subramanian <84348052+krishnan-aot@users.noreply.github.com>
2023-12-18 08:44:06 -0800

    feat: ORV2-1370 Handling server side pagination in frontend (#890)
    Co-authored-by: zgong-gov <ZhongYu.Gong@gov.bc.ca>

--- frontend/src/features/permits/components/permit-list/ActivePermitList.tsx
+++ frontend/src/features/permits/components/permit-list/ActivePermitList.tsx
@@ -1,29 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
 
-import "./ActivePermitList.scss";
 import { InfoBcGovBanner } from "../../../../common/components/banners/InfoBcGovBanner";
-import { FIVE_MINUTES } from "../../../../common/constants/constants";
-import { getPermits } from "../../apiManager/permitsAPI";
-import { BasePermitList } from "./BasePermitList";
 import { BANNER_MESSAGES } from "../../../../common/constants/bannerMessages";
+import "./ActivePermitList.scss";
+import { BasePermitList } from "./BasePermitList";
 
 /**
  * A wrapper with the query to load the table with active permits.
  */
 export const ActivePermitList = () => {
-  const query = useQuery({
-    queryKey: ["activePermits"],
-    queryFn: () => getPermits(),
-    keepPreviousData: true,
-    staleTime: FIVE_MINUTES,
-  });
-
   return (
     <div className="table-container">
-      <InfoBcGovBanner
-        msg={BANNER_MESSAGES.PERMIT_REFUND_REQUEST} 
-      />
-      <BasePermitList query={query} />
+      <InfoBcGovBanner msg={BANNER_MESSAGES.PERMIT_REFUND_REQUEST} />
+      <BasePermitList />
     </div>
   );
 };
