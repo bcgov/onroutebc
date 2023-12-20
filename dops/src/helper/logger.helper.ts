5e484a0967c4cc8c26a3b3a324053229f21d2217
praju-aot <praveen.raju@aot-technologies.com>
2023-12-19 19:05:53 -0500

    Update typeorm log format

---
+++ dops/src/helper/logger.helper.ts
@@ -0,0 +1,27 @@
+import { LogLevel } from 'typeorm';
+
+export const getTypeormLogLevel = (logLevelString: string): LogLevel[] => {
+  const logLevels = logLevelString ? logLevelString.split(',') : ['error'];
+
+  const logLevelValues = logLevels.map((logLevel) => {
+    const logLevelValue = logLevel as LogLevel;
+    if (!isValidLogLevel(logLevelValue)) {
+      throw new Error(`Unknown log level: ${logLevel}`);
+    }
+    return logLevelValue;
+  });
+
+  return logLevelValues;
+};
+
+const isValidLogLevel = (logLevel: LogLevel): boolean => {
+  return [
+    'query',
+    'schema',
+    'error',
+    'warn',
+    'info',
+    'log',
+    'migration',
+  ].includes(logLevel);
+};
