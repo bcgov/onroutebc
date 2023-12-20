635e553bdace62fdfa2244de820a3ed09fd9c6e4
praju-aot <praveen.raju@aot-technologies.com>
2023-12-20 02:32:05 -0500

    Log incoming request and headers

--- vehicles/src/common/middleware/req.res.logger.ts
+++ vehicles/src/common/middleware/req.res.logger.ts
@@ -6,7 +6,14 @@ export class HTTPLoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { method, originalUrl } = request;
+    const { method, originalUrl, headers } = request;
+
+    // Log when a request is received
+    this.logger.log(
+      `Received ${method} ${originalUrl} - ${request.get(
+        'user-agent',
+      )} \n ${JSON.stringify(headers)}`,
+    );
 
     response.on('finish', () => {
       const { statusCode } = response;
