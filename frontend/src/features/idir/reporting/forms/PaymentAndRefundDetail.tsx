82b4bbf6900e4e7864245bb51963f3ffa23c48b1
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2024-01-04 10:23:14 -0800

    ORV2-1762 Financial report - pass userguid instead of username

--- frontend/src/features/idir/reporting/forms/PaymentAndRefundDetail.tsx
+++ frontend/src/features/idir/reporting/forms/PaymentAndRefundDetail.tsx
@@ -78,7 +78,9 @@ export const PaymentAndRefundDetail = () => {
       toDateTime: dayjs().set("h", 20).set("m", 59).set("s", 59).set("ms", 999),
       paymentMethods: Object.keys(CONSOLIDATED_PAYMENT_METHODS),
       permitType: Object.keys(permitTypes ?? []),
-      users: Object.keys(permitIssuers ?? {}),
+      // permitIssues is a <userName, userGUID> record.
+      // So, Object.values is what we need.
+      users: Object.values(permitIssuers ?? {}),
     },
     reValidateMode: "onBlur",
   });
@@ -108,7 +110,9 @@ export const PaymentAndRefundDetail = () => {
 
   useEffect(() => {
     if (permitIssuers) {
-      setValue("users", Object.keys(permitIssuers));
+      // permitIssues is a <userName, userGUID> record.
+      // So, Object.values is what we need.
+      setValue("users", Object.values(permitIssuers));
     }
   }, [ispermitIssuersQueryLoading]);
 
