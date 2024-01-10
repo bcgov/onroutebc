ddc081113cd503444764fd52c95ec81f6250250f
Praveen Raju <80779423+praju-aot@users.noreply.github.com>
2024-01-10 17:28:26 -0500

    ORV2-1807 Logging, formatting and refactoring (#1068)

--- dops/src/middleware/req.res.logger.ts
+++ dops/src/middleware/req.res.logger.ts
@@ -6,21 +6,27 @@ export class HTTPLoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { method, originalUrl, headers } = request;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { method, originalUrl, headers, body } = request;
 
     const headersObj = { ...headers };
     if (process.env.NODE_ENV === 'production' && headersObj.authorization) {
-      headersObj.authorization = 'xxxxxxxxx';
+      headersObj.authorization = undefined;
     }
+
     // Log when a request is received
     this.logger.log(
-      `Request: ${method} ${originalUrl} \n ${JSON.stringify(headersObj)}`,
+      `Request: ${method} ${originalUrl} \nHeader: ${JSON.stringify(
+        headersObj,
+      )} \nPayload: ${JSON.stringify(body)}`,
     );
 
     response.on('finish', () => {
       const { statusCode } = response;
       const contentLength = response.get('content-length');
-      const hostedHttpLogFormat = `Response: ${method} ${originalUrl} ${statusCode} ${contentLength}`;
+      const responseTime = response.get('X-Response-Time');
+      console.log(`Response Time: ${responseTime} ms`);
+      const hostedHttpLogFormat = `Response: ${method} ${originalUrl} StatusCode: ${statusCode} ContentLength: ${contentLength} ResponseTime: ${responseTime}ms`;
       this.logger.log(hostedHttpLogFormat);
     });
     next();
