fdb29dda0cd821d124ce15a2de96d069b0e6b862
erikataot <erik.yuzwa@aot-technologies.com>
2024-01-26 09:08:10 -0800

    feat: updating with isenum validator

--- vehicles/src/modules/feature-flags/dto/response/read-feature-flag.dto.ts
+++ vehicles/src/modules/feature-flags/dto/response/read-feature-flag.dto.ts
@@ -1,5 +1,7 @@
 import { AutoMap } from '@automapper/classes';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum } from 'class-validator';
+import { FeatureFlagValue } from 'src/common/enum/feature-flag-value.enum';
 
 /**
  * JSON representation of response object when retrieving a feature flag.
@@ -20,9 +22,10 @@ export class ReadFeatureFlagDto {
     featureKey: string;
 
     @AutoMap()
+    @IsEnum(FeatureFlagValue)
     @ApiProperty({
         description: 'The feature_flag value.',
-        example: 'TRUE',
+        example: 'ENABLED',
     })
-    featureValue: string;
+    featureValue: FeatureFlagValue;
 }
