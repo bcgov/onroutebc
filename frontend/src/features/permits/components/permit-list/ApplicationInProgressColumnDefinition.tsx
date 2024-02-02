7cbdab8af6c5b68590172a5ca983be6fb583a923
erikataot <erik.yuzwa@aot-technologies.com>
2024-02-02 15:01:08 -0800

    feat: updated columns

---
+++ frontend/src/features/permits/components/permit-list/ApplicationInProgressColumnDefinition.tsx
@@ -0,0 +1,52 @@
+import { MRT_ColumnDef } from "material-react-table";
+import { Link } from "react-router-dom";
+
+import { ApplicationInProgress } from "../../types/application";
+import { APPLICATIONS_ROUTES } from "../../../../routes/constants";
+
+export const ApplicationInProgressColumnDefinition: MRT_ColumnDef<ApplicationInProgress>[] =
+  [
+    {
+      accessorKey: "applicationNumber",
+      header: "Application #",
+      accessorFn: (row) => row.applicationNumber,
+      Cell: (props: { cell: any; row: any }) => {
+        const permitIdStr = `${props.row.original.permitId}`;
+        return (
+          <Link
+            to={`${APPLICATIONS_ROUTES.DETAILS(permitIdStr)}`}
+            className="column-link column-link--application-details"
+          >
+            {props.cell.getValue()}
+          </Link>
+        );
+      },
+    },
+    {
+      accessorKey: "permitType",
+      header: "Permit Type",
+    },
+    {
+      accessorKey: "permitData.vehicleDetails.unitNumber",
+      header: "Unit #",
+    },
+    {
+      accessorKey: "permitData.vehicleDetails.vin",
+      header: "VIN",
+    },
+    {
+      accessorKey: "permitData.vehicleDetails.plate",
+      header: "Plate",
+    },
+    {
+      accessorKey: "permitData.startDate",
+      header: "Permit Start Date",
+    },
+    {
+      accessorKey: "updatedDateTime",
+      header: "Last Updated",
+    },
+  ];
+
+export const ApplicationNotFoundColumnDefinition: MRT_ColumnDef<ApplicationInProgress>[] =
+  [];
