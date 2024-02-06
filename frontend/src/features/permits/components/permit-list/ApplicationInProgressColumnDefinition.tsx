dbbf5d539111fe81062fd884eabca8fe6ddf27f2
erikataot <erik.yuzwa@aot-technologies.com>
2024-02-06 12:51:25 -0800

    fix: use CustomNavLink

--- frontend/src/features/permits/components/permit-list/ApplicationInProgressColumnDefinition.tsx
+++ frontend/src/features/permits/components/permit-list/ApplicationInProgressColumnDefinition.tsx
@@ -1,7 +1,7 @@
 import { MRT_ColumnDef } from "material-react-table";
 import { ApplicationInProgress } from "../../types/application";
 import { APPLICATIONS_ROUTES } from "../../../../routes/constants";
-import { CustomLink } from "../../../../common/components/links/CustomLink";
+import { CustomNavLink } from "../../../../common/components/links/CustomNavLink";
 
 export const ApplicationInProgressColumnDefinition: MRT_ColumnDef<ApplicationInProgress>[] =
   [
@@ -12,12 +12,12 @@ export const ApplicationInProgressColumnDefinition: MRT_ColumnDef<ApplicationInP
       Cell: (props: { cell: any; row: any }) => {
         const permitIdStr = `${props.row.original.permitId}`;
         return (
-          <CustomLink
+          <CustomNavLink
             to={`${APPLICATIONS_ROUTES.DETAILS(permitIdStr)}`}
             className="column-link column-link--application-details"
           >
             {props.cell.getValue()}
-          </CustomLink>
+          </CustomNavLink>
         );
       },
     },
