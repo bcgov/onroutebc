b75abd1386d881377599e68375bc1d97dac05d52
erikataot <erik.yuzwa@aot-technologies.com>
2024-02-14 09:04:52 -0800

    feat: cleanup

--- vehicles/src/modules/feature-flags/feature-flags.module.ts
+++ vehicles/src/modules/feature-flags/feature-flags.module.ts
@@ -1,8 +1,5 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
 import { FeatureFlag } from './entities/feature-flag.entity';
 import { FeatureFlagsController } from './feature-flags.controller';
 import { FeatureFlagsService } from './feature-flags.service';
@@ -16,17 +13,4 @@ import { FeatureFlagsProfile } from './profiles/feature-flags.profile';
   exports: [FeatureFlagsService],
 })
 export class FeatureFlagsModule {
-  static forRoot(dataSource: DataSource, cacheManager: Cache): DynamicModule {
-    return {
-      module: FeatureFlagsModule,
-      imports: [TypeOrmModule.forFeature([FeatureFlag])],
-      providers: [
-        FeatureFlagsService,
-        { provide: DataSource, useValue: dataSource },
-        { provide: CACHE_MANAGER, useValue: cacheManager },
-        FeatureFlagsProfile,
-      ],
-      controllers: [FeatureFlagsController],
-    };
-  }
 }
