5d728e531c7888e5d166e9b44565c6340df6330d
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2024-02-16 11:33:09 -0800

    Path and query param validation in tandem

--- vehicles/src/modules/company-user-management/users/company-users.controller.ts
+++ vehicles/src/modules/company-user-management/users/company-users.controller.ts
@@ -26,6 +26,7 @@ import { UpdateUserDto } from './dto/request/update-user.dto';
 import { UpdateUserStatusDto } from './dto/request/update-user-status.dto';
 import { GetCompanyUserQueryParamsDto } from './dto/request/queryParam/getCompanyUser.query-params.dto';
 import { GetCompanyUserByUserGUIDPathParamsDto } from './dto/request/pathParam/getCompanyUserByUserGUID.path-params.dto';
+import { GetCompanyQueryParamsDto } from '../company/dto/request/queryParam/getCompany.query-params.dto';
 
 @ApiTags('Company and User Management - Company User')
 @ApiBadRequestResponse({
@@ -119,6 +120,7 @@ export class CompanyUsersController {
   @Roles(Role.READ_USER)
   @Get(':userGUID')
   async get(
+    @Query() getCompanyQueryParamsDto: GetCompanyQueryParamsDto,
     @Param() params: GetCompanyUserByUserGUIDPathParamsDto,
   ): Promise<ReadUserDto> {
     const { companyId, userGUID } = params;
