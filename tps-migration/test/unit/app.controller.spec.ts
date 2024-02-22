f27b19e348f8bcf6a1c474de187b112cbfe45312
praju-aot <praveen.raju@aot-technologies.com>
2024-02-22 15:39:45 -0500

    Fix test spec and formatting

--- tps-migration/test/unit/app.controller.spec.ts
+++ tps-migration/test/unit/app.controller.spec.ts
@@ -1,14 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from '../../src/app.controller';
 import { AppService } from '../../src/app.service';
+import { FeatureFlagsService } from '../../src/modules/feature-flags/feature-flags.service';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
 
 describe('AppController', () => {
   let appController: AppController;
+  let featureFlagsService: DeepMocked<FeatureFlagsService>;
 
   beforeEach(async () => {
+    featureFlagsService = createMock<FeatureFlagsService>();
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: FeatureFlagsService,
+          useValue: featureFlagsService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
