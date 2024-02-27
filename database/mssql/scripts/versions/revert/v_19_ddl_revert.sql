33ab2aee57af28e453db84f83f6f38c81d485667
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-02-27 10:12:38 -0700

    changing database script version

--- database/mssql/scripts/versions/revert/v_19_ddl_revert.sql
+++ database/mssql/scripts/versions/revert/v_19_ddl_revert.sql
@@ -17,7 +17,7 @@ GO
 DECLARE @VersionDescription VARCHAR(255)
 SET @VersionDescription = 'Reverting addition of user status to company user table'
 
-INSERT [dbo].[ORBC_SYS_VERSION] ([VERSION_ID], [DESCRIPTION], [RELEASE_DATE]) VALUES (16, @VersionDescription, getutcdate())
+INSERT [dbo].[ORBC_SYS_VERSION] ([VERSION_ID], [DESCRIPTION], [RELEASE_DATE]) VALUES (18, @VersionDescription, getutcdate())
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
 
