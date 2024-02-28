ec3272892dafefb0d9391a942bdfb358831b7c03
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-02-28 10:34:39 -0700

    application status array immutable

--- vehicles/src/common/enum/application-status.enum.ts
+++ vehicles/src/common/enum/application-status.enum.ts
@@ -18,20 +18,22 @@ export enum ApplicationStatus {
 /**
  * Apllication statuses to be considered for Application In Progress (AIP) from IDIR User POV.
  */
-export const idirUserAIPStatus: ApplicationStatus[] = [
+export const idirUserAIPStatus: Readonly<ApplicationStatus[]> = [
   ApplicationStatus.IN_PROGRESS,
   ApplicationStatus.WAITING_PAYMENT,
   ApplicationStatus.CANCELLED,
+  ApplicationStatus.UNDER_REVIEW,
 ];
 
 /**
  * Apllication statuses to be considered for Application In Progress (AIP) from CV Client POV.
  */
-export const cvClientAIPStatus: ApplicationStatus[] = [
+export const cvClientAIPStatus: Readonly<ApplicationStatus[]> = [
   ApplicationStatus.IN_PROGRESS,
   ApplicationStatus.WAITING_PAYMENT,
+  ApplicationStatus.UNDER_REVIEW,
 ];
 
-export const deleteCvClientAIP: ApplicationStatus = ApplicationStatus.CANCELLED;
+export const  deleteCvClientAIP: Readonly <ApplicationStatus> = ApplicationStatus.CANCELLED;
 
-export const deleteIdirAIP: ApplicationStatus = ApplicationStatus.DELETED;
+export const deleteIdirAIP: Readonly <ApplicationStatus> = ApplicationStatus.DELETED;
