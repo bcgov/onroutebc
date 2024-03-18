4ad307e1ae8baadca57582cf875a4305761dd581
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2024-03-18 11:17:05 -0700

    Finish up shopping cart controller

--- vehicles/src/modules/shopping-cart/shopping-cart.controller.ts
+++ vehicles/src/modules/shopping-cart/shopping-cart.controller.ts
@@ -1,47 +1,137 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  Req,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiMethodNotAllowedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Request } from 'express';
 import { Roles } from '../../common/decorator/roles.decorator';
 import { Role } from '../../common/enum/roles.enum';
+import { ClientUserAuthGroup } from '../../common/enum/user-auth-group.enum';
+import { ExceptionDto } from '../../common/exception/exception.dto';
+import { IUserJWT } from '../../common/interface/user-jwt.interface';
 import { CreateShoppingCartDto } from './dto/create-shopping-cart.dto';
-import { UpdateShoppingCartDto } from './dto/update-shopping-cart.dto';
-import { ShoppingCartService } from './shopping-cart.service';
 import { GetShoppingCartQueryParamsDto } from './dto/request/getShoppingCart.query-params.dto';
 import { ResultDto } from './dto/response/result.dto';
+import { UpdateShoppingCartDto } from './dto/update-shopping-cart.dto';
+import { ShoppingCartService } from './shopping-cart.service';
 
 @ApiBearerAuth()
 @ApiTags('Shopping Cart')
-@Controller('/permit/applications/shopping-cart')
+@Controller('/shopping-cart')
+@ApiMethodNotAllowedResponse({
+  description: 'The Application Api Method Not Allowed Response',
+  type: ExceptionDto,
+})
+@ApiInternalServerErrorResponse({
+  description: 'The Application Api Internal Server Error Response',
+  type: ExceptionDto,
+})
 export class ShoppingCartController {
   constructor(private readonly shoppingCartService: ShoppingCartService) {}
 
+  /**
+   * Adds a new item to the shopping cart.
+   *
+   * @param createShoppingCartDto - The DTO to create a new shopping cart item.
+   * @returns The result of the creation operation.
+   */
+  @ApiOperation({
+    summary: 'Adds one or more applications from the shopping cart.',
+    description:
+      'Adds one or more applications from the shopping cart, enforcing authentication.',
+  })
+  @ApiCreatedResponse({
+    description: 'The result of the changes to cart.',
+    type: ResultDto,
+  })
   @Post()
   @Roles(Role.WRITE_PERMIT)
-  create(@Body() createShoppingCartDto: CreateShoppingCartDto) {
-    return this.shoppingCartService.create(createShoppingCartDto);
+  async addToCart(
+    @Req() request: Request,
+    @Body() createShoppingCartDto: CreateShoppingCartDto,
+  ): Promise<ResultDto> {
+    return await this.shoppingCartService.create(createShoppingCartDto);
   }
 
+  /**
+   * Retrieves applications within the shopping cart based on the user's permissions and optional query parameters.
+   *
+   * @param request - The incoming request object, used to extract the user's authentication details.
+   * @param getShoppingCartQueryParamsDto - DTO containing optional query parameters for filtering the shopping cart contents.
+   * @returns A promise resolved with the applications found in the shopping cart for the authenticated user.
+   */
+  @ApiOperation({
+    summary: 'Returns the applications in the shopping cart.',
+    description:
+      'Returns one or more applications from the shopping cart, enforcing authentication.',
+  })
+  @ApiOkResponse({
+    description: 'The result of the changes to cart.',
+    type: ResultDto,
+  })
   @Get()
   @Roles(Role.WRITE_PERMIT)
-  findAll(
+  async getApplicationsInCart(
+    @Req() request: Request,
     @Query() getShoppingCartQueryParamsDto: GetShoppingCartQueryParamsDto,
   ) {
-    const { companyId } = getShoppingCartQueryParamsDto;
-    return this.shoppingCartService.findAll();
+    const currentUser = request.user as IUserJWT;
+    if (currentUser.idir_user_guid && !currentUser.companyId) {
+      const { companyId } = getShoppingCartQueryParamsDto;
+      return await this.shoppingCartService.findApplicationsInCart(companyId);
+    } else if (
+      currentUser.orbcUserAuthGroup ===
+      ClientUserAuthGroup.COMPANY_ADMINISTRATOR
+    ) {
+      const { companyId } = currentUser;
+      return await this.shoppingCartService.findApplicationsInCart(companyId);
+    } else {
+      const { companyId, userGUID } = currentUser;
+      return await this.shoppingCartService.findApplicationsInCart(
+        companyId,
+        userGUID,
+      );
+    }
   }
-  
 
-  @Delete()
-  @Roles(Role.WRITE_PERMIT)
   /**
-   * Removes a shopping cart item.
+   * Removes one or more applications from the shopping cart.
    *
    * @param updateShoppingCartDto - The DTO to update a shopping cart.
    * @returns The result of the removal operation.
    */
-  async remove(
+  @Delete()
+  @Roles(Role.WRITE_PERMIT)
+  @ApiOperation({
+    summary: 'Removes one or more applications from the shopping cart.',
+    description:
+      'Removes one or more applications from the shopping cart, enforcing authentication.',
+  })
+  @ApiOkResponse({
+    description: 'The result of the changes to cart.',
+    type: ResultDto,
+    status: 200,
+  })
+  async removeFromCart(
+    @Req() request: Request,
     @Body() updateShoppingCartDto: UpdateShoppingCartDto,
   ): Promise<ResultDto> {
+    const currentUser = request.user as IUserJWT;
     return await this.shoppingCartService.remove(updateShoppingCartDto);
   }
 }
--- vehicles/src/modules/shopping-cart/shopping-cart.service.ts
+++ vehicles/src/modules/shopping-cart/shopping-cart.service.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { LogAsyncMethodExecution } from '../../common/decorator/log-async-method-execution.decorator';
 import { ApplicationStatus } from '../../common/enum/application-status.enum';
 import { Permit as Application } from '../permit-application-payment/permit/entities/permit.entity';
 import { CreateShoppingCartDto } from './dto/create-shopping-cart.dto';
@@ -10,27 +11,89 @@ import { UpdateShoppingCartDto } from './dto/update-shopping-cart.dto';
 
 @Injectable()
 export class ShoppingCartService {
+  private readonly logger = new Logger(ShoppingCartService.name);
   constructor(
     @InjectRepository(Application)
     private readonly applicationRepository: Repository<Application>,
   ) {}
-  create(createShoppingCartDto: CreateShoppingCartDto) {}
 
-  findAll() {
-    return `This action returns all shoppingCart`;
+  /**
+   * Creates a shopping cart by setting the application status of permits to `IN_CART`.
+   * The function accepts a DTO containing details to identify and update the necessary permits.
+   *
+   * @param createShoppingCartDto - Data Transfer Object containing the details of the permits to be added to the shopping cart.
+   * @returns A ResultDto object which includes arrays of permit application IDs that were successfully added to the cart ('success'), and those that were not ('failure').
+   */
+  @LogAsyncMethodExecution()
+  async create(
+    createShoppingCartDto: CreateShoppingCartDto,
+  ): Promise<ResultDto> {
+    return await this.updateApplicationStatus(
+      createShoppingCartDto,
+      ApplicationStatus.IN_CART,
+    );
   }
 
-  async remove({
-    applicationIds,
-    companyId,
-  }: UpdateShoppingCartDto): Promise<ResultDto> {
+  /**
+   * Fetches all permit applications within a shopping cart for a specific company.
+   * Optionally filters the results by the user GUID if provided.
+   *
+   * @param companyId - The ID of the company for which to retrieve permit applications.
+   * @param userGUID - (Optional) The GUID of a user to further filter the applications by the application owner.
+   * @returns A promise resolved with a list of permit applications currently in the shopping cart that match the criteria.
+   */
+  @LogAsyncMethodExecution()
+  async findApplicationsInCart(companyId: number, userGUID?: string) {
+    return await this.applicationRepository.find({
+      where: {
+        permitStatus: ApplicationStatus.IN_CART,
+        company: { companyId },
+        applicationOwner: { userGUID: userGUID ? userGUID : '1=1' },
+      },
+    });
+  }
+
+  /**
+   * Removes items from the shopping cart by updating their application status to IN_PROGRESS.
+   * It takes a DTO containing the IDs of applications to be updated and processes them accordingly.
+   *
+   * @param updateShoppingCartDto - Data Transfer Object containing the details required to update the application status of items in the shopping cart.
+   * @returns A ResultDto object which contains two arrays: 'success' with IDs of successfully updated applications, and 'failure' with IDs of applications that failed to update.
+   */
+  @LogAsyncMethodExecution()
+  async remove(
+    updateShoppingCartDto: UpdateShoppingCartDto,
+  ): Promise<ResultDto> {
+    return await this.updateApplicationStatus(
+      updateShoppingCartDto,
+      ApplicationStatus.IN_PROGRESS,
+    );
+  }
+
+  /**
+   * Updates the status of multiple applications to either `IN_CART` or `IN_PROGRESS`.
+   * This private function is utilized internally within the service to reflect changes in the application status based on actions performed on the shopping cart.
+   *
+   * @param {UpdateShoppingCartDto | CreateShoppingCartDto} dto - An object that contains arrays of application IDs (`applicationIds`) and the company ID (`companyId`) associated with these applications.
+   * @param {ApplicationStatus.IN_CART | ApplicationStatus.IN_PROGRESS} statusToUpdateTo - The new status to which the applications will be updated. It must be either `IN_CART` for adding to the cart, or `IN_PROGRESS` for removing from the cart.
+   * @returns {Promise<ResultDto>} A `ResultDto` instance containing two arrays: `success` with IDs of applications successfully updated, and `failure` with IDs of applications that failed to update.
+   */
+
+  @LogAsyncMethodExecution()
+  private async updateApplicationStatus(
+    {
+      applicationIds,
+      companyId,
+    }: UpdateShoppingCartDto | CreateShoppingCartDto,
+    statusToUpdateTo: ApplicationStatus.IN_CART | ApplicationStatus.IN_PROGRESS,
+  ): Promise<ResultDto> {
     const { success, failure } = new ResultDto();
     for (const applicationId of applicationIds) {
       try {
         const { affected } = await this.applicationRepository.update(
           { permitId: applicationId, company: { companyId } },
           {
-            permitStatus: ApplicationStatus.IN_PROGRESS,
+            permitStatus: statusToUpdateTo,
           },
         );
         if (affected === 1) {
@@ -38,8 +101,8 @@ export class ShoppingCartService {
         } else {
           failure.push(applicationId);
         }
-      } catch (e) {
-        console.log('error::', e);
+      } catch (error) {
+        this.logger.error(error);
         failure.push(applicationId);
       }
     }
