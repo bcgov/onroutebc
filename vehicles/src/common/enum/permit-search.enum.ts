9e72cb042a1d16e2ef1e19aadd5ebe10b0f7d634
praju-aot <praveen.raju@aot-technologies.com>
2024-03-21 11:59:00 -0400

    Remove company and client name search from Get Permit

--- vehicles/src/common/enum/permit-search.enum.ts
+++ vehicles/src/common/enum/permit-search.enum.ts
@@ -1,7 +1,5 @@
 export enum PermitSearch {
   PLATE = 'plate',
   PERMIT_NUMBER = 'permitNumber',
-  CLIENT_NUMBER = 'clientNumber',
-  COMPANY_NAME = 'companyName',
   APPLICATION_NUMBER = 'applicationNumber',
 }
