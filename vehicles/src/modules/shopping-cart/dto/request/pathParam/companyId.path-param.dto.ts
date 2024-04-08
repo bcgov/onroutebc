1371d655a47229ae0fd57d783df0d94c6b49169a
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2024-04-08 09:46:09 -0700

    Swtiching to integer check

--- vehicles/src/modules/shopping-cart/dto/request/pathParam/companyId.path-param.dto.ts
+++ vehicles/src/modules/shopping-cart/dto/request/pathParam/companyId.path-param.dto.ts
@@ -1,14 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import { IsNumber, IsPositive } from 'class-validator';
+import { IsInt, IsPositive } from 'class-validator';
 
 export class CompanyIdPathParamDto {
   @ApiProperty({
     description: `Id of the company the cart belongs to.`,
     example: 74,
   })
-  @Type(() => Number)
-  @IsNumber({ allowNaN: false, maxDecimalPlaces: 0 })
+  @IsInt()
   @IsPositive()
   companyId: number;
 }
