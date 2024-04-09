1fb16b51e2a43062d7d4d224637f9a8abff07cc3
bcgov-brwang <bcgov.brwang@gmail.com>
2024-04-09 13:26:31 -0700

    ORV2-2161: added initial cypress test cases

---
+++ frontend/cypress/e2e/hom-page.cy.ts
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe('Default Page Test', () => {
+    it('Visits the default page and checks for "onRouteBC"', () => {
+      const baseUrl = Cypress.env('CYPRESS_baseUrl');
+      if (!baseUrl) {
+        throw new Error('Base URL is not defined in environment variables');
+      }
+      const path = '/';
+      const url = baseUrl + path;
+      cy.visit(url); 
+      cy.contains('onRouteBC').should('exist');
+      cy.contains('Log in with BCeID').should('exist');
+      cy.contains('Log in with IDIR').should('exist');
+    });
+  });
+
+  describe('Log in with BCeID button', () => {
+    it('Redirects to another page when "Log in with BCeID" button is clicked', () => {
+      const baseUrl = Cypress.env('CYPRESS_baseUrl');
+      cy.visit(baseUrl);
+      cy.contains('Log in with BCeID').click();
+      cy.url().then((url) => {
+      cy.log('Current URL:', url);
+      expect(url).to.include(baseUrl);
+      });
+    });
+  });
+
+  
