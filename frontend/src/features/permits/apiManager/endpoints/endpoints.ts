faf6a3446f85ff5316c363707b4d234d6d546da8
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2024-04-11 09:49:26 -0700

    Remove unused variable

--- frontend/src/features/permits/apiManager/endpoints/endpoints.ts
+++ frontend/src/features/permits/apiManager/endpoints/endpoints.ts
@@ -1,34 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { VEHICLES_URL } from "../../../../common/apiManager/endpoints/endpoints";
 
-const APPLICATIONS_API_BASE_2 = (companyId: string) =>
+const APPLICATIONS_API_BASE = (companyId: string) =>
   `${VEHICLES_URL}/companies/${companyId}/applications`;
 
-const APPLICATIONS_API_BASE_22 = (_s: any, companyId: string) => {
-  return `${VEHICLES_URL}/companies/${companyId}/applications`;
-};
 
-const PERMITS_API_BASE_2 = (companyId: string) =>
+const PERMITS_API_BASE = (companyId: string) =>
   `${VEHICLES_URL}/companies/${companyId}/permits`;
 
 const STAFF_PERMIT_API_BASE = `${VEHICLES_URL}/permits`;
 
 export const APPLICATIONS_API_ROUTES = {
-  CREATE: (companyId: string) => APPLICATIONS_API_BASE_22`${companyId}`,
-  UPDATE: (companyId: string) => APPLICATIONS_API_BASE_2(companyId),
-  GET: (companyId: string) => APPLICATIONS_API_BASE_2(companyId),
-  DELETE: (companyId: string) => APPLICATIONS_API_BASE_2(companyId),
+  CREATE: (companyId: string) => APPLICATIONS_API_BASE(`${companyId}`),
+  UPDATE: (companyId: string) => APPLICATIONS_API_BASE(companyId),
+  GET: (companyId: string) => APPLICATIONS_API_BASE(companyId),
+  DELETE: (companyId: string) => APPLICATIONS_API_BASE(companyId),
 };
 
 export const PERMITS_API_ROUTES = {
-  BASE: (companyId: string) => PERMITS_API_BASE_2(companyId),
-  GET: (companyId: string) => PERMITS_API_BASE_2(companyId),
-  ISSUE: (companyId: string) => `${APPLICATIONS_API_BASE_2(companyId)}/issue`,
+  BASE: (companyId: string) => PERMITS_API_BASE(companyId),
+  GET: (companyId: string) => PERMITS_API_BASE(companyId),
+  ISSUE: (companyId: string) => `${APPLICATIONS_API_BASE(companyId)}/issue`,
   AMEND: APPLICATIONS_API_ROUTES.CREATE,
   DOWNLOAD: (companyId: string, permitId: string) =>
-    `${PERMITS_API_BASE_2(companyId)}/${permitId}/document`,
+    `${PERMITS_API_BASE(companyId)}/${permitId}/document`,
   RECEIPT: (companyId: string, permitId: string) =>
-    `${PERMITS_API_BASE_2(companyId)}/${permitId}/receipt`,
+    `${PERMITS_API_BASE(companyId)}/${permitId}/receipt`,
   VOID: (permitId: string) => `${STAFF_PERMIT_API_BASE}/${permitId}/void`,
   RESEND: (permitId: string) =>
     `${STAFF_PERMIT_API_BASE}/${permitId}/notification`,
