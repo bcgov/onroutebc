86eae34c235af473541f706c8814f5578a3f2660
Chris Berg <chris.berg@aot-technologies.com>
2024-04-12 11:30:07 -0700

    Removing un-needed baseUrl expansion

--- frontend/cypress/e2e/home-page.cy.ts
+++ frontend/cypress/e2e/home-page.cy.ts
@@ -2,13 +2,7 @@
 
 describe('Default Page Test', () => {
     it('Visits the default page and checks for "onRouteBC"', () => {
-      const baseUrl = Cypress.env('CYPRESS_baseUrl');
-      if (!baseUrl) {
-        throw new Error('Base URL is not defined in environment variables');
-      }
-      const path = '/';
-      const url = baseUrl + path;
-      cy.visit(url); 
+      cy.visit('/'); 
       cy.contains('onRouteBC').should('exist');
       cy.contains('Log in with BCeID').should('exist');
       cy.contains('Log in with IDIR').should('exist');
@@ -17,11 +11,10 @@ describe('Default Page Test', () => {
 
   describe('Log in with BCeID button', () => {
     it('Redirects to another page when "Log in with BCeID" button is clicked', () => {
-      cy.visit(baseUrl);
+      cy.visit('/');
       cy.contains('Log in with BCeID').click();
       cy.url().then((url) => {
       cy.log('Current URL:', url);
-      expect(url).to.include(baseUrl);
       });
     });
   });
