df03810e8d2637b5a3cfd29d4d91acd107944518
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-04-23 23:42:10 -0600

    static values

--- vehicles/src/modules/cgi-sftp/cgi-sftp.module.ts
+++ vehicles/src/modules/cgi-sftp/cgi-sftp.module.ts
@@ -7,11 +7,18 @@ import { CgiSftpController } from './cgi-sftp.controller';
   imports: [
     SftpModule.forRoot(
       {
-        host: process.env.CFS_SFTP_HOST,
-        port: Number(process.env.CFS_SFTP_PORT),
-        username: process.env.CFS_SFTP_USERNAME,
-        privateKey: process.env.CFS_PRIVATE_KEY,
-        passphrase: process.env.CFS_PRIVATE_KEY_PASSPHRASE,
+        host: "Trooper.cas.gov.bc.ca",
+        port: "22",
+        username: "f3535t",
+        privateKey: `-----BEGIN OPENSSH PRIVATE KEY-----
+        b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABBl5uVCkN
+        DWVOdqkM5r8pTXAAAAEAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAIHoMPb7P9MSB3SGd
+        rsUOqzro6NZ8Q8cT73NrCC2bm7gZAAAAoGUsbGyviF9zKrQQYFSgny9j3T13GZ/A1G84dP
+        4y4DKT4dDw3XG5SwkHsJISC8kCoXvVasUTqBkoDQV9ygUiMz/Vul3dK44RVnouTVSvME6V
+        QXuscakbo7QxqOCDSN4/qepUtpKMlvyXWTWtSvWF7GNxebwRS4/WH4EAeVK8s0iOjOnvPu
+        +IV3mVbIHSkky69UKUP6UdGLu6CG9TelFhNXw=
+        -----END OPENSSH PRIVATE KEY-----`,
+        passphrase: "Amsterdam!99",
         debug: console.log,
       },
       false,
