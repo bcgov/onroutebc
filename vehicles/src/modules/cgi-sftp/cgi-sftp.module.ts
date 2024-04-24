7d35f15daf33071ab5286fd3baccb46d8b67fb1d
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-04-24 00:03:42 -0600

    updating key format

--- vehicles/src/modules/cgi-sftp/cgi-sftp.module.ts
+++ vehicles/src/modules/cgi-sftp/cgi-sftp.module.ts
@@ -11,12 +11,12 @@ import { CgiSftpController } from './cgi-sftp.controller';
         port: "22",
         username: "f3535t",
         privateKey: `-----BEGIN OPENSSH PRIVATE KEY-----
-        b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABBl5uVCkN
-        DWVOdqkM5r8pTXAAAAEAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAIHoMPb7P9MSB3SGd
-        rsUOqzro6NZ8Q8cT73NrCC2bm7gZAAAAoGUsbGyviF9zKrQQYFSgny9j3T13GZ/A1G84dP
-        4y4DKT4dDw3XG5SwkHsJISC8kCoXvVasUTqBkoDQV9ygUiMz/Vul3dK44RVnouTVSvME6V
-        QXuscakbo7QxqOCDSN4/qepUtpKMlvyXWTWtSvWF7GNxebwRS4/WH4EAeVK8s0iOjOnvPu
-        +IV3mVbIHSkky69UKUP6UdGLu6CG9TelFhNXw=
+        b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABC/PmR4Pu
+        1h52p1K4JrRttiAAAAEAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAIHoMPb7P9MSB3SGd
+        rsUOqzro6NZ8Q8cT73NrCC2bm7gZAAAAoDTz01E7tfEqGAG0hU9A/ZhoEfKmopH/USXpQs
+        S+qxxTPdgxhfO+p4hbLySFB8/4CYuRaKROiTURbjJo1pRUcVS4deEsgyUDdwLlQpshhBeS
+        NC1S3VZq6NaTei83x4aqA4dVkRF0aLsnG7IqJSUuhATcrhN94veKjyeEeAAjzJI8XpiJj0
+        uvbkwilrUZzjYrB4EsDyHqPHla2zj3XoeUYuI=
         -----END OPENSSH PRIVATE KEY-----`,
         passphrase: "Amsterdam!99",
         debug: console.log,
