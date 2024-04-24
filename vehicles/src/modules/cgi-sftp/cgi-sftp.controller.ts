97700e4c4ffad432a51bcc5c2c01b580aa6a61c6
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-04-24 10:40:37 -0600

    securing api

--- vehicles/src/modules/cgi-sftp/cgi-sftp.controller.ts
+++ vehicles/src/modules/cgi-sftp/cgi-sftp.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { CgiSftpService } from './cgi-sftp.service';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { Roles } from 'src/common/decorator/roles.decorator';
+import { IDIR_USER_AUTH_GROUP_LIST } from 'src/common/enum/user-auth-group.enum';
+import { Role } from 'src/common/enum/roles.enum';
 
+@ApiBearerAuth()
 @Controller('cgisftp')
 export class CgiSftpController {
   constructor(private readonly cgiSftpService: CgiSftpService) {}
@@ -11,6 +15,10 @@ export class CgiSftpController {
   @ApiOperation({
     summary: 'Get file list',
     description: 'Retrieves list of file in current folder.',
+  })
+  @Roles({
+    userAuthGroup: IDIR_USER_AUTH_GROUP_LIST,
+    oneOf: [Role.READ_PERMIT],
   })
   @Get()
   async list(): Promise<string> {
