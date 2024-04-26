33353e7944b5b46606b15411ee4f77259eac1be9
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-04-26 16:24:16 -0600

    file upload changes

--- vehicles/src/modules/cgi-sftp/cgi-sftp.controller.ts
+++ vehicles/src/modules/cgi-sftp/cgi-sftp.controller.ts
@@ -1,12 +1,14 @@
 import {
   Controller,
   Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { CgiSftpService } from './cgi-sftp.service';
-import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiConsumes, ApiBody } from '@nestjs/swagger';
 import { Roles } from 'src/common/decorator/roles.decorator';
 import { IDIR_USER_AUTH_GROUP_LIST } from 'src/common/enum/user-auth-group.enum';
 import { Role } from 'src/common/enum/roles.enum';
@@ -31,9 +33,35 @@ export class CgiSftpController {
   async list(): Promise<string> {
     return await this.cgiSftpService.list();
   }
+  
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @Roles({
+    userAuthGroup: IDIR_USER_AUTH_GROUP_LIST,
+    oneOf: [Role.READ_PERMIT],
+  })
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadCGIFile(@UploadedFile() file: Express.Multer.File) {
+  uploadCGIFile(@UploadedFile(new ParseFilePipe({
+    validators: [
+      new MaxFileSizeValidator({ maxSize: 100000000 }),
+      /**
+       * TODO explore custom validator to verify files magic number rather
+       * than extention in the filename. Also, accept multiple file types */
+      //new FileTypeValidator({ fileType: 'pdf' }),
+    ],
+  }),
+) file: Express.Multer.File) {
     console.log(file);
   }
 }
