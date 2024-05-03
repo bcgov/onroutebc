30a69fcf4efbf550061c091b4aec06c7af9de0bb
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2024-05-03 13:09:23 -0700

    updating API docs

--- vehicles/src/modules/shopping-cart/dto/response/result.dto.ts
+++ vehicles/src/modules/shopping-cart/dto/response/result.dto.ts
@@ -1,5 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ResultDto {
+  @ApiProperty({
+    description: 'The collection of identifiers that succeeded.',
+    example: ['74', '76'],
+  })
   success: string[];
 
+  @ApiProperty({
+    description: 'The collection of identifiers that failed.',
+    example: ['45', '866'],
+  })
   failure: string[];
 }
