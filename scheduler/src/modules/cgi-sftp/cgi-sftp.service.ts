500d0cd7279c98485fb2579b6a79c2113f671023
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-05-06 12:49:05 -0600

    sftp changes

--- scheduler/src/modules/cgi-sftp/cgi-sftp.service.ts
+++ scheduler/src/modules/cgi-sftp/cgi-sftp.service.ts
@@ -7,6 +7,9 @@ export class CgiSftpService {
   async uploadToSFTP(filename: string, path: string) {
     const remoteFilePath: string = `./data/${filename}`;
     const localFilePath: string = `${path}/${filename}`;
+    console.log('********************************************************************')
+    console.log('Listing files::',this.sftpClient.list(path))
+    console.log('********************************************************************')
     const uploadStatus = await this.sftpClient.upload(
       remoteFilePath,
       localFilePath,
