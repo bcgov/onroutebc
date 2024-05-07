648f62daca80459ba77f9ffa72c4216b6880805b
bcgov-brwang <bcgov.brwang@gmail.com>
2024-05-07 09:58:24 -0700

    update config file

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "cypress";
-
+ 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    // baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://onroutebc-1327-frontend.apps.silver.devops.gov.bc.ca',
+    // baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -12,31 +14,33 @@ export default defineConfig({
   // bruce test
   env: {
     // baseUrl: 'http://localhost:3000/',
-
+ 
     // test url
-    baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/',
+    // baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/',
+    baseUrl: 'https://onroutebc-1327-frontend.apps.silver.devops.gov.bc.ca/',
+    // baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/',
     LOGIN_URL: 'https://logontest7.gov.bc.ca',
     PAYBC_URL: 'https://www.beanstream.com',
-    DEV_CC_NUMBER: '4030000010001234',
-    DEV_CC_CVD: '123',
-    DEV_CC_EXPMONTH: '12',
-    DEV_CC_EXPYEAR: '25',
-
+    CC_NUMBER: '4030000010001234',
+    CC_CVD: '123',
+    CC_EXPMONTH: '12',
+    CC_EXPYEAR: '25',
+ 
     // DEV
-    // TEST_USER: 'Tomstrucking',
-    // TEST_PASSWORD: 'Orbc123#',
-
-    // TEST
-    TEST_USER: 'ORBCTST1',
-    TEST_PASSWORD: 'orbcTST2023+',
-
-
+    TEST_USER: 'Tomstrucking',
+    TEST_PASSWORD: 'Orbc123#',
+ 
+    // // TEST
+    // TEST_USER: 'ORBCTST1',
+    // TEST_PASSWORD: 'orbcTST2023+',
+ 
+ 
     // TEST
     // TEST_USER: 'ORBCTST3',
     // TEST_PASSWORD: 'orbcTST+2023',
-
-    // SELECT_VEHICLE: '61',
-    SELECT_VEHICLE: 'MCL36',
-    
+ 
+    SELECT_VEHICLE: '61',
+    // SELECT_VEHICLE: 'MCL36',
+   
   }
 });
