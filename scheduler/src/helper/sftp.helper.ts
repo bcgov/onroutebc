1f0d2a91709aa413f97dc55e883cf61f0fa9e08c
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-05-09 13:22:59 -0600

    remove unneccesary code

--- scheduler/src/helper/sftp.helper.ts
+++ scheduler/src/helper/sftp.helper.ts
@@ -1,21 +1,4 @@
-import { SFTPWrapper } from 'ssh2';
 import * as Client from 'ssh2-sftp-client';
-
-export const config = {
-  host: process.env.CFS_SFTP_HOST,
-  port: Number(process.env.CFS_SFTP_PORT),
-  privateKey: process.env.CFS_PRIVATE_KEY,
-  passphrase: process.env.CFS_PRIVATE_KEY_PASSPHRASE,
-  username: process.env.CFS_SFTP_USERNAME,
-};
-
-export const getSFTPConnection = async (): Promise<SFTPWrapper> => {
-  const sftp = new Client();
-  const connectionInfo: Client.ConnectOptions = getSFTPConnectionInfo();
-
-  const sftpWrapper: SFTPWrapper = await sftp.connect(connectionInfo);
-  return sftpWrapper;
-};
 
 export const getSFTPConnectionInfo = (): Client.ConnectOptions => {
   const host = process.env.CFS_SFTP_HOST;
