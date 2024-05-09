0cc55cbb5b15bdfa707670d6930b9536df766107
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-05-09 15:45:40 -0600

    changes from file read to passing file as argument

--- scheduler/src/modules/cgi-sftp/cgi-sftp.controller.ts
+++ scheduler/src/modules/cgi-sftp/cgi-sftp.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CgiSftpService } from './cgi-sftp.service';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiQuery } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiBearerAuth()
 @Controller('cgi-sftp')
@@ -8,7 +15,24 @@ export class CgiSftpController {
   constructor(private readonly cgiSftpService: CgiSftpService) {}
 
   @Post()
-  upload() {
-    this.cgiSftpService.upload();
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiQuery({ name: 'fileName', required: true })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  upload(
+    @UploadedFile() file: Express.Multer.File,
+    @Query('fileName') fileName: string,
+  ) {
+    this.cgiSftpService.upload(file, fileName);
   }
 }
--- scheduler/src/modules/cgi-sftp/cgi-sftp.service.ts
+++ scheduler/src/modules/cgi-sftp/cgi-sftp.service.ts
@@ -1,36 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
-import * as path from 'path';
 import { getSFTPConnectionInfo } from 'src/helper/sftp.helper';
 import * as Client from 'ssh2-sftp-client';
 
 @Injectable()
 export class CgiSftpService {
-  upload() {
+  upload(fileData: Express.Multer.File, fileName: string) {
     const sftp = new Client();
     const connectionInfo: Client.ConnectOptions = getSFTPConnectionInfo();
-    const files: string[] = [];
-    const localPath = '/tmp';
     const remotePath = './data/';
-    fs.readdirSync(localPath).forEach((file) => {
-      if (fs.statSync(path.join(localPath, file)).isFile()) files.push(file);
-    });
-    files.forEach((file) => {
-      const data = fs.createReadStream(path.join(localPath, file));
-      sftp
-        .connect(connectionInfo)
-        .then(() => {
-          return sftp.put(data, remotePath + file);
-        })
-        .then(() => {
-          return sftp.end();
-        })
-        .then(() => {
-          return fs.rmSync(path.join(localPath, file));
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+    sftp
+      .connect(connectionInfo)
+      .then(() => {
+        return sftp.put(fileData.buffer, remotePath + fileName);
+      })
+      .then(() => {
+        return sftp.end();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
