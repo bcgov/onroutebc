464563d5fc39a0010f199d7ed3531f57e0227311
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2024-05-10 18:11:27 -0700

    Handle leap year for calculating expiry dates with 1-year duration, add test cases, refactor and remove hardcoded duration selections

--- frontend/src/features/permits/constants/tros.ts
+++ frontend/src/features/permits/constants/tros.ts
@@ -1,5 +1,6 @@
 import { tros } from "./tros.json";
 import { PermitCommodity } from "../types/PermitCommodity";
+import { BASE_DAYS_IN_YEAR } from "./constants";
 
 export const TROS_INELIGIBLE_POWERUNITS = [...tros.ineligiblePowerUnitSubtypes];
 export const TROS_INELIGIBLE_TRAILERS = [...tros.ineligibleTrailerSubtypes];
@@ -9,3 +10,19 @@ export const MANDATORY_TROS_COMMODITIES: PermitCommodity[] =
     (commodity: PermitCommodity) =>
       commodity.condition === "CVSE-1000" || commodity.condition === "CVSE-1070"
   );
+
+export const MIN_TROS_DURATION = 30;
+export const TROS_DURATION_OPTIONS = [
+  { value: MIN_TROS_DURATION, label: "30 Days" },
+  { value: 60, label: "60 Days" },
+  { value: 90, label: "90 Days" },
+  { value: 120, label: "120 Days" },
+  { value: 150, label: "150 Days" },
+  { value: 180, label: "180 Days" },
+  { value: 210, label: "210 Days" },
+  { value: 240, label: "240 Days" },
+  { value: 270, label: "270 Days" },
+  { value: 300, label: "300 Days" },
+  { value: 330, label: "330 Days" },
+  { value: BASE_DAYS_IN_YEAR, label: "1 Year" },
+];
---
+++ frontend/src/features/permits/helpers/tests/permitState.test.ts
@@ -0,0 +1,46 @@
+import dayjs from "dayjs";
+
+import { getExpiryDate } from "../permitState";
+import { DATE_FORMATS, dayjsToLocalStr } from "../../../../common/helpers/formatDate";
+import { BASE_DAYS_IN_YEAR } from "../../constants/constants";
+
+describe("Permit States", () => {
+  it("yields correct expiry date given start date and non-year duration", () => {
+    // Arrange
+    const startDate = dayjs("2023-09-01");
+    const duration = 30;
+
+    // Act
+    const expiryDate = getExpiryDate(startDate, duration);
+    const expiryDateStr = dayjsToLocalStr(expiryDate, DATE_FORMATS.DATEONLY);
+
+    // Assert
+    expect(expiryDateStr).toBe("2023-09-30"); // +30 (-1) days
+  });
+
+  it("yields correct expiry date (before leap year Feb-29) given start date and 1-year duration", () => {
+    // Arrange
+    const startDate = dayjs("2023-02-28");
+    const duration = BASE_DAYS_IN_YEAR;
+
+    // Act
+    const expiryDate = getExpiryDate(startDate, duration);
+    const expiryDateStr = dayjsToLocalStr(expiryDate, DATE_FORMATS.DATEONLY);
+
+    // Assert
+    expect(expiryDateStr).toBe("2024-02-27"); // +365 (-1) days
+  });
+
+  it("yields correct expiry date for leap year given start date and 1-year duration", () => {
+    // Arrange
+    const startDate = dayjs("2023-03-01");
+    const duration = BASE_DAYS_IN_YEAR;
+
+    // Act
+    const expiryDate = getExpiryDate(startDate, duration);
+    const expiryDateStr = dayjsToLocalStr(expiryDate, DATE_FORMATS.DATEONLY);
+
+    // Assert
+    expect(expiryDateStr).toBe("2024-02-29"); // +366 (-1) days
+  });
+});
