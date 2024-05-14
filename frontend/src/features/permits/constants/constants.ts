5aefa14d09409b1e70e11fa655aef6208d768869
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2024-05-14 13:34:12 -0700

    Reduce duplication

--- frontend/src/features/permits/constants/constants.ts
+++ frontend/src/features/permits/constants/constants.ts
@@ -26,3 +26,19 @@ export const PERMIT_TYPE_CHOOSE_FROM_OPTIONS = [
 ];
 
 export const BASE_DAYS_IN_YEAR = 365;
+export const COMMON_MIN_DURATION = 30;
+
+export const COMMON_DURATION_OPTIONS = [
+  { value: COMMON_MIN_DURATION, label: "30 Days" },
+  { value: 60, label: "60 Days" },
+  { value: 90, label: "90 Days" },
+  { value: 120, label: "120 Days" },
+  { value: 150, label: "150 Days" },
+  { value: 180, label: "180 Days" },
+  { value: 210, label: "210 Days" },
+  { value: 240, label: "240 Days" },
+  { value: 270, label: "270 Days" },
+  { value: 300, label: "300 Days" },
+  { value: 330, label: "330 Days" },
+  { value: BASE_DAYS_IN_YEAR, label: "1 Year" },
+];
--- frontend/src/features/permits/constants/trow.ts
+++ frontend/src/features/permits/constants/trow.ts
@@ -1,23 +1,10 @@
 import { trow } from "./trow.json";
 import { PermitCommodity } from "../types/PermitCommodity";
-import { BASE_DAYS_IN_YEAR } from "./constants";
+import { COMMON_DURATION_OPTIONS, COMMON_MIN_DURATION } from "./constants";
 
 export const TROW_INELIGIBLE_POWERUNITS = [...trow.ineligiblePowerUnitSubtypes];
 export const TROW_INELIGIBLE_TRAILERS = [...trow.ineligibleTrailerSubtypes];
 export const TROW_COMMODITIES: PermitCommodity[] = [...trow.commodities];
 export const MANDATORY_TROW_COMMODITIES: PermitCommodity[] = [...TROW_COMMODITIES];
-export const MIN_TROW_DURATION = 30;
-export const TROW_DURATION_OPTIONS = [
-  { value: MIN_TROW_DURATION, label: "30 Days" },
-  { value: 60, label: "60 Days" },
-  { value: 90, label: "90 Days" },
-  { value: 120, label: "120 Days" },
-  { value: 150, label: "150 Days" },
-  { value: 180, label: "180 Days" },
-  { value: 210, label: "210 Days" },
-  { value: 240, label: "240 Days" },
-  { value: 270, label: "270 Days" },
-  { value: 300, label: "300 Days" },
-  { value: 330, label: "330 Days" },
-  { value: BASE_DAYS_IN_YEAR, label: "1 Year" },
-];
+export const MIN_TROW_DURATION = COMMON_MIN_DURATION;
+export const TROW_DURATION_OPTIONS = [...COMMON_DURATION_OPTIONS];
