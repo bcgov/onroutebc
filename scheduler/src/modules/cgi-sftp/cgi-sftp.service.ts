ddbab8f6645371ab70f56e1e894d480e1c23ee18
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-05-14 16:17:20 -0600

    parameterizing variables, added finally block

--- scheduler/src/modules/cgi-sftp/cgi-sftp.service.ts
+++ scheduler/src/modules/cgi-sftp/cgi-sftp.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { getSFTPConnectionInfo } from 'src/helper/sftp.helper';
 import * as Client from 'ssh2-sftp-client';
 
@@ -7,20 +7,19 @@ export class CgiSftpService {
   upload(fileData: Express.Multer.File, fileName: string) {
     const sftp = new Client();
     const connectionInfo: Client.ConnectOptions = getSFTPConnectionInfo();
-    const remotePath = './data/';
+    const remotePath = process.env.CFS_REMOTE_PATH;
     sftp
       .connect(connectionInfo)
       .then(() => {
         console.log('writing file', remotePath + fileName);
         return sftp.put(fileData.buffer, remotePath + fileName);
       })
-      .then(() => {
-        console.log('closing connection');
-        return sftp.end();
-      })
       .catch((err) => {
-        console.log('caught error');
-        console.log(err);
+        throw new InternalServerErrorException(err);
+      })
+      .finally(() => {
+        console.log('closing connection');
+        void sftp.end();
       });
   }
 }
