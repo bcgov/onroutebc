3e38db3b866ca94ae34209f59b750d5a862d3d9f
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-05-15 14:57:11 -0600

    removing controller and parameterizing docker

--- scheduler/src/modules/cgi-sftp/cgi-sftp.module.ts
+++ scheduler/src/modules/cgi-sftp/cgi-sftp.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CgiSftpService } from './cgi-sftp.service';
-import { CgiSftpController } from './cgi-sftp.controller';
 
 @Module({
   providers: [CgiSftpService],
-  controllers: [CgiSftpController],
 })
 export class CgiSftpModule {}
