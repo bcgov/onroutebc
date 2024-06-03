5770939508e56e2ea84a9455ce056b1565692d34
Krishnan Subramanian <84348052+krishnan-aot@users.noreply.github.com>
2024-06-03 15:54:52 -0700

    Update app.controller.ts
    Signed-off-by: Krishnan Subramanian <84348052+krishnan-aot@users.noreply.github.com>

--- scheduler/src/app.controller.ts
+++ scheduler/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
-
+// sample comment
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
