477a4902c40ff8aab7099be5733ca80c354969dd
bcgov-brwang <bcgov.brwang@gmail.com>
2024-06-05 12:48:22 -0700

    ORV2-2161: config commit don't push to remote

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "cypress";
  
 export default defineConfig({
   e2e: {
-    // baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
     // baseUrl: 'https://onroutebc-1327-frontend.apps.silver.devops.gov.bc.ca',
-    baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca',
+    // baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -13,12 +13,12 @@ export default defineConfig({
   },
   // bruce test
   env: {
-    // baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://localhost:3000/',
  
     // test url
     // baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/',
     // baseUrl: 'https://onroutebc-1327-frontend.apps.silver.devops.gov.bc.ca/',
-    baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/',
+    // baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/',
     LOGIN_URL: 'https://logontest7.gov.bc.ca',
     PAYBC_URL: 'https://www.beanstream.com',
     CC_NUMBER: '4030000010001234',
@@ -26,21 +26,29 @@ export default defineConfig({
     CC_EXPMONTH: '12',
     CC_EXPYEAR: '25',
  
-    // // DEV
+    // // // DEV BCeID
     // TEST_USER: 'Tomstrucking',
     // TEST_PASSWORD: 'Orbc123#',
  
-    // // TEST
-    TEST_USER: 'ORBCTST1',
-    TEST_PASSWORD: 'orbcTST2023+',
+    // // DEV IDIR
+    TEST_USER: 'brwang',
+    TEST_PASSWORD: 'Dslcfjzbjhmdys_10',
+
+
+    // // TEST works
+    // TEST_USER: 'ORBCTST1',
+    // TEST_PASSWORD: 'orbcTST2023+',
  
  
     // TEST
     // TEST_USER: 'ORBCTST3',
     // TEST_PASSWORD: 'orbcTST+2023',
  
-    // SELECT_VEHICLE: '61',
-    SELECT_VEHICLE: 'MCL36',
+    // DEV
+    SELECT_VEHICLE: '61',
+
+    // TEST
+    // SELECT_VEHICLE: 'MCL36',
    
   }
 });
