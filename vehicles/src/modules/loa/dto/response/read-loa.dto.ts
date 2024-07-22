8469c501c69e02ba11454004a65f4f2e077628ec
Praveen Raju <80779423+praju-aot@users.noreply.github.com>
2024-07-22 17:59:48 -0400

    feat: ORV2-2299 Letter Of Authorization : Part 1 (#1500)
    Signed-off-by: Praveen Raju <80779423+praju-aot@users.noreply.github.com>
    Co-authored-by: gchauhan-aot <garima.chauhan@aot-technologies.com>
    Co-authored-by: zgong-gov <ZhongYu.Gong@gov.bc.ca>
    Co-authored-by: Krishnan Subramanian <84348052+krishnan-aot@users.noreply.github.com>

---
+++ vehicles/src/modules/loa/dto/response/read-loa.dto.ts
@@ -0,0 +1,92 @@
+import { AutoMap } from '@automapper/classes';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum } from 'class-validator';
+import { PermitType } from 'src/common/enum/permit-type.enum';
+
+export class ReadLoaDto {
+  @AutoMap()
+  @ApiProperty({
+    example: 1,
+    description: 'Unique identifier for the LoA.',
+  })
+  loaId: number;
+
+  @AutoMap()
+  @ApiProperty({
+    example: 1,
+    description: 'Unique LoA Number',
+  })
+  loaNumber: number;
+
+  @AutoMap()
+  @ApiProperty({
+    description: 'Id of the company requesting the LoA.',
+    example: 74,
+    required: false,
+  })
+  companyId: number;
+
+  @AutoMap()
+  @ApiProperty({
+    example: '2023-07-13',
+    description: 'Effective start date of the LoA.',
+  })
+  startDate: string;
+
+  @AutoMap()
+  @ApiProperty({
+    example: '2023-08-13',
+    description: 'Effective end date of the LoA.',
+    required: false,
+  })
+  expiryDate?: string;
+
+  @AutoMap()
+  @ApiProperty({
+    description: 'DMS Document ID used to retrieve the PDF of the LoA.',
+  })
+  documentId?: string;
+
+  @AutoMap()
+  @ApiProperty({
+    example: 'sample.pdf',
+    description: 'DMS Document Name of the LoA.',
+  })
+  fileName: string;
+
+  @AutoMap()
+  @ApiProperty({
+    example: 'This LoA was modified for so-and-so reason.',
+    description: 'Comment/Reason for modifying an LoA.',
+    required: false,
+  })
+  comment?: string;
+
+  @AutoMap()
+  @IsEnum(PermitType, { each: true })
+  @ApiProperty({
+    isArray: true,
+    enum: PermitType,
+    description: 'List of permit types associated with the LoA.',
+    example: [PermitType.TERM_OVERSIZE, PermitType.TERM_OVERWEIGHT],
+  })
+  loaPermitType: PermitType[];
+
+  @AutoMap()
+  @ApiProperty({
+    description: 'List of trailer IDs associated with the LoA.',
+    isArray: true,
+    required: false,
+    example: ['1'],
+  })
+  trailers?: string[];
+
+  @AutoMap()
+  @ApiProperty({
+    description: 'List of power unit IDs associated with the LoA.',
+    isArray: true,
+    required: false,
+    example: ['1'],
+  })
+  powerUnits?: string[];
+}
