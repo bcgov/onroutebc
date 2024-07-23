260ba43e6214cac892160859098f307bcb071436
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2024-07-23 09:24:46 -0700

    Exclusively usePermissions hook in RenderIf

--- frontend/src/common/components/reusable/RenderIf.tsx
+++ frontend/src/common/components/reusable/RenderIf.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { usePermissionMatrix } from "../../authentication/PermissionMatrix";
 import { PermissionConfigType } from "../../authentication/types";
 
@@ -7,69 +6,17 @@ import { PermissionConfigType } from "../../authentication/types";
  */
 export const RenderIf = ({
   component,
-  disallowedAuthGroups,
-  allowedIDIRAuthGroups,
-  allowedBCeIDAuthGroups,
-  featureFlag,
-  additionalConditionToCall,
-  onlyConditionToCheck
+  ...permissionConfig
 }: {
   /**
    * The component to be rendered.
    */
   component: JSX.Element;
 } & PermissionConfigType): JSX.Element => {
-  const aas = usePermissionMatrix({
-    disallowedAuthGroups,
-    allowedIDIRAuthGroups,
-    allowedBCeIDAuthGroups,
-    featureFlag,
-    additionalConditionToCall,
-    onlyConditionToCheck
-  });
-  console.log('aas:::', aas);
-  if (aas) {
-    return <>{component}</>
+  const shouldRender = usePermissionMatrix(permissionConfig);
+  if (shouldRender) {
+    return <>{component}</>;
   } else {
-    return <></>
+    return <></>;
   }
-  // const { userDetails, idirUserDetails } = useContext(OnRouteBCContext);
-  // const { data: featureFlags } = useFeatureFlagsQuery();
-  // const isIdir = Boolean(idirUserDetails?.userAuthGroup);
-  
-  // // If the onlyConditionToCheck function is given, call that alone and exit.
-  // if (onlyConditionToCheck && onlyConditionToCheck()) {
-  //   return <>{component}</>; 
-  // }
-  // let shouldRender = false;
-  // let currentUserAuthGroup;
-  // if (isIdir) {
-  //   currentUserAuthGroup = idirUserDetails?.userAuthGroup;
-  //   shouldRender = Boolean(
-  //     currentUserAuthGroup &&
-  //       allowedIDIRAuthGroups?.includes(currentUserAuthGroup),
-  //   );
-  // } else {
-  //   currentUserAuthGroup = userDetails?.userAuthGroup;
-  //   shouldRender = Boolean(
-  //     currentUserAuthGroup &&
-  //       allowedBCeIDAuthGroups?.includes(currentUserAuthGroup),
-  //   );
-  // }
-  // if (disallowedAuthGroups?.length) {
-  //   shouldRender = Boolean(
-  //     currentUserAuthGroup &&
-  //       !disallowedAuthGroups.includes(currentUserAuthGroup),
-  //   );
-  // }
-  // if (shouldRender && additionalConditionToCall) {
-  //   shouldRender = shouldRender && additionalConditionToCall();
-  // }
-  // if (featureFlag) {
-  //   shouldRender = featureFlags?.[featureFlag] === "ENABLED";
-  // }
-  // if (shouldRender) {
-  //   return <>{component}</>;
-  // }
-  // return <></>;
 };
