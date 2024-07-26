79515519cbb7d357d0aa4eb498eb2a3eb2ae3633
gchauhan-aot <113390759+gchauhan-aot@users.noreply.github.com>
2024-07-26 12:47:41 -0600

    ORV2-1506 Database Table Creation for OnRouteBC Special Authorization (#1518)
    Co-authored-by: John Fletcher <john.fletcher@aot-technologies.com>

--- database/mssql/test/versions/v_35_2_test.sql
+++ database/mssql/test/versions/v_35_2_test.sql
@@ -1,6 +1,6 @@
 -- Test that the auth groups have been inserted correctly
 SET NOCOUNT ON
 
-SELECT COUNT(*) FROM $(DB_NAME).[access].[ORBC_USER_AUTH_GROUP_TYPE] 
+SELECT COUNT(*) FROM $(DB_NAME).[access].[ORBC_GROUP_ROLE] 
 WHERE USER_AUTH_GROUP_TYPE = 'FINANCE'
    AND ROLE_TYPE = 'ORBC-READ-LCV-FLAG'
