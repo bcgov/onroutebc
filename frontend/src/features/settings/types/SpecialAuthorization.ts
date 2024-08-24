ea058eda532443557bf613e74f05eaf74559b60b
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2024-08-23 17:34:08 -0700

    Update enum names for no-fee permit types

--- frontend/src/features/settings/types/SpecialAuthorization.ts
+++ frontend/src/features/settings/types/SpecialAuthorization.ts
@@ -3,10 +3,10 @@ import { PermitType } from "../../permits/types/PermitType";
 
 export const NO_FEE_PERMIT_TYPES = {
   CA_GOVT: "CA_GOVT",
-  MUNICPALITY: "MUNICPALITY",
+  MUNICIPALITY: "MUNICIPALITY",
   SCHOOL: "SCHOOL",
-  USA_GOVT: "USA_GOVT",
-  ANY_USA_GOVT: "ANY_USA_GOVT",
+  USA_FEDERAL_GOVT: "USA_FEDERAL_GOVT",
+  OTHER_USA_GOVT: "OTHER_USA_GOVT",
 } as const;
 
 export type NoFeePermitType = typeof NO_FEE_PERMIT_TYPES[keyof typeof NO_FEE_PERMIT_TYPES];
@@ -17,11 +17,11 @@ export const noFeePermitTypeDescription = (noFeePermitType: NoFeePermitType) => 
   switch (noFeePermitType) {
     case NO_FEE_PERMIT_TYPES.CA_GOVT:
       return "The government of Canada or any province or territory";
-    case NO_FEE_PERMIT_TYPES.MUNICPALITY:
+    case NO_FEE_PERMIT_TYPES.MUNICIPALITY:
       return "A municipality";
     case NO_FEE_PERMIT_TYPES.SCHOOL:
       return "A school district outside of BC (S. 9 Commercial Transport Act)";
-    case NO_FEE_PERMIT_TYPES.USA_GOVT:
+    case NO_FEE_PERMIT_TYPES.USA_FEDERAL_GOVT:
       return "The government of the United States of America";
     default:
       return "The government of any state or county in the United States of America";
