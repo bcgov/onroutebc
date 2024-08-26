3a1aae9889c9a2067192da28678246f41ad068b3
Krishnan Subramanian <krishnan.s@aot-technologies.com>
2024-08-26 16:33:22 -0700

    Remove comment

--- vehicles/src/common/decorator/permissions.decorator.ts
+++ vehicles/src/common/decorator/permissions.decorator.ts
@@ -2,7 +2,5 @@ import { SetMetadata } from '@nestjs/common';
 import { IPermissions } from '../interface/permissions.interface';
 import { PERMISSIONS_KEY } from '../constants/api.constant';
 
-// Todo: Change IPermissions array to a single object when removing
-// ...permissions.
 export const Permissions = (permissions: IPermissions) =>
   SetMetadata(PERMISSIONS_KEY, permissions);
