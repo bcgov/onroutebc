5a767d3e9befc2365ab7432782ff65a3ae82df59
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-08-26 11:29:59 -0600

    review comments

--- database/mssql/test/versions/v_40_1_test.sql
+++ database/mssql/test/versions/v_40_1_test.sql
@@ -1,4 +1,4 @@
--- Test that the trigger sets previous revision to SUPERSEDED
+-- Test that no fee type MUNICIPALITY has been inserted correctly
 SET NOCOUNT ON
 SELECT COUNT(*) FROM $(DB_NAME).permit.ORBC_NO_FEE_TYPE 
 WHERE 
--- database/mssql/test/versions/v_40_2_test.sql
+++ database/mssql/test/versions/v_40_2_test.sql
@@ -1,4 +1,4 @@
--- Test that the trigger sets previous revision to SUPERSEDED
+-- Test that no fee type USA_FEDERAL_GOVT has been inserted correctly
 SET NOCOUNT ON
 SELECT COUNT(*) FROM $(DB_NAME).permit.ORBC_NO_FEE_TYPE 
 WHERE 
