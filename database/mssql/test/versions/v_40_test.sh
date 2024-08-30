f7d8511ecdecc60b42f588d7f30240a304320430
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-08-30 14:12:59 -0600

    remove test result var from echo

--- database/mssql/test/versions/v_40_test.sh
+++ database/mssql/test/versions/v_40_test.sh
@@ -31,14 +31,14 @@ fi
 
 TEST_40_4_RESULT=$(/opt/mssql-tools/bin/sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -v DB_NAME=${DATABASE} -h -1 -i ${TESTS_DIR}/v_40_4_test.sql | xargs)
 if [[ "$TEST_40_4_RESULT" != "NULL" ]]; then
-    echo "Test 40.4 passed: added foreign key constraints to No fee type table $TEST_40_4_RESULT"
+    echo "Test 40.4 passed: added foreign key constraints to No fee type table"
 else
-    echo "******** Test 40.4 failed: Could not add foreign constraints to No fee type table $TEST_40_4_RESULT"
+    echo "******** Test 40.4 failed: Could not add foreign constraints to No fee type table"
 fi
 
 TEST_40_5_RESULT=$(/opt/mssql-tools/bin/sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -v DB_NAME=${DATABASE} -h -1 -i ${TESTS_DIR}/v_40_5_test.sql | xargs)
 if [[ "$TEST_40_5_RESULT" != "NULL"  ]]; then
-    echo "Test 40.5 passed: added primary key constraints to No fee type table $TEST_40_5_RESULT"
+    echo "Test 40.5 passed: added primary key constraints to No fee type table"
 else
-    echo "******** Test 40.5 failed: Could not add primary key constraints to No fee type table $TEST_40_5_RESULT"
+    echo "******** Test 40.5 failed: Could not add primary key constraints to No fee type table"
 fi
