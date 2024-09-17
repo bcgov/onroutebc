7dddbad4b10496a815409ba96d25861c97eb7306
GlenAOT <160973940+GlenAOT@users.noreply.github.com>
2024-09-17 10:40:04 -0700

    comment out relevant counter code, pending future removal

--- frontend/src/common/components/tabs/types/TabComponentProps.ts
+++ frontend/src/common/components/tabs/types/TabComponentProps.ts
@@ -2,6 +2,7 @@ import { Nullable } from "../../../types/common";
 
 export interface TabComponentProps {
   label: string;
-  count?: Nullable<number>;
+  // TODO remove this if we no longer need tab counters
+  // count?: Nullable<number>;
   component: JSX.Element;
 }
