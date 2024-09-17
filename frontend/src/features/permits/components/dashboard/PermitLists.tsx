0568121923616743739506c6b3103a4d290a98c6
GlenAOT <160973940+GlenAOT@users.noreply.github.com>
2024-09-17 10:19:20 -0700

    remove applications in progress counter

--- frontend/src/features/permits/components/dashboard/PermitLists.tsx
+++ frontend/src/features/permits/components/dashboard/PermitLists.tsx
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
-
+import React from "react";
 import { TabLayout } from "../../../../common/components/dashboard/TabLayout";
 import { StartApplicationAction } from "../../pages/Application/components/dashboard/StartApplicationAction";
 import { ActivePermitList } from "../permit-list/ActivePermitList";
 import { ExpiredPermitList } from "../permit-list/ExpiredPermitList";
 import { ApplicationsInProgressList } from "../permit-list/ApplicationsInProgressList";
 import { ApplicationsInReviewList } from "../permit-list/ApplicationsInReviewList";
-import { Nullable } from "../../../../common/types/common";
 import { usePermissionMatrix } from "../../../../common/authentication/PermissionMatrix";
 import { RenderIf } from "../../../../common/components/reusable/RenderIf";
 
 export const PermitLists = React.memo(() => {
-  const [applicationsInProgressCount, setApplicationsInProgressCount] =
-    useState<Nullable<number>>();
-  const handleApplicationsCountChange = (count: number) => {
-    setApplicationsInProgressCount(count);
-  };
   const tabs = [];
 
   const showApplicationsInProgressTab = usePermissionMatrix({
@@ -28,12 +21,7 @@ export const PermitLists = React.memo(() => {
   if (showApplicationsInProgressTab) {
     tabs.push({
       label: "Applications in Progress",
-      count: applicationsInProgressCount,
-      component: (
-        <ApplicationsInProgressList
-          onCountChange={handleApplicationsCountChange}
-        />
-      ),
+      component: <ApplicationsInProgressList />,
     });
   }
 
