f2cfe87ce730d468d786925092496888c32b4549
praju-aot <praveen.raju@aot-technologies.com>
2024-09-24 13:51:40 -0400

    ORV2-2693 Add STOS permit templates to OnRouteBC
    Code changes for STOS permit templates including VOID and REVOKED.
    Tickets have been created for uploading the templates to BCBox.

--- vehicles/src/common/enum/template-name.enum.ts
+++ vehicles/src/common/enum/template-name.enum.ts
@@ -1,6 +1,21 @@
+import { ApplicationStatus } from './application-status.enum';
+import { PermitType } from './permit-type.enum';
+
 export enum TemplateName {
   PERMIT = 'PERMIT',
   PAYMENT_RECEIPT = 'PAYMENT_RECEIPT',
   PERMIT_VOID = 'PERMIT_VOID',
   PERMIT_REVOKED = 'PERMIT_REVOKED',
+  PERMIT_STOS = 'PERMIT_STOS',
+  PERMIT_STOS_VOID = 'PERMIT_STOS_VOID',
+  PERMIT_STOS_REVOKED = 'PERMIT_STOS_REVOKED',
+  PERMIT_TROS = 'PERMIT_TROS',
+  PERMIT_TROS_VOID = 'PERMIT_TROS_VOID',
+  PERMIT_TROS_REVOKED = 'PERMIT_TROS_REVOKED',
 }
+
+export type PermitTemplateMapping = {
+  [K in ApplicationStatus]?: {
+    [T in PermitType | 'default']?: TemplateName;
+  };
+};
