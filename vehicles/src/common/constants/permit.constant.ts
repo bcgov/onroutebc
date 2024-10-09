db8797198e8064d50cc66dc028ed4fbedf252d36
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-10-09 16:15:13 -0600

    moved to api constant

--- vehicles/src/common/constants/permit.constant.ts
+++ vehicles/src/common/constants/permit.constant.ts
@@ -8,7 +8,6 @@ export const TROW_TERM = 30;
 export const TROW_PRICE_PER_TERM = 100;
 export const TROW_MIN_VALID_DURATION = 1;
 export const TROW_MAX_VALID_DURATION = 366;
-export const TIMEZONE_PACIFIC = "America/Vancouver";
 
 export const PERMIT_TYPES_FOR_QUEUE: readonly PermitType[] = [
   PermitType.SINGLE_TRIP_OVERSIZE,
