e43200aaa7cc65f04ba3f7c4223ed11337750553
gchauhan-aot <garima.chauhan@aot-technologies.com>
2024-10-10 12:49:21 -0600

    resolving linting issues

---
+++ scheduler/src/modules/cgi-sftp/cgi-sftp.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Controller,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CgiSftpService } from './cgi-sftp.service';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiQuery } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@ApiBearerAuth()
+@Controller('cgi-sftp')
+export class CgiSftpController {
+  constructor(private readonly cgiSftpService: CgiSftpService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiQuery({ name: 'fileName', required: true })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  async upload(
+    @UploadedFile() file: Express.Multer.File,
+    @Query('fileName') fileName: string,
+  ) {
+    return await this.cgiSftpService.upload(file, fileName);
+  }
+}
