f5af7f3333db64d3be6f3fe87142d11008af7cf5
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2024-11-19 15:51:47 -0800

    ORV2-3027: added bceid user crud on vehicles
    ORV2-3027: added bceid user crud on vehicles

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -10,11 +10,16 @@ module.exports = defineConfig({
     env: {
       idir_username: '', 
       idir_password: '',
-      bceid_username: 'ORBCTST1', 
-      bceid_password: 'orbcTST2023+',
+      bceid_username: '', 
+      bceid_password: '',
       update_term_oversize_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/applications/581', 
       new_tros_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/create-application/TROS', 
       new_trow_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/create-application/TROW', 
+      new_power_unit_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/manage-vehicles/add-powerunit',
+      update_power_unit_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/manage-vehicles/power-units/106',
+      manage_vehicle_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/manage-vehicles',
+      new_trailer_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/manage-vehicles/add-trailer',
+      update_trailer_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/manage-vehicles/trailers/106',
     },
   },
 });
