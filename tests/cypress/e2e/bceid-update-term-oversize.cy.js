ebc8aea930ba697127d5954979df9104e2a4534d
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2024-11-27 10:29:43 -0800

    new a tros application first then update it
    new a tros application first then update it

--- tests/cypress/e2e/bceid-update-term-oversize.cy.js
+++ tests/cypress/e2e/bceid-update-term-oversize.cy.js
@@ -21,7 +21,60 @@ describe('Login Test for OnRouteBC', () => {
     cy.get('[name="btnSubmit"]').click();
     cy.wait(5000);
 
-    cy.visit('/applications/12');
+
+
+
+    // new a TROS application first
+    cy.visit('/create-application/TROS');
+    cy.wait(5000);
+
+    // fill out the form
+    cy.get('#application-select-vehicle').type('MCL36');
+    cy.wait(5000);
+
+    cy.get('[name="permitData.vehicleDetails.vin"]').click({ force: true }).type('MCL36A');
+    cy.wait(5000);
+
+    cy.get('[name="permitData.vehicleDetails.plate"]').type('L4NDO');
+    cy.wait(5000);
+
+    cy.get('[name="permitData.vehicleDetails.make"]').type('BMW');
+    cy.wait(5000);
+
+    cy.get('[name="permitData.vehicleDetails.year"]').type('2020');
+    cy.wait(5000);
+
+    cy.get('[id="mui-component-select-permitData.vehicleDetails.countryCode"]').scrollIntoView().click();
+    cy.wait(5000);
+
+    cy.get('[data-value="CA"]').click();
+    cy.wait(5000);
+
+    cy.get('[id="mui-component-select-permitData.vehicleDetails.provinceCode"]').click();
+    cy.wait(5000);
+
+    cy.get('[data-value="BC"]').click();
+    cy.wait(5000);
+
+    cy.get('[id="mui-component-select-permitData.vehicleDetails.vehicleType"]').click(({ force: true }));
+    cy.wait(5000);
+
+    cy.get('[data-value="powerUnit"]').click();
+    cy.wait(5000);
+
+    cy.get('[id="mui-component-select-permitData.vehicleDetails.vehicleSubType"]').click({ force: true });
+    cy.wait(5000);
+    
+    cy.get('[data-value="REGTRCK"]').click();
+    cy.wait(5000);
+
+    cy.get('[data-testid="continue-application-button"]').click({ force: true });
+    cy.wait(5000);
+
+    cy.visit('/applications');
+    cy.wait(5000);
+
+    cy.get('a.column-link--application-details').first().click();
     cy.wait(5000);
 
     // update phone ext
