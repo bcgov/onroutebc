0dd7552e32a6120ff877702ef6685a865a391e2e
praju-aot <praveen.raju@aot-technologies.com>
2025-01-06 12:40:59 -0500

    ORV2-3072 Determine permitIssuedBy field based on the issuer

--- vehicles/src/modules/permit-application-payment/application/application.service.ts
+++ vehicles/src/modules/permit-application-payment/application/application.service.ts
@@ -47,7 +47,10 @@ import {
 } from '../../../common/enum/user-role.enum';
 import { DeleteDto } from '../../common/dto/response/delete.dto';
 import { ReadApplicationMetadataDto } from './dto/response/read-application-metadata.dto';
-import { doesUserHaveRole } from '../../../common/helper/auth.helper';
+import {
+  doesUserHaveRole,
+  isIdirOrSAUser,
+} from '../../../common/helper/auth.helper';
 import {
   ACTIVE_APPLICATION_STATUS,
   ACTIVE_APPLICATION_STATUS_FOR_ISSUANCE,
@@ -698,13 +701,22 @@ export class ApplicationService {
           fetchedApplication.permitNumber = permitNumber;
           fetchedApplication.permitStatus = ApplicationStatus.ISSUED;
           fetchedApplication.permitIssueDateTime = new Date();
-          const issuer =
-            fetchedApplication?.issuer?.userGUID || currentUser.userGUID;
-          const permitIssuedBy =
-            currentUser.orbcUserDirectory === Directory.IDIR ||
-            currentUser.orbcUserDirectory === Directory.SERVICE_ACCOUNT
+          let issuer: string;
+          let permitIssuedBy: PermitIssuedBy;
+          if (fetchedApplication?.issuer?.userGUID) {
+            issuer = fetchedApplication?.issuer?.userGUID;
+            permitIssuedBy = isIdirOrSAUser(
+              fetchedApplication?.issuer?.directory,
+            )
               ? PermitIssuedBy.PPC
               : PermitIssuedBy.SELF_ISSUED;
+          } else {
+            issuer = currentUser.userGUID;
+            permitIssuedBy = isIdirOrSAUser(currentUser.orbcUserDirectory)
+              ? PermitIssuedBy.PPC
+              : PermitIssuedBy.SELF_ISSUED;
+          }
+
           const queryRunner = this.dataSource.createQueryRunner();
           await queryRunner.connect();
           await queryRunner.startTransaction();
