f6797d688353d92838997815debfa03016eaa4e5
Krishnan Subramanian <84348052+krishnan-aot@users.noreply.github.com>
2025-01-21 10:57:24 -0800

    ORV2-3263 BE Remove fax (#1744)

--- vehicles/src/common/helper/notification.helper.ts
+++ vehicles/src/common/helper/notification.helper.ts
@@ -1,24 +1,9 @@
 /**
- * Generates a fax email address by formatting the provided fax number.
+ * Validates a list of email addresses, removing duplicates and empty values.
  *
- * @param {string} fax - The fax number to format.
- * @returns {string} The formatted fax email address, or undefined if the fax number is invalid.
+ * @param {string[]} emailList - The list of email addresses to validate.
+ * @returns {string[]} The validated list of email addresses, without duplicates or empty values.
  */
-export const generateFaxEmail = (fax: string): string => {
-  //Removes plus(+),paranthesis and hyphens
-  const formattedFaxNumber = fax?.replace(/[+\D-]/g, '');
-
-  return formattedFaxNumber
-    ? process.env.BCGOV_FAX_EMAIL?.replace('<faxnumber>', formattedFaxNumber)
-    : undefined;
-};
-
-/**
- * Validates a list of email and fax addresses, removing duplicates and empty values.
- *
- * @param {string[]} emailList - The list of email and fax addresses to validate.
- * @returns {string[]} The validated list of email and fax addresses, without duplicates or empty values.
- */
-export const validateEmailandFaxList = (emailList: string[]): string[] => {
+export const validateEmailList = (emailList: string[]): string[] => {
   return Array.from(new Set(emailList?.filter(Boolean)));
 };
--- vehicles/src/modules/company-user-management/company/company.service.ts
+++ vehicles/src/modules/company-user-management/company/company.service.ts
@@ -257,7 +257,6 @@ export class CompanyService {
         companyPostalCode: readCompanyUserDto.mailingAddress.postalCode,
         companyEmail: readCompanyUserDto.email,
         companyPhoneNumber: readCompanyUserDto.phone,
-        companyFaxNumber: readCompanyUserDto.fax,
         primaryContactFirstname: readCompanyUserDto.primaryContact.firstName,
         primaryContactLastname: readCompanyUserDto.primaryContact.lastName,
         primaryContactEmail: readCompanyUserDto.primaryContact.email,
