64e3eae95813ed08fbef151e38f0da6169ef1252
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2025-01-30 15:31:39 -0800

    Revert "ORV2-3221: use TRKTRAC instead of DOLLIES"
    This reverts commit b3cc11097dd97eca5dd6a3b7636eb05673cd2351.

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -6,23 +6,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca',
-    baseUrl: 'https://onroutebc-uat-frontend.apps.silver.devops.gov.bc.ca',
-    // baseUrl: 'https://onroutebc-1725-frontend.apps.silver.devops.gov.bc.ca',
-    // baseUrl: 'https://onroutebc-1729-frontend.apps.silver.devops.gov.bc.ca',
+    baseUrl: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca',
     env: {
-      idir_username: 'TRORBCT1', 
-      idir_password: 'ydYa,GWCus4q5eGjuPoJ',
-      // DEV
-      // bceid_username: 'Tomstrucking', 
-      // bceid_password: 'Orbc123#',
-      // TEST
-      // bceid_username: 'ORBCTST1', 
-      // bceid_password: 'orbcTST2023+',
-      // UAT
-      bceid_username: 'ORBCBLANE', 
-      bceid_password: 'Ilmylilmm-Dn8',
-      
+      idir_username: '', 
+      idir_password: '',
+      bceid_username: '', 
+      bceid_password: '',
       update_term_oversize_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/applications/581', 
       new_tros_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/create-application/TROS', 
       new_trow_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/create-application/TROW', 
@@ -31,7 +20,6 @@ module.exports = defineConfig({
       manage_vehicle_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/manage-vehicles',
       new_trailer_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/manage-vehicles/add-trailer',
       update_trailer_url: 'https://onroutebc-test-frontend.apps.silver.devops.gov.bc.ca/manage-vehicles/trailers/106',
-      wait_time: 5000,
     },
   },
 });
