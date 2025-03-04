9efb069737074ba217109681aac190a9b253f757
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2025-03-04 13:10:39 -0800

    Integrate BC Geocoder API into frontend, add geocoder address search functionality, replace address input with Geocoder input

--- frontend/src/features/permits/pages/Application/components/form/PermitForm.tsx
+++ frontend/src/features/permits/pages/Application/components/form/PermitForm.tsx
@@ -40,6 +40,8 @@ export const PermitForm = () => {
     revisionHistory,
     commodityOptions,
     highwaySequence,
+    tripOrigin,
+    tripDestination,
     totalDistance,
     nextAllowedSubtypes,
     powerUnitSubtypeNamesMap,
@@ -58,6 +60,8 @@ export const PermitForm = () => {
     onClearVehicle,
     onUpdateLOAs,
     onUpdateHighwaySequence,
+    onUpdateTripOrigin,
+    onUpdateTripDestination,
     onUpdateTotalDistance,
     onUpdateVehicleConfigTrailers,
     onChangeCommodityType,
@@ -136,8 +140,12 @@ export const PermitForm = () => {
           feature={feature}
           permitType={permitType}
           highwaySequence={highwaySequence}
+          tripOrigin={tripOrigin}
+          tripDestination={tripDestination}
           totalDistance={totalDistance}
           onUpdateHighwaySequence={onUpdateHighwaySequence}
+          onUpdateTripOrigin={onUpdateTripOrigin}
+          onUpdateTripDestination={onUpdateTripDestination}
           onUpdateTotalDistance={onUpdateTotalDistance}
         />
 
