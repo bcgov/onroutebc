2d74ecbd38c432cbab1a109231775d69bc6bc064
gchauhan-aot <garima.chauhan@aot-technologies.com>
2025-03-12 13:31:11 -0600

    updating review comments

--- scheduler/src/common/constants/garms.constant.ts
+++ scheduler/src/common/constants/garms.constant.ts
@@ -1,8 +1,7 @@
-export const SER_NO_FROM = '               ';
-export const SER_NO_TO = '               ';
-export const VOID_IND = ' ';
-export const GARMS_CASH_FILLER =
-  '                                                                  ';
+export const SER_NO_FROM = ' '.repeat(15);
+export const SER_NO_TO = ' '.repeat(15);
+export const VOID_IND = ' '.repeat(1);
+export const GARMS_CASH_FILLER =' '.repeat(66);;
 export const INV_UNITS = '00000';
 export const INV_QTY = '00000';
 export const AGENT_NUMBER = '14035';
