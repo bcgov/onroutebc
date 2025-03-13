66ac7b750ef2c03fa72ea526dcde217af90963b9
gchauhan-aot <garima.chauhan@aot-technologies.com>
2025-03-13 13:38:00 -0600

    removing query param from controller

--- scheduler/src/modules/garms/garms.controller.ts
+++ scheduler/src/modules/garms/garms.controller.ts
@@ -1,17 +1,11 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { GarmsService } from './garms.service';
-import { GarmsExtractType } from '../common/enum/garms-extract-type.enum';
-import { ApiQuery } from '@nestjs/swagger';
 
 @Controller('garms')
 export class GarmsController {
   constructor(private readonly garmsService: GarmsService) {}
-  @ApiQuery({
-    name: 'garmsExtractType',
-    enum: GarmsExtractType,
-  })
   @Get()
-  findAll(@Query('garmsExtractType') garmsExtractType: GarmsExtractType) {
-    return this.garmsService.processCashTransactions(garmsExtractType);
+  findAll() {
+    return this.garmsService.processCashTransactions();
   }
 }
