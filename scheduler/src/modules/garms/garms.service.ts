d06fdb8999056c41346a082367fe19a6c778fa39
gchauhan-aot <garima.chauhan@aot-technologies.com>
2025-03-13 14:25:32 -0600

    reducing debug level

--- scheduler/src/modules/garms/garms.service.ts
+++ scheduler/src/modules/garms/garms.service.ts
@@ -285,7 +285,7 @@ export class GarmsService {
       password: process.env.GARMS_PWD,
       // additinal settings for lftp command.
       additionalLftpCommands:
-        'set cache:enable no;set ftp:passive-mode on;set ftp:use-size no;set ftp:ssl-protect-data yes;set ftp:ssl-force yes;set ftps:initial-prot "P";set net:connection-limit 1;set net:max-retries 1;debug 5;', // Additional commands to pass to lftp, splitted by ';'
+        'set cache:enable no;set ftp:passive-mode on;set ftp:use-size no;set ftp:ssl-protect-data yes;set ftp:ssl-force yes;set ftps:initial-prot "P";set net:connection-limit 1;set net:max-retries 1;debug 3;', // Additional commands to pass to lftp, splitted by ';'
     };
     const ftps: FTPS = new FTPS(options);
     try {
@@ -295,7 +295,6 @@ export class GarmsService {
       ftps.raw(
         `SITE LRecl=${recordLength} ; put -a ${localFilePath} -o "'${remoteFilePath}'"`,
       );
-      ftps.pwd().exec(console.log);
     } catch (e) {
       throw new InternalServerErrorException(e);
     } finally {
