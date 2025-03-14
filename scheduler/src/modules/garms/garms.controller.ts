7a27e5e5978f5ad3aa12dc728375586e51101e57
gchauhan-aot <garima.chauhan@aot-technologies.com>
2025-03-14 16:40:34 -0600

    remove delete method

--- scheduler/src/modules/garms/garms.controller.ts
+++ scheduler/src/modules/garms/garms.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { GarmsService } from './garms.service';
 
 @Controller('garms')
@@ -7,9 +7,5 @@ export class GarmsController {
   @Get()
   async findAll() {
     await this.garmsService.processCashTransactions();
-  }
-  @Delete()
-  DeleteOldFile() {
-    this.garmsService.cleanOldFiles();
   }
 }
