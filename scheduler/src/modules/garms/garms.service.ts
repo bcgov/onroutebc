378f86cf63f0672769996b72688b074319e118a0
gchauhan-aot <garima.chauhan@aot-technologies.com>
2025-03-18 12:16:42 -0600

    async file upload

--- scheduler/src/modules/garms/garms.service.ts
+++ scheduler/src/modules/garms/garms.service.ts
@@ -64,7 +64,7 @@ export class GarmsService {
 
         const remoteFilePath = process.env.GARMS_ENV + GARMS_CASH_FILE_LOCATION;
         const recordLength = GARMS_CASH_FILE_LRECL;
-        this.upload(fileName, recordLength, remoteFilePath);
+        await this.upload(fileName, recordLength, remoteFilePath);
         await this.saveTransactionIds(transactions, fileId);
       } else {
         this.logger.log('No data to process for GARMS cash file');
@@ -282,7 +282,7 @@ export class GarmsService {
    * @param recordLength
    * @param remoteFilePath
    */
-  upload(fileName: string, recordLength: number, remoteFilePath: string) {
+  async upload(fileName: string, recordLength: number, remoteFilePath: string) {
     const username = process.env.GARMS_USER;
     const password = process.env.GARMS_PWD;
     if (username && password) {
@@ -297,11 +297,16 @@ export class GarmsService {
       };
       const ftps: FTPS = new FTPS(options);
       try {
-        this.logger.log('sending file to garms', localFilePath);
-        ftps.raw(`SITE LRecl=${recordLength}`).exec(console.log);
-        // site command is to set record length to 140 for remote server. put -a is for ascii mode, -e to delete source file after successful transfer -o for remote file name.
-        const ftpCommand = `SITE LRecl=${recordLength}; put -aE ${localFilePath}  -o "'${remoteFilePath}'"`;
-        ftps.raw(ftpCommand).exec(console.log);
+        const uploadPromise = new Promise(() => {
+          this.logger.log('sending file to garms', localFilePath);
+          ftps.raw(`SITE LRecl=${recordLength}`).exec(console.log);
+          // site command is to set record length to 140 for remote server. put -a is for ascii mode, -e to delete source file after successful transfer -o for remote file name.
+          const ftpCommand = `SITE LRecl=${recordLength}; put -aE ${localFilePath}  -o "'${remoteFilePath}'"`;
+          ftps.raw(ftpCommand).exec(console.log);
+        });
+        this.logger.log('await upload promise');
+        // Wait for the upload to complete before proceeding
+        await uploadPromise;
       } catch (e) {
         this.logger.error('Error during FTP upload or file operation', e);
         throw new InternalServerErrorException(e);
