9281285d03caea7574f8082f960ad313b247351f
Abhishek Dere <abhishek.dere@gov.bc.ca>
2025-04-02 11:42:30 -0700

    validation check

--- frontend/src/features/manageProfile/components/forms/companyInfo/subForms/CompanyInfoGeneralForm.tsx
+++ frontend/src/features/manageProfile/components/forms/companyInfo/subForms/CompanyInfoGeneralForm.tsx
@@ -1,3 +1,4 @@
+import { useFormContext, useWatch } from "react-hook-form";
 import isPostalCode from "validator/lib/isPostalCode";
 
 import "./CompanyInfoGeneralForm.scss";
@@ -16,88 +17,104 @@ export const CompanyInfoGeneralForm = ({
   feature,
 }: {
   feature: ORBCFormFeatureType;
-}) => (
-  <div className="company-info-general-form">
-    <CustomFormComponent
-      type="input"
-      feature={feature}
-      options={{
-        name: "mailingAddress.addressLine1",
-        rules: {
-          required: { value: true, message: requiredMessage() },
-          validate: {
-            validateAddress1: (address1: string) =>
-              (address1.length >= 1 && address1.length <= 150) ||
-              invalidAddressLength(1, 150),
-          },
-        },
-        label: "Address (Line 1)",
-      }}
-      className="company-info-general-form__input"
-    />
+}) => {
+  const { control } = useFormContext();
+  const selectedCountry = useWatch({
+    control,
+    name: "mailingAddress.countryCode",
+  });
 
-    <CustomFormComponent
-      type="input"
-      feature={feature}
-      options={{
-        name: "mailingAddress.addressLine2",
-        rules: {
-          required: false,
-          validate: {
-            validateAddress2: (address2?: string) =>
-              isValidOptionalString(address2, { maxLength: 100 }) ||
-              invalidAddressLength(1, 100),
-          },
-        },
-        label: "Address (Line 2)",
-      }}
-      className="company-info-general-form__input"
-    />
-
-    <CountryAndProvince
-      feature={feature}
-      countryField="mailingAddress.countryCode"
-      provinceField="mailingAddress.provinceCode"
-      countryClassName="company-info-general-form__input"
-      provinceClassName="company-info-general-form__input"
-    />
-
-    <div className="side-by-side-inputs">
+  return (
+    <div className="company-info-general-form">
       <CustomFormComponent
         type="input"
         feature={feature}
         options={{
-          name: "mailingAddress.city",
+          name: "mailingAddress.addressLine1",
           rules: {
             required: { value: true, message: requiredMessage() },
             validate: {
-              validateCity: (city: string) =>
-                (city.length >= 1 && city.length <= 100) ||
-                invalidCityLength(1, 100),
+              validateAddress1: (address1: string) =>
+                (address1.length >= 1 && address1.length <= 150) ||
+                invalidAddressLength(1, 150),
             },
           },
-          label: "City",
+          label: "Address (Line 1)",
         }}
-        className="company-info-general-form__input company-info-general-form__input--left"
+        className="company-info-general-form__input"
       />
+
       <CustomFormComponent
         type="input"
         feature={feature}
         options={{
-          name: "mailingAddress.postalCode",
+          name: "mailingAddress.addressLine2",
           rules: {
-            required: { value: true, message: requiredMessage() },
+            required: false,
             validate: {
-              validatePostalCode: (postalCode: string) =>
-                isPostalCode(postalCode, "CA") || isPostalCode(postalCode, "US")
-                  ? true
-                  : invalidPostalCode(),
+              validateAddress2: (address2?: string) =>
+                isValidOptionalString(address2, { maxLength: 100 }) ||
+                invalidAddressLength(1, 100),
             },
           },
-          label: "Postal / Zip Code",
+          label: "Address (Line 2)",
         }}
-        className="company-info-general-form__input company-info-general-form__input--right"
+        className="company-info-general-form__input"
       />
+
+      <CountryAndProvince
+        feature={feature}
+        countryField="mailingAddress.countryCode"
+        provinceField="mailingAddress.provinceCode"
+        countryClassName="company-info-general-form__input"
+        provinceClassName="company-info-general-form__input"
+      />
+
+      <div className="side-by-side-inputs">
+        <CustomFormComponent
+          type="input"
+          feature={feature}
+          options={{
+            name: "mailingAddress.city",
+            rules: {
+              required: { value: true, message: requiredMessage() },
+              validate: {
+                validateCity: (city: string) =>
+                  (city.length >= 1 && city.length <= 100) ||
+                  invalidCityLength(1, 100),
+              },
+            },
+            label: "City",
+          }}
+          className="company-info-general-form__input company-info-general-form__input--left"
+        />
+        <CustomFormComponent
+          type="input"
+          feature={feature}
+          options={{
+            name: "mailingAddress.postalCode",
+            rules: {
+              required: { value: true, message: requiredMessage() },
+              validate: {
+                validatePostalCode: (postalCode: string) => {
+                  if (!selectedCountry) return true;
+                  // Standard validation for CA, US, MX
+                  if (["CA", "US", "MX"].includes(selectedCountry)) {
+                    return (
+                      isPostalCode(postalCode, selectedCountry) ||
+                      invalidPostalCode()
+                    );
+                  }
+                  // For other countries: Only check max length (30 chars)
+                  return postalCode.length < 30 || invalidPostalCode();
+                },
+              },
+            },
+            label: "Postal / Zip Code",
+          }}
+          className="company-info-general-form__input company-info-general-form__input--right"
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
