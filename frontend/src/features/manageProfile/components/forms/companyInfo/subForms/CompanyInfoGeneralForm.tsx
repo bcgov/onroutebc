2a22616262dd182cf0b1a6fd57ae7b50f683d5a0
Abhishek Dere <abhishek.dere@gov.bc.ca>
2025-04-03 08:58:29 -0700

    removed validation by country because of the possibility of breaking.

--- frontend/src/features/manageProfile/components/forms/companyInfo/subForms/CompanyInfoGeneralForm.tsx
+++ frontend/src/features/manageProfile/components/forms/companyInfo/subForms/CompanyInfoGeneralForm.tsx
@@ -1,3 +1,5 @@
+import isPostalCode from "validator/lib/isPostalCode";
+
 import "./CompanyInfoGeneralForm.scss";
 import { CountryAndProvince } from "../../../../../../common/components/form/CountryAndProvince";
 import { CustomFormComponent } from "../../../../../../common/components/form/CustomFormComponents";
@@ -14,88 +16,89 @@ export const CompanyInfoGeneralForm = ({
   feature,
 }: {
   feature: ORBCFormFeatureType;
-}) => {
-  return (
-    <div className="company-info-general-form">
+}) => (
+  <div className="company-info-general-form">
+    <CustomFormComponent
+      type="input"
+      feature={feature}
+      options={{
+        name: "mailingAddress.addressLine1",
+        rules: {
+          required: { value: true, message: requiredMessage() },
+          validate: {
+            validateAddress1: (address1: string) =>
+              (address1.length >= 1 && address1.length <= 150) ||
+              invalidAddressLength(1, 150),
+          },
+        },
+        label: "Address (Line 1)",
+      }}
+      className="company-info-general-form__input"
+    />
+
+    <CustomFormComponent
+      type="input"
+      feature={feature}
+      options={{
+        name: "mailingAddress.addressLine2",
+        rules: {
+          required: false,
+          validate: {
+            validateAddress2: (address2?: string) =>
+              isValidOptionalString(address2, { maxLength: 100 }) ||
+              invalidAddressLength(1, 100),
+          },
+        },
+        label: "Address (Line 2)",
+      }}
+      className="company-info-general-form__input"
+    />
+
+    <CountryAndProvince
+      feature={feature}
+      countryField="mailingAddress.countryCode"
+      provinceField="mailingAddress.provinceCode"
+      countryClassName="company-info-general-form__input"
+      provinceClassName="company-info-general-form__input"
+    />
+
+    <div className="side-by-side-inputs">
       <CustomFormComponent
         type="input"
         feature={feature}
         options={{
-          name: "mailingAddress.addressLine1",
+          name: "mailingAddress.city",
           rules: {
             required: { value: true, message: requiredMessage() },
             validate: {
-              validateAddress1: (address1: string) =>
-                (address1.length >= 1 && address1.length <= 150) ||
-                invalidAddressLength(1, 150),
+              validateCity: (city: string) =>
+                (city.length >= 1 && city.length <= 100) ||
+                invalidCityLength(1, 100),
             },
           },
-          label: "Address (Line 1)",
+          label: "City",
         }}
-        className="company-info-general-form__input"
+        className="company-info-general-form__input company-info-general-form__input--left"
       />
-
       <CustomFormComponent
         type="input"
         feature={feature}
         options={{
-          name: "mailingAddress.addressLine2",
+          name: "mailingAddress.postalCode",
           rules: {
-            required: false,
+            required: { value: true, message: requiredMessage() },
             validate: {
-              validateAddress2: (address2?: string) =>
-                isValidOptionalString(address2, { maxLength: 100 }) ||
-                invalidAddressLength(1, 100),
+              validatePostalCode: (postalCode: string) =>
+                (postalCode.length >= 5 &&
+                  postalCode.length <= 30 &&
+                  isPostalCode(postalCode, "any")) ||
+                invalidPostalCode(),
             },
           },
-          label: "Address (Line 2)",
+          label: "Postal / Zip Code",
         }}
-        className="company-info-general-form__input"
+        className="company-info-general-form__input company-info-general-form__input--right"
       />
-
-      <CountryAndProvince
-        feature={feature}
-        countryField="mailingAddress.countryCode"
-        provinceField="mailingAddress.provinceCode"
-        countryClassName="company-info-general-form__input"
-        provinceClassName="company-info-general-form__input"
-      />
-
-      <div className="side-by-side-inputs">
-        <CustomFormComponent
-          type="input"
-          feature={feature}
-          options={{
-            name: "mailingAddress.city",
-            rules: {
-              required: { value: true, message: requiredMessage() },
-              validate: {
-                validateCity: (city: string) =>
-                  (city.length >= 1 && city.length <= 100) ||
-                  invalidCityLength(1, 100),
-              },
-            },
-            label: "City",
-          }}
-          className="company-info-general-form__input company-info-general-form__input--left"
-        />
-        <CustomFormComponent
-          type="input"
-          feature={feature}
-          options={{
-            name: "mailingAddress.postalCode",
-            rules: {
-              required: { value: true, message: requiredMessage() },
-              validate: {
-                validatePostalCode: (postalCode: string) =>
-                  postalCode.length <= 30 || invalidPostalCode(),
-              },
-            },
-            label: "Postal / Zip Code",
-          }}
-          className="company-info-general-form__input company-info-general-form__input--right"
-        />
-      </div>
     </div>
-  );
-};
+  </div>
+);
