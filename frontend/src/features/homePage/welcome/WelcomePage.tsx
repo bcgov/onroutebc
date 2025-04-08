17c56c62623caf8f91d7ee342c2cb580c5ac16c8
praju-aot <praveen.raju@aot-technologies.com>
2025-04-08 10:23:35 -0400

    Display Client Name on Welcome Page
    Display Client name on welcome page for unclaimed company. When a Basic BCeID user logs in, display the given_name from token if company name does not exists

--- frontend/src/features/homePage/welcome/WelcomePage.tsx
+++ frontend/src/features/homePage/welcome/WelcomePage.tsx
@@ -9,20 +9,27 @@ import Card from "@mui/material/Card";
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { getCompanyNameFromSession } from "../../../common/apiManager/httpRequestHandler";
+import {
+  getCompanyNameFromSession,
+  getLoginUserGivenNameFromSession,
+} from "../../../common/apiManager/httpRequestHandler";
 import OnRouteBCContext from "../../../common/authentication/OnRouteBCContext";
 import { GreenCheckIcon } from "../../../common/components/icons/GreenCheckIcon";
 import { RedXMarkIcon } from "../../../common/components/icons/RedXMarkIcon";
-import { Nullable } from "../../../common/types/common";
+import { Nullable, Optional } from "../../../common/types/common";
 import {
   CREATE_PROFILE_WIZARD_ROUTES,
   PROFILE_ROUTES,
 } from "../../../routes/constants";
 import { BC_COLOURS } from "../../../themes/bcGovStyles";
 import "./welcome.scss";
+import { VerifiedClient } from "../../../common/authentication/types";
 
 const isInvitedUser = (companyNameFromContext?: string): boolean =>
   Boolean(companyNameFromContext);
+
+const isUnclaimedClient = (unclaimed?: Optional<VerifiedClient>): boolean =>
+  Boolean(unclaimed);
 
 const isNewCompanyProfile = (companyNameFromContext?: string): boolean =>
   !isInvitedUser(companyNameFromContext);
@@ -37,7 +44,7 @@ const WelcomeCompanyName = ({
     <>
       <div className="separator-line"></div>
       <div className="welcome-page__company-info">
-        <div className="company-label">Company Name</div>
+        <div className="company-label">Client Name</div>
         <div className="company-name">{companyName}</div>
       </div>
     </>
@@ -129,6 +136,7 @@ const ChallengeOption = ({
  */
 export const WelcomePage = React.memo(() => {
   const companyNameFromToken = getCompanyNameFromSession();
+  const givenNameFromToken = getLoginUserGivenNameFromSession();
   const { companyLegalName: companyNameFromContext, unclaimedClient } =
     useContext(OnRouteBCContext);
   return (
@@ -138,11 +146,19 @@ export const WelcomePage = React.memo(() => {
           <div className="welcome-graphic"></div>
           <h2>Welcome to onRouteBC!</h2>
         </div>
-        {isInvitedUser(companyNameFromContext) ? (
-          <WelcomeCompanyName companyName={companyNameFromContext} />
-        ) : (
-          <WelcomeCompanyName companyName={companyNameFromToken} />
-        )}
+        {(() => {
+          if (isUnclaimedClient(unclaimedClient)) {
+            return (
+              <WelcomeCompanyName companyName={unclaimedClient?.legalName} />
+            );
+          } else if (isInvitedUser(companyNameFromContext)) {
+            return <WelcomeCompanyName companyName={companyNameFromContext} />;
+          } else if (companyNameFromToken) {
+            return <WelcomeCompanyName companyName={companyNameFromToken} />;
+          } else {
+            return <WelcomeCompanyName companyName={givenNameFromToken} />;
+          }
+        })()}
         <div className="separator-line"></div>
         {/**
          * If the user is an invited user to a company that exists
@@ -165,11 +181,11 @@ export const WelcomePage = React.memo(() => {
          */}
         {/**
          * No challenge Workflow
-         * 
+         *
          * If there is a matching unclaimedClient for the user, we
          * redirect them to the company profile info wizard
          * and prepopulate the form with the unclaimedClient data.
-         * 
+         *
          * This covers the scenario where the user is a new BCeID user and
          * - Unclaimed company was created by staff and user was invited.
          * - Unclaimed company was migrated from TPS WEB which had users.
