d6167e0ff333792c8d2285544f473256ca9f029e
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2025-04-16 18:32:41 -0700

    Fix test case for testing quarterly durations

--- frontend/src/features/permits/helpers/tests/permitState.test.ts
+++ frontend/src/features/permits/helpers/tests/permitState.test.ts
@@ -46,13 +46,25 @@ describe("Permit States", () => {
 
   it("yields correct expiry date for quarterly permit with given start date", () => {
     // Arrange
-    const startDate = dayjs("2023-03-01");
+    const startDateQ1 = dayjs("2023-03-01");
+    const startDateQ2 = dayjs("2023-04-01");
+    const startDateQ3 = dayjs("2023-07-01");
+    const startDateQ4 = dayjs("2023-10-01");
 
     // Act
-    const expiryDate = getExpiryDate(startDate, true, 0);
-    const expiryDateStr = dayjsToLocalStr(expiryDate, DATE_FORMATS.DATEONLY);
+    const expiryDateQ1 = getExpiryDate(startDateQ1, true, 0);
+    const expiryDateStrQ1 = dayjsToLocalStr(expiryDateQ1, DATE_FORMATS.DATEONLY);
+    const expiryDateQ2 = getExpiryDate(startDateQ2, true, 0);
+    const expiryDateStrQ2 = dayjsToLocalStr(expiryDateQ2, DATE_FORMATS.DATEONLY);
+    const expiryDateQ3 = getExpiryDate(startDateQ3, true, 0);
+    const expiryDateStrQ3 = dayjsToLocalStr(expiryDateQ3, DATE_FORMATS.DATEONLY);
+    const expiryDateQ4 = getExpiryDate(startDateQ4, true, 0);
+    const expiryDateStrQ4 = dayjsToLocalStr(expiryDateQ4, DATE_FORMATS.DATEONLY);
 
     // Assert
-    expect(expiryDateStr).toBe("2023-04-30");
+    expect(expiryDateStrQ1).toBe("2023-03-31");
+    expect(expiryDateStrQ2).toBe("2023-06-30");
+    expect(expiryDateStrQ3).toBe("2023-09-30");
+    expect(expiryDateStrQ4).toBe("2023-12-31");
   });
 });
