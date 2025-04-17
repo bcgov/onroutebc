6f509e2e5aa2e0ce8479f82fb31bc5d39a5a1871
praju-aot <praveen.raju@aot-technologies.com>
2025-04-17 12:22:35 -0400

    Update swagger to throw 404 if there are no outage notifications configured

--- public/src/modules/outage-notification/outage-notification.controller.ts
+++ public/src/modules/outage-notification/outage-notification.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { OutageNotificationService } from './outage-notification.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiNotFoundResponse } from '@nestjs/swagger';
 import { ReadOutageNotificationDto } from './dto/read-outage-notification.dto';
+import { ExceptionDto } from '../../common/exception/exception.dto';
 
 @Controller('outage-notification')
+@ApiNotFoundResponse({
+  description: 'The Outage Notification Api Not Found Response',
+  type: ExceptionDto,
+})
 export class OutageNotificationController {
   constructor(
     private readonly outageNotificationService: OutageNotificationService,
