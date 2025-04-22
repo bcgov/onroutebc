7e2ffb33ef4e5f0ac0fb3f199905f8cea7385592
Abhishek Dere <abhishek.dere@gov.bc.ca>
2025-04-22 14:26:57 -0700

    added constants, and fixed a bug for repetitive queries.

--- frontend/src/features/outage/hooks/useOutageNotification.ts
+++ frontend/src/features/outage/hooks/useOutageNotification.ts
@@ -1,12 +1,14 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { getOutageNotification } from "../apiManager/outageNotification";
+import { THIRTY_MINUTES } from "../../../common/constants/constants";
 
 export const useOutageNotification = () => {
   return useQuery({
     queryKey: ["outage-notification"],
     queryFn: getOutageNotification,
-    staleTime: 30 * 60 * 1000, // 30 minutes
-    refetchOnWindowFocus: false,   
+    staleTime: THIRTY_MINUTES,
+    refetchOnWindowFocus: false,
+    retry: false,   
   });
 };
