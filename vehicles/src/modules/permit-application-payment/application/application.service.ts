fc34418b6f2c2d6b6a3268bab301a257419e7d4c
praju-aot <praveen.raju@aot-technologies.com>
2025-04-30 13:55:05 -0400

    Remove amend applications from results

--- vehicles/src/modules/permit-application-payment/application/application.service.ts
+++ vehicles/src/modules/permit-application-payment/application/application.service.ts
@@ -472,6 +472,10 @@ export class ApplicationService {
     // Filter permits without permit numbers (i.e., applications)
     permitsQuery = permitsQuery.where('permit.permitNumber IS NULL');
 
+    permitsQuery = permitsQuery.where('permit.revision = :revision', {
+      revision: 0,
+    });
+
     // Filter by companyId if provided
     if (companyId) {
       permitsQuery = permitsQuery.andWhere('company.companyId = :companyId', {
