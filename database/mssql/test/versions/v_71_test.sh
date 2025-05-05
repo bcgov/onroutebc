6093481c928679429e56362b1014171b0c57d7b3
gchauhan-aot <garima.chauhan@aot-technologies.com>
2025-05-05 15:23:11 -0600

    updating message

--- database/mssql/test/versions/v_71_test.sh
+++ database/mssql/test/versions/v_71_test.sh
@@ -11,8 +11,8 @@ parse_options "${USAGE}" ${@}
 TEST_71_1_RESULT=$(/opt/mssql-tools/bin/sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -v DB_NAME=${DATABASE} -h -1 -i ${TESTS_DIR}/v_71_1_test.sql | xargs)
 # Returns 24 for an nvarchar(12) data type due to larger nvarchar storage
 if [[ $TEST_71_1_RESULT -eq 15 ]]; then
-    echo "Test 71.1 passed: CREDIT_ACCOUNT_NUMBER column length updated correctly"
+    echo "Test 71.1 passed: POSTAL_CODE column length updated correctly"
 else
-    echo "******** Test 71.1 failed: CREDIT_ACCOUNT_NUMBER column length not updated correctly: "
+    echo "******** Test 71.1 failed: POSTAL_CODE column length not updated correctly: "
     echo $TEST_71_1_RESULT
 fi
