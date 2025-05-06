25c6b961eec066f47425dc66f471733f3c840e74
gchauhan-aot <garima.chauhan@aot-technologies.com>
2025-05-05 22:04:34 -0600

    Postal code update for ORBC_ADDRESS_HIST table

--- database/mssql/test/versions/v_71_test.sh
+++ database/mssql/test/versions/v_71_test.sh
@@ -9,10 +9,17 @@ parse_options "${USAGE}" ${@}
 # TESTS_DIR variable set by the calling test-runner script.
 
 TEST_71_1_RESULT=$(/opt/mssql-tools/bin/sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -v DB_NAME=${DATABASE} -h -1 -i ${TESTS_DIR}/v_71_1_test.sql | xargs)
-# Returns 24 for an nvarchar(12) data type due to larger nvarchar storage
 if [[ $TEST_71_1_RESULT -eq 15 ]]; then
     echo "Test 71.1 passed: POSTAL_CODE column length updated correctly"
 else
     echo "******** Test 71.1 failed: POSTAL_CODE column length not updated correctly: "
     echo $TEST_71_1_RESULT
+fi
+
+TEST_71_2_RESULT=$(/opt/mssql-tools/bin/sqlcmd -U ${USER} -P "${PASS}" -S ${SERVER} -v DB_NAME=${DATABASE} -h -1 -i ${TESTS_DIR}/v_71_2_test.sql | xargs)
+if [[ $TEST_71_2_RESULT -eq 15 ]]; then
+    echo "Test 71.2 passed: POSTAL_CODE column length updated correctly"
+else
+    echo "******** Test 71.2 failed: POSTAL_CODE column length not updated correctly: "
+    echo $TEST_71_2_RESULT
 fi
