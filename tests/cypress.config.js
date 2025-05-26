031d58da8709600ac74de1b60928eb855cd01799
Chris Berg <chris.berg@aot-technologies.com>
2025-05-26 14:27:19 -0700

    Revert "wip: experimenting with github timeouts, removing tests"
    This reverts commit f46ca8a077dbdbfab169d165a68bf08d7240d08f.

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   video: true,
   e2e: {
-    experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
