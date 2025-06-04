c4f76c2801e237b1f15c1627c0e515981f83b71f
GlenAOT <160973940+GlenAOT@users.noreply.github.com>
2025-06-04 12:07:57 -0700

    remove eslint-disable comment

--- frontend/src/features/settings/pages/SpecialAuthorizations/SpecialAuthorizations.tsx
+++ frontend/src/features/settings/pages/SpecialAuthorizations/SpecialAuthorizations.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useContext, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
