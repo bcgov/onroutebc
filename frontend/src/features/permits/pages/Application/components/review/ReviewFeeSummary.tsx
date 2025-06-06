77504ed26f7b74a820a6cc84144696ebab56e81b
GlenAOT <160973940+GlenAOT@users.noreply.github.com>
2025-06-06 15:28:20 -0700

    Conditionally render AmendOrVoidFeeSummary component inside ReviewFeeSummary

--- frontend/src/features/permits/pages/Application/components/review/ReviewFeeSummary.tsx
+++ frontend/src/features/permits/pages/Application/components/review/ReviewFeeSummary.tsx
@@ -3,12 +3,26 @@ import { Box, Typography } from "@mui/material";
 import "./ReviewFeeSummary.scss";
 import { ConfirmationCheckboxes } from "./ConfirmationCheckboxes";
 import { FeeSummary } from "../../../../components/feeSummary/FeeSummary";
-import { PermitType } from "../../../../types/PermitType";
+import { DEFAULT_PERMIT_TYPE, PermitType } from "../../../../types/PermitType";
 import { Nullable } from "../../../../../../common/types/common";
 import {
   PERMIT_REVIEW_CONTEXTS,
   PermitReviewContext,
 } from "../../../../types/PermitReviewContext";
+import { useContext } from "react";
+import { AmendPermitContext } from "../../../Amend/context/AmendPermitContext";
+import { isValidTransaction } from "../../../../helpers/payment";
+import { calculateNetAmount } from "../../../../helpers/feeSummary";
+import { useParams } from "react-router-dom";
+import {
+  applyWhenNotNullable,
+  getDefaultRequiredVal,
+} from "../../../../../../common/helpers/util";
+import { useFetchSpecialAuthorizations } from "../../../../../settings/hooks/specialAuthorizations";
+import { usePolicyEngine } from "../../../../../policy/hooks/usePolicyEngine";
+import { useCalculateRefundAmount } from "../../../../hooks/useCalculateRefundAmount";
+import { serializePermitData } from "../../../../helpers/serialize/serializePermitData";
+import { AmendOrVoidFeeSummary } from "../../../../components/amendOrVoidFeeSummary/AmendOrVoidFeeSummary";
 
 export const ReviewFeeSummary = ({
   hasAttemptedSubmission,
@@ -25,6 +39,49 @@ export const ReviewFeeSummary = ({
   fee: string;
   reviewContext: PermitReviewContext;
 }) => {
+  const isAmendAction = reviewContext === PERMIT_REVIEW_CONTEXTS.AMEND;
+
+  const { amendmentApplication, permitHistory } =
+    useContext(AmendPermitContext);
+
+  const validTransactionHistory = permitHistory.filter((history) =>
+    isValidTransaction(history.paymentMethodTypeCode, history.pgApproved),
+  );
+
+  const currentPermitValue = calculateNetAmount(validTransactionHistory);
+
+  const { companyId: companyIdParam } = useParams();
+  const companyId: number = applyWhenNotNullable(
+    (id) => Number(id),
+    companyIdParam,
+    0,
+  );
+
+  permitType = getDefaultRequiredVal(
+    DEFAULT_PERMIT_TYPE,
+    amendmentApplication?.permitType,
+    permitType,
+  );
+
+  const { data: specialAuthorizations } =
+    useFetchSpecialAuthorizations(companyId);
+  const policyEngine = usePolicyEngine(specialAuthorizations);
+  const calculatedRefundAmount = useCalculateRefundAmount(
+    validTransactionHistory,
+    {
+      permitType,
+      permitData: amendmentApplication?.permitData
+        ? serializePermitData(amendmentApplication.permitData)
+        : {},
+    },
+    policyEngine,
+  );
+
+  const amountToRefund = -1 * calculatedRefundAmount;
+
+  // amountToRefund is a negative number so we add here rather than subtract
+  const newPermitValue = currentPermitValue + amountToRefund;
+
   return (
     <Box className="review-fee-summary">
       <Box className="review-fee-summary__header">
@@ -32,7 +89,15 @@ export const ReviewFeeSummary = ({
       </Box>
       <Box className="review-fee-summary__body">
         <Box className="fee-summary-wrapper">
-          <FeeSummary permitType={permitType} feeSummary={fee} />
+          {isAmendAction ? (
+            <AmendOrVoidFeeSummary
+              currentPermitValue={`${currentPermitValue}`}
+              newPermitValue={`${newPermitValue}`}
+              amountToRefund={`${amountToRefund}`}
+            />
+          ) : (
+            <FeeSummary permitType={permitType} feeSummary={fee} />
+          )}
 
           <ConfirmationCheckboxes
             hasAttemptedSubmission={hasAttemptedSubmission}
