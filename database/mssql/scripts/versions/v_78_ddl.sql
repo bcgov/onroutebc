f157796d3f31daf74342003c43808675472d6e6b
praju-aot <praveen.raju@aot-technologies.com>
2025-07-08 15:41:17 -0400

    Add History table and trigger to ORBC_LOGIN

--- database/mssql/scripts/versions/v_78_ddl.sql
+++ database/mssql/scripts/versions/v_78_ddl.sql
@@ -11,7 +11,9 @@ GO
 BEGIN TRANSACTION
 GO
 
-
+/** ORBC_LOGIN captures login information for all users, including BCeID and IDIR.
+* The table does not have any foreign key constraints to ORBC_USER or ORBC_COMPANY
+* as it may represent a new user. */
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
 CREATE TABLE [dbo].[ORBC_LOGIN] (
@@ -63,12 +65,70 @@ EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'User''s busine
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'User''s username as set in bceid, bcsc or IDIR' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ORBC_LOGIN', @level2type=N'COLUMN',@level2name=N'USERNAME'
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'User''s email as set in bceid, bcsc or IDIR' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ORBC_LOGIN', @level2type=N'COLUMN',@level2name=N'EMAIL'
 
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Application code is responsible for retrieving the row and then incrementing the value of the CONCURRENCY_CONTROL_NUMBER column by one prior to issuing an update. If this is done then the update will succeed, provided that the row was not updated by any other transactions in the period between the read and the update operations.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ORBC_LOGIN', @level2type=N'COLUMN',@level2name=N'CONCURRENCY_CONTROL_NUMBER'
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The user or proxy account that created the record.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ORBC_LOGIN', @level2type=N'COLUMN',@level2name=N'DB_CREATE_USERID'
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The date and time the record was created.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ORBC_LOGIN', @level2type=N'COLUMN',@level2name=N'DB_CREATE_TIMESTAMP'
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The user or proxy account that created or last updated the record.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ORBC_LOGIN', @level2type=N'COLUMN',@level2name=N'DB_LAST_UPDATE_USERID'
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The date and time the record was created or last updated.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ORBC_LOGIN', @level2type=N'COLUMN',@level2name=N'DB_LAST_UPDATE_TIMESTAMP'
 GO
+
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+CREATE SEQUENCE [dbo].[ORBC_LOGIN_H_ID_SEQ] AS [bigint] START WITH 1 INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 50;
+
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+CREATE TABLE [dbo].[ORBC_LOGIN_HIST](
+  _LOGIN_HIST_ID [bigint] DEFAULT (NEXT VALUE FOR [dbo].[ORBC_LOGIN_H_ID_SEQ]) NOT NULL
+  ,EFFECTIVE_DATE_HIST [datetime] NOT NULL default getutcdate()
+  ,END_DATE_HIST [datetime]
+  , [LOGIN_ID] int NOT NULL, [USERNAME] nvarchar(50) NOT NULL, [USER_GUID] char(32) NOT NULL, [COMPANY_GUID] char(32) NULL, [EMAIL] nvarchar(100) NULL, [LOGIN_DATE_TIME] datetime2 NOT NULL, [APP_CREATE_TIMESTAMP] datetime2 NULL, [APP_CREATE_USERID] nvarchar(30) NULL, [APP_CREATE_USER_GUID] char(32) NULL, [APP_CREATE_USER_DIRECTORY] nvarchar(30) NULL, [APP_LAST_UPDATE_TIMESTAMP] datetime2 NULL, [APP_LAST_UPDATE_USERID] nvarchar(30) NULL, [APP_LAST_UPDATE_USER_GUID] char(32) NULL, [APP_LAST_UPDATE_USER_DIRECTORY] nvarchar(30) NULL, [CONCURRENCY_CONTROL_NUMBER] int NULL, [DB_CREATE_USERID] varchar(63) NOT NULL, [DB_CREATE_TIMESTAMP] datetime2 NOT NULL, [DB_LAST_UPDATE_USERID] varchar(63) NOT NULL, [DB_LAST_UPDATE_TIMESTAMP] datetime2 NOT NULL
+  )
+
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+-- Create primary key constraint ORBC_LOGIN_HIST_PK
+PRINT N'Create Primary constraint ORBC_LOGIN_HIST_PK'  
+ALTER TABLE [dbo].[ORBC_LOGIN_HIST] ADD CONSTRAINT ORBC_LOGIN_HIST_PK PRIMARY KEY CLUSTERED (_LOGIN_HIST_ID);  
+GO
+
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+-- Create unique key constraint ORBC_LOGIN_HIST_UK
+PRINT N'Create unique constraint ORBC_LOGIN_HIST_UK'  
+ALTER TABLE [dbo].[ORBC_LOGIN_HIST] ADD CONSTRAINT ORBC_LOGIN_HIST_UK UNIQUE (_LOGIN_HIST_ID,END_DATE_HIST)
+GO
+
+
+PRINT N'Create trigger dbo.ORBC_LOGIN_I_S_U_TR'
+
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+CREATE TRIGGER ORBC_LOGIN_I_S_U_TR ON [dbo].[ORBC_LOGIN] FOR INSERT, UPDATE, DELETE AS
+SET NOCOUNT ON
+BEGIN TRY
+DECLARE @curr_date datetime;
+SET @curr_date = getutcdate();
+  IF NOT EXISTS(SELECT * FROM inserted) AND NOT EXISTS(SELECT * FROM deleted) 
+    RETURN;
+
+  -- historical
+  IF EXISTS(SELECT * FROM deleted)
+    update [dbo].[ORBC_LOGIN_HIST] set END_DATE_HIST = @curr_date where LOGIN_ID in (select LOGIN_ID from deleted) and END_DATE_HIST is null;
+  
+  IF EXISTS(SELECT * FROM inserted)
+    insert into [dbo].[ORBC_LOGIN_HIST] ([LOGIN_ID], [USERNAME], [USER_GUID], [COMPANY_GUID], [EMAIL], [LOGIN_DATE_TIME], [APP_CREATE_TIMESTAMP], [APP_CREATE_USERID], [APP_CREATE_USER_GUID], [APP_CREATE_USER_DIRECTORY], [APP_LAST_UPDATE_TIMESTAMP], [APP_LAST_UPDATE_USERID], [APP_LAST_UPDATE_USER_GUID], [APP_LAST_UPDATE_USER_DIRECTORY], [CONCURRENCY_CONTROL_NUMBER], [DB_CREATE_USERID], [DB_CREATE_TIMESTAMP], [DB_LAST_UPDATE_USERID], [DB_LAST_UPDATE_TIMESTAMP], _LOGIN_HIST_ID, END_DATE_HIST, EFFECTIVE_DATE_HIST)
+      select [LOGIN_ID], [USERNAME], [USER_GUID], [COMPANY_GUID], [EMAIL], [LOGIN_DATE_TIME], [APP_CREATE_TIMESTAMP], [APP_CREATE_USERID], [APP_CREATE_USER_GUID], [APP_CREATE_USER_DIRECTORY], [APP_LAST_UPDATE_TIMESTAMP], [APP_LAST_UPDATE_USERID], [APP_LAST_UPDATE_USER_GUID], [APP_LAST_UPDATE_USER_DIRECTORY], [CONCURRENCY_CONTROL_NUMBER], [DB_CREATE_USERID], [DB_CREATE_TIMESTAMP], [DB_LAST_UPDATE_USERID], [DB_LAST_UPDATE_TIMESTAMP], (next value for [dbo].[ORBC_LOGIN_H_ID_SEQ]) as [_LOGIN_HIST_ID], null as [END_DATE_HIST], @curr_date as [EFFECTIVE_DATE_HIST] from inserted;
+
+END TRY
+BEGIN CATCH
+   IF @@trancount > 0 ROLLBACK TRANSACTION
+   EXEC orbc_error_handling
+END CATCH;
+go
 
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
