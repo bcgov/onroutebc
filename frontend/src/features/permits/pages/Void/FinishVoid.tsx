62a1d9dce0d59fbd77c7791c04c32d3b47e6007e
Praveen Raju <80779423+praju-aot@users.noreply.github.com>
2025-07-29 18:21:54 -0400

    Fix/code resolution (#2133)
    Co-authored-by: cberg-aot <93226309+cberg-aot@users.noreply.github.com>
    Co-authored-by: glen-aot <160973940+glen-aot@users.noreply.github.com>
    Co-authored-by: GlenAOT <160973940+GlenAOT@users.noreply.github.com>
    Co-authored-by: John Fletcher <113134542+john-fletcher-aot@users.noreply.github.com>
    Co-authored-by: Chris Berg <christopherpberg@gmail.com>

--- frontend/src/features/permits/pages/Void/FinishVoid.tsx
+++ frontend/src/features/permits/pages/Void/FinishVoid.tsx
@@ -94,7 +94,6 @@ export const FinishVoid = () => {
         applicationIds: [existingAmendmentApplicationId],
       });
     }
-
     voidPermitMutation.mutate({
       permitId: voidPermitData.permitId,
       voidData: mapToVoidRequestData(
