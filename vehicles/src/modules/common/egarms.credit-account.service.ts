67640d23429097b3894b530a36b5bddbfdb9dde1
praju-aot <praveen.raju@aot-technologies.com>
2025-08-15 16:07:57 -0400

    Implement circuit breaker

--- vehicles/src/modules/common/egarms.credit-account.service.ts
+++ vehicles/src/modules/common/egarms.credit-account.service.ts
@@ -1,6 +1,5 @@
 import { HttpService } from '@nestjs/axios';
 import {
-  BadRequestException,
   HttpStatus,
   Injectable,
   InternalServerErrorException,
@@ -12,16 +11,67 @@ import { EGARMS_CREDIT_API_SYSTEM_ID } from '../../common/constants/api.constant
 import { lastValueFrom } from 'rxjs';
 import { XMLParser } from 'fast-xml-parser';
 import { IEGARMSResponse } from '../../common/interface/egarms-response.interface';
-import { throwUnprocessableEntityException } from '../../common/helper/exception.helper';
+import * as CircuitBreaker from 'opossum';
 
 @Injectable()
 export class EGARMSCreditAccountService {
   private readonly logger = new Logger(EGARMSCreditAccountService.name);
+  private readonly circuitBreaker: CircuitBreaker;
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) {
+    const options = {
+      timeout: 10000,
+      errorThresholdPercentage: 50,
+      resetTimeout: 30000,      
+    };
+
+    // Initialize CircuitBreaker with a function that returns a Promise
+    this.circuitBreaker = new CircuitBreaker(
+      (params: { creditAccountNumber: string }) =>
+        this.makeEGARMSRequest(params.creditAccountNumber),
+      options,
+    );
+
+    this.setupEventListeners();
+  }
+
+  private setupEventListeners() {
+    this.circuitBreaker.on('timeout', () => {
+      this.logger.error('EGARMS Circuit Breaker - Request timed out');
+    });
+
+    this.circuitBreaker.on('open', () => {
+      this.logger.error('EGARMS Circuit Breaker - Open');
+    });
+
+    this.circuitBreaker.on('close', () => {
+      this.logger.error('EGARMS Circuit Breaker - close');
+    });
+
+    this.circuitBreaker.on('halfOpen', () => {
+      this.logger.error('EGARMS Circuit Breaker - halfOpen');
+    });
+
+    this.circuitBreaker.on('failure', (error) => {
+      this.logger.error('EGARMS circuit breaker error:', error);
+    });
+  }
 
   @LogAsyncMethodExecution()
   async getCreditAccountDetailsFromEGARMS(
+    creditAccountNumber: string,
+  ): Promise<IEGARMSResponse> {
+    try {
+      return (await this.circuitBreaker.fire({
+        creditAccountNumber,
+      })) as IEGARMSResponse;
+    } catch (error) {
+      this.logger.error('Credit Account is unavailable', error);
+      throw new InternalServerErrorException('Credit Account is unavailable.');
+    }
+  }
+
+  private async makeEGARMSRequest(
     creditAccountNumber: string,
   ): Promise<IEGARMSResponse> {
     try {
@@ -41,13 +91,6 @@ export class EGARMSCreditAccountService {
         this.httpService.get(url, reqConfig),
       );
 
-      if ((response.status as HttpStatus) === HttpStatus.UNAUTHORIZED) {
-        throwUnprocessableEntityException(
-          'eGARMS threw an unauthorized exception. Please verify your credentials.',
-          { errorCode: 'CREDIT_ACCOUNT_EGARMS_UNAUTHORIZED' },
-        );
-      }
-
       const parser = new XMLParser({ ignoreDeclaration: true });
       return parser.parse(response.data as string) as IEGARMSResponse;
     } catch (error) {
@@ -62,18 +105,14 @@ export class EGARMSCreditAccountService {
         'Error retrieving Credit Account details from EGARMS',
         error,
       );
-      throw new InternalServerErrorException(
-        'Error retrieving Credit Account details from EGARMS',
-      );
+      throw new InternalServerErrorException('Credit Account is unavailable.');
     }
 
     const axiosError = error as AxiosError;
 
     if (!axiosError?.response) {
       this.logger.error(axiosError?.message, axiosError?.stack);
-      throw new InternalServerErrorException(
-        'Error retrieving Credit Account details from EGARMS',
-      );
+      throw new InternalServerErrorException('Credit Account is unavailable.');
     }
 
     const status = axiosError?.response?.status as HttpStatus;
@@ -84,7 +123,7 @@ export class EGARMSCreditAccountService {
           'Unauthorized access to EGARMS. Please verify your credentials.',
         );
         throw new InternalServerErrorException(
-          'Unexpected error from EGARMS',
+          'Credit Account is unavailable.',
           axiosError?.response?.data,
         );
       case HttpStatus.BAD_REQUEST:
@@ -92,14 +131,17 @@ export class EGARMSCreditAccountService {
           'Invalid request to EGARMS:',
           axiosError?.response?.data,
         );
-        throw new BadRequestException(axiosError?.response?.data);
+        throw new InternalServerErrorException(
+          'Credit Account is unavailable.',
+          axiosError?.response?.data,
+        );
       default:
         this.logger.error(
           `EGARMS returned unexpected status ${status}:`,
           axiosError?.response?.data,
         );
         throw new InternalServerErrorException(
-          'Unexpected error from EGARMS',
+          'Credit Account is unavailable.',
           axiosError?.response?.data,
         );
     }
