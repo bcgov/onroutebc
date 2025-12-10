2d9f40713292e889d2b89c9e3312a1d9c77dd43b
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2025-12-10 11:35:45 -0800

    Revert list of highways link

--- frontend/src/features/permits/pages/Application/components/form/TripDetailsSection/HighwaySequences.tsx
+++ frontend/src/features/permits/pages/Application/components/form/TripDetailsSection/HighwaySequences.tsx
@@ -104,6 +104,17 @@ export const HighwaySequences = ({
               Please refer to the
               <CustomExternalLink
                 className="highways-link"
+                href={ONROUTE_WEBPAGE_LINKS.LIST_OF_BC_HIGHWAYS}
+                openInNewTab={true}
+                withLinkIcon={true}
+              >
+                <span className="highways-link__title">
+                  List of Highways in British Columbia
+                </span>
+              </CustomExternalLink>
+              and the
+              <CustomExternalLink
+                className="highways-link"
                 href={ONROUTE_WEBPAGE_LINKS.COMMERCIAL_VEHICLE_ROUTING_TOOL}
                 openInNewTab={true}
                 withLinkIcon={true}
