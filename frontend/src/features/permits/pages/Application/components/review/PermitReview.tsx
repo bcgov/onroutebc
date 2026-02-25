f1b0868b97a7a96973c14d045f82ec8e38108808
zgong-gov <123983557+zgong-gov@users.noreply.github.com>
2026-02-25 01:17:53 -0800

    ORV2-5234: Reverting return trip changes (#2225)

--- frontend/src/features/permits/pages/Application/components/review/PermitReview.tsx
+++ frontend/src/features/permits/pages/Application/components/review/PermitReview.tsx
@@ -201,7 +201,6 @@ export const PermitReview = (props: PermitReviewProps) => {
           routeDetails={props.route}
           oldRouteDetails={props.oldFields?.permitData?.permittedRoute}
           showChangedFields={props.showChangedFields}
-          permitType={props.permitType}
         />
 
         <ThirdPartyLiabilitySection
