73890ea3e0a460e7eeac4bb59adbedc01c4246d2
Abhishek Dere <abhishek.dere@gov.bc.ca>
2026-03-11 14:22:02 -0700

    Orv2 5173 dependency upgrades (#2227)

--- frontend/src/common/pages/Loading.tsx
+++ frontend/src/common/pages/Loading.tsx
@@ -1,35 +1,19 @@
-import { Box, Typography } from "@mui/material";
-import { useEffect } from "react";
-import lottie from "lottie-web";
-import loading_6 from "../../../public/loading_grey.json";
+import { Box, Typography, CircularProgress } from "@mui/material";
 
 export const Loading = () => {
-  let count = 0;
-  useEffect(() => {
-    const exsitingSvg = document.getElementsByTagName("svg");
-    if (exsitingSvg.length !== 0) {
-      const container = document.getElementById("lottie-container");
-
-      if (container && count === 0) {
-        lottie.loadAnimation({
-          container: container,
-          renderer: "svg", // or 'canvas', 'html'
-          loop: true,
-          autoplay: true,
-          animationData: loading_6,
-        });
-        count++;
-      }
-    }
-  }, []);
-
   return (
     <div style={{ paddingTop: "24px" }}>
-      <Box className="success feature-container">
-        <div
-          id="lottie-container"
-          style={{ width: "188px", height: "188px" }}
-        ></div>
+      <Box
+        className="success feature-container"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <CircularProgress size={120} thickness={4} sx={{ color: "#606060" }} />
+
         <Typography variant="h4" sx={{ color: "#313132" }}>
           Processing, please wait...
         </Typography>
