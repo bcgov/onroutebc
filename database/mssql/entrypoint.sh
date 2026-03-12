cac32c3bf03ba8e33739b40f964e65f49e12e697
Praveen Raju <praveen.raju@aot-technologies.com>
2026-03-12 19:35:55 -0400

    Run as root user

--- database/mssql/entrypoint.sh
+++ database/mssql/entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 # Taken from https://github.com/microsoft/mssql-docker/blob/master/linux/preview/examples/mssql-customize/entrypoint.sh
 
-# Trust SQL Server certificate globally and disable encryption for self-signed certificates
-# export SQLCMDTRUSTSERVERCERTIFICATE=yes
-# export SQLCMDENCRYPT=no
+# Ensure /var/opt/mssql has correct permissions for SQL Server to write to it
+# (directory permissions may be lost on Docker Desktop due to mounted volumes)
+mkdir -p /var/opt/mssql/.system
+mkdir -p /var/opt/mssql/backup
+mkdir -p /var/opt/mssql/data
+mkdir -p /var/opt/mssql/log
+chown -R mssql:mssql /var/opt/mssql
+chmod -R 755 /var/opt/mssql
 
-# Start the script to create and initialize the DB
+# Trust SQL Server certificate globally and disable encryption for self-signed certificates
+export SQLCMDTRUSTSERVERCERTIFICATE=yes
+export SQLCMDENCRYPT=no
+
+# Start the script to create and initialize the DB (in background)
 /usr/config/configure-db.sh &
 
-# Start SQL Server
-/opt/mssql/bin/sqlservr
+exec gosu mssql /opt/mssql/bin/sqlservr
