e61e86028c535bee0782e80c4bafe6c6285d166e
Praveen Raju <praveen.raju@aot-technologies.com>
2026-03-13 12:57:35 -0400

    Optimize the image

--- database/mssql/entrypoint.sh
+++ database/mssql/entrypoint.sh
@@ -3,12 +3,12 @@
 
 # Ensure /var/opt/mssql has correct permissions for SQL Server to write to it
 # (directory permissions may be lost on Docker Desktop due to mounted volumes)
-mkdir -p /var/opt/mssql/.system
-mkdir -p /var/opt/mssql/backup
-mkdir -p /var/opt/mssql/data
-mkdir -p /var/opt/mssql/log
-chown -R mssql:mssql /var/opt/mssql
-chmod -R 755 /var/opt/mssql
+# mkdir -p /var/opt/mssql/.system
+# mkdir -p /var/opt/mssql/backup
+# mkdir -p /var/opt/mssql/data
+# mkdir -p /var/opt/mssql/log
+# chown -R mssql:mssql /var/opt/mssql
+# chmod -R 755 /var/opt/mssql
 
 # Trust SQL Server certificate globally and disable encryption for self-signed certificates
 export SQLCMDTRUSTSERVERCERTIFICATE=yes
@@ -17,4 +17,5 @@ export SQLCMDENCRYPT=no
 # Start the script to create and initialize the DB (in background)
 /usr/config/configure-db.sh &
 
+# Drop to mssql user to run SQL Server (gosu is like sudo but lighter weight)
 exec gosu mssql /opt/mssql/bin/sqlservr
