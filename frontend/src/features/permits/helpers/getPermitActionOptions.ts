a88e6ff4d270e8df73d0caf8e4f2dbf3055707a3
acoard-aot <61285798+acoard-aot@users.noreply.github.com>
2026-07-15 11:44:49 -0700

    feat: Add Amend and Void/Revoke to Expired Permits and Search
    Had to modify PermitRowOptions, which is the component which does most of heavy lifting, passing in permitStatus and permitApprovalSource. (We use source to detect if it's a TPS migration). We put a lot of our logic in getPermitRowActionOptions.
    
    I renamed some variables, like isPermitInactive to be more accurate of what they actually meausre now, also Expired etc.
    
    I also added UI route guards here, to make sure even if someone navigates to page directly it won't work to bypass our UI.
    
    Most of my 'shared' work here is in getPermitActionOptions

--- frontend/src/features/permits/helpers/getPermitActionOptions.ts
+++ frontend/src/features/permits/helpers/getPermitActionOptions.ts
@@ -1,22 +1,86 @@
-import { PermitActionType } from "../types/PermitActionType";
+import {
+  PERMIT_ACTION_TYPES,
+  PermitActionType,
+} from "../types/PermitActionType";
+import { PermitApprovalSource } from "../types/PermitApprovalSource";
+import { PermitStatus } from "../types/PermitStatus";
 import { getPermitActionLabel } from "./getPermitActionLabel";
+import { isPermitEligibleForAmendOrRevokeActions } from "./isPermitEligibleForAmendOrRevokeActions";
+
+export interface PermitActionPermissions {
+  canResendPermit: boolean;
+  canViewPermitReceipt: boolean;
+  canViewExpiredPermitReceipt: boolean;
+  canAmendPermit: boolean;
+  canVoidPermit: boolean;
+  canCopyPermit: boolean;
+}
 
 /**
  * Returns options for the row actions.
- * @param isExpired Has the permit expired?
  * @returns Action options that can be performed for the permit.
  */
 export const getPermitActionOptions = (
   permitActions: {
     action: PermitActionType;
-    isAuthorized: (isAuthorized: boolean) => boolean;
+    isAuthorized: boolean;
   }[],
-  isExpired: boolean,
 ) => {
   return permitActions
-    .filter((action) => action.isAuthorized(isExpired))
+    .filter((action) => action.isAuthorized)
     .map(({ action }) => ({
       label: getPermitActionLabel(action),
       value: action,
     }));
+};
+
+export const getPermitRowActionOptions = ({
+  isPermitInactiveOrExpired,
+  permitStatus,
+  permitApprovalSource,
+  permissions,
+}: {
+  isPermitInactiveOrExpired: boolean;
+  permitStatus: PermitStatus;
+  permitApprovalSource: PermitApprovalSource;
+  permissions: PermitActionPermissions;
+}) => {
+  const {
+    canResendPermit,
+    canViewPermitReceipt,
+    canViewExpiredPermitReceipt,
+    canAmendPermit,
+    canVoidPermit,
+    canCopyPermit,
+  } = permissions;
+
+  const isEligibleForAmendOrRevoke = isPermitEligibleForAmendOrRevokeActions(
+    permitStatus,
+    permitApprovalSource,
+  );
+
+  return getPermitActionOptions([
+    {
+      action: PERMIT_ACTION_TYPES.COPY,
+      isAuthorized: canCopyPermit,
+    },
+    {
+      action: PERMIT_ACTION_TYPES.RESEND,
+      isAuthorized: canResendPermit,
+    },
+    {
+      action: PERMIT_ACTION_TYPES.VIEW_RECEIPT,
+      isAuthorized:
+        (!isPermitInactiveOrExpired && canViewPermitReceipt) ||
+        (isPermitInactiveOrExpired && canViewExpiredPermitReceipt),
+    },
+    {
+      action: PERMIT_ACTION_TYPES.AMEND,
+      isAuthorized: isEligibleForAmendOrRevoke && canAmendPermit,
+    },
+    {
+      action: PERMIT_ACTION_TYPES.VOID_REVOKE,
+      isAuthorized: isEligibleForAmendOrRevoke && canVoidPermit,
+    },
+  ]);
 };
