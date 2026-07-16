a530965e145b53b3aca66e339b36750499334f09
zgong-gov <ZhongYu.Gong@gov.bc.ca>
2026-07-16 15:39:40 -0700

    Add missing logic for determining start date selection for Highway Crossing permit type

--- frontend/src/features/permits/hooks/usePermitDateSelection.ts
+++ frontend/src/features/permits/hooks/usePermitDateSelection.ts
@@ -115,6 +115,7 @@ export const usePermitDateSelection = ({
         isAmend,
       ),
       maxAllowedFutureStartDate: getMaxAllowedPermitFutureStartDate(
+        permitType,
         currentDate,
         isStaff,
       ),
