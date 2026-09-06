fe8e184a1334041383a80041aba9291ab7f5f0d1
Chris Berg <christopherpberg@gmail.com>
2026-09-05 18:30:13 -0700

    feat: updating ingress urls to support tls hostname

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/frontend/'
   envDir: path.resolve(process.cwd() + "/../"),
   server: {
     port: 3000,
