634d550aeedb47f6e2a6b12e8352e86d5e8d779f
Praveen Raju <praveen.raju@aot-technologies.com>
2026-09-26 01:48:34 -0400

    migrate public ts5 to ts6

--- public/src/main.ts
+++ public/src/main.ts
@@ -6,7 +6,7 @@ import { FallbackExceptionFilter } from './common/filters/fallback.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import helmet from 'helmet';
 import { customLogger } from './common/logger/logger.config';
-import * as responseTime from 'response-time';
+import responseTime from 'response-time';
 import { CustomValidationPipe } from './common/pipe/custom-validation.pipe';
 
 const allowedOrigins = [
--- public/test/e2e/app.e2e-spec.ts
+++ public/test/e2e/app.e2e-spec.ts
@@ -1,8 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from '@app/app.module';
-import { App } from 'supertest/types';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -17,7 +16,8 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer() as unknown as App)
+    const server = app.getHttpServer() as Parameters<typeof request>[0];
+    return request(server)
       .get('/')
       .expect(200)
       .expect('Public API Healthcheck!');
